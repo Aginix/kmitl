f5183d20d283f9870e24a6e0edb549951c9cd57c
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-02-11 15:00:06 +0700

    [ADD] account_analytic_kmitl

---
+++ account_analytic_kmitl/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "KMITL Account Analytic",
+    "version": "16.0.1.0.0",
+    "summary": """ KMITL Account Analytic""",
+    "category": "KMITL/Accounting",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "depends": ["account_analytic_parent"],
+    "data": [
+        "data/account.analytic.account.csv",
+        "data/account.analytic.plan.csv",
+    ],
+    "auto_install": False,
+    "license": "LGPL-3",
+}
