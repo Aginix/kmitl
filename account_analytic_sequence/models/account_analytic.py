3e988a2f6b44874b797a025e28f93649cdb4d314
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-02-13 22:57:34 +0700

    update

--- account_analytic_sequence/models/account_analytic.py
+++ account_analytic_sequence/models/account_analytic.py
@@ -5,8 +5,8 @@ from odoo import fields, models
 _logger = logging.getLogger(__name__)
 
 
-class AccountAnalytic(models.Model):
-    _inherit = "account.analytic"
+class AccountAnalyticAccount(models.Model):
+    _inherit = "account.analytic.account"
     _order = "sequence, name"
 
     sequence = fields.Integer(default=50)
