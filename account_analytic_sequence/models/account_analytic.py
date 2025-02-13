87b3b7636bfa994fd1cd4f9ba0f086b0ce915d06
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-02-13 22:47:04 +0700

    [ADD] account_analytic_kmitl

--- account_analytic_sequence/models/account_analytic.py
+++ account_analytic_sequence/models/account_analytic.py
@@ -5,8 +5,8 @@ from odoo import fields, models
 _logger = logging.getLogger(__name__)
 
 
-class AccountAnalyticPlan(models.Model):
-    _inherit = "account.analytic.plan"
+class AccountAnalytic(models.Model):
+    _inherit = "account.analytic"
     _order = "sequence, name"
 
     sequence = fields.Integer(default=50)
