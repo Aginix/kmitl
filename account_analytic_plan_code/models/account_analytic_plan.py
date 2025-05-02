d1a3486694f3f0ed0c3b2c4f65d62619541aec0a
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-05-02 17:39:28 +0700

    Update account_analytic_plan.py

--- account_analytic_plan_code/models/account_analytic_plan.py
+++ account_analytic_plan_code/models/account_analytic_plan.py
@@ -6,6 +6,7 @@ _logger = logging.getLogger(__name__)
 
 
 class AccountAnalyticPlan(models.Model):
+    _name = "account.analytic.plan"
     _inherit = ["account.analytic.plan", "mail.activity.mixin"]
 
     code = fields.Char(tracking=True)  # For internal identification
