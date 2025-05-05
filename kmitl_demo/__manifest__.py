4778f9a076e284946ccd80c36ca4db511e97e790
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-05-05 17:12:31 +0700

    [ADD] kmitl_demo (#16)
    * [ADD] kmitl_demo
    
    * fix
    
    * fix
    
    * fix again
    
    * update
    
    * fix name
    
    * update
    
    * u
    
    * update hr.department.csv
    
    * fix file
    
    * fix dependencies
    
    * update hr.department.csv
    
    * fix duplicated record id
    
    * add enable analytic account by default
    
    * [IMP] add analytic_plan_departments data
    
    * add account.fiscal.year.csv
    
    * add logo
    
    * fix logo name
    
    * fix
    
    * remove error
    
    * fix
    
    * setup coa
    
    * remove error

---
+++ kmitl_demo/__manifest__.py
@@ -0,0 +1,26 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "KMITL Demo",
+    "version": "16.0.0.0.0",
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "depends": [
+        "base",
+        "account",
+        "account_kmitl",
+        "hr",
+        "hr_department_code",
+        "account_fiscal_year",
+    ],
+    "data": [
+        "data/company.xml",
+        "data/hr.department.csv",
+        "data/account.fiscal.year.csv",
+    ],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
