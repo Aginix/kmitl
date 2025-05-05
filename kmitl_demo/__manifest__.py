64ae59d454f812445c791a7868d796d0aaecbbb3
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-05-05 11:17:27 +0700

    fix again

--- kmitl_demo/__manifest__.py
+++ kmitl_demo/__manifest__.py
@@ -1,13 +1,15 @@
-# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
-    "name": "KMITL demo",
-    "version": "",
-    "summary": """ KMITL Demo """,
-    "author": "",
-    "website": "",
-    "category": "",
-    "depends": ["account_kmitl"],
-    "data": ["data/company.xml"],
+    "name": "KMITL Demo",
+    "version": "16.0.0.0.0",
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "depends": ["base", "account", "account_kmitl"],
+    "data": [
+        "data/company.xml",
+    ],
     "application": False,
     "installable": True,
     "auto_install": False,
