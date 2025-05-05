9dbd9afb8dc62097c861e0201fd46e4eaeaa908b
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-05-05 11:08:45 +0700

    fix

--- kmitl_demo/__manifest__.py
+++ kmitl_demo/__manifest__.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'KMITL demo',
-    'version': '',
-    'summary': """ KMITL Demo """,
-    'author': '',
-    'website': '',
-    'category': '',
-    'depends': ['base', 'account_kmitl', 'account_analytic_kmitl'],
-    'data': [
-
-    ],
-
-    'application': false,
-    'installable': True,
-    'auto_install': False,
-    'license': 'LGPL-3',
+    "name": "KMITL demo",
+    "version": "",
+    "summary": """ KMITL Demo """,
+    "author": "",
+    "website": "",
+    "category": "",
+    "depends": ["account_kmitl"],
+    "data": ["data/company.xml"],
+    "application": false,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
 }
