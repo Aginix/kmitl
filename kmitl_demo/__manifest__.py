9b52212d52b1ed1a7170418cd562325aadfc1e37
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-05-05 11:40:00 +0700

    update

--- kmitl_demo/__manifest__.py
+++ kmitl_demo/__manifest__.py
@@ -6,10 +6,8 @@
     "category": "KMITL",
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
-    "depends": ["base", "account", "account_kmitl"],
-    "data": [
-        "data/company.xml",
-    ],
+    "depends": ["base", "account", "account_kmitl", "hr"],
+    "data": ["data/company.xml", "data/hr_department.csv"],
     "application": False,
     "installable": True,
     "auto_install": False,
