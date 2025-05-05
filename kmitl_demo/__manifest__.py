e2331c0507b3e46a23c8fcebeefa6464d9b25b50
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-05-05 11:03:23 +0700

    [ADD] kmitl_demo

---
+++ kmitl_demo/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'KMITL demo',
+    'version': '',
+    'summary': """ KMITL Demo """,
+    'author': '',
+    'website': '',
+    'category': '',
+    'depends': ['base', 'account_kmitl', 'account_analytic_kmitl'],
+    'data': [
+
+    ],
+
+    'application': false,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
