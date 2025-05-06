1acf1f7c7eed47466981aa15b6df036f1faa9bb7
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-05-06 15:08:02 +0700

    update fields

--- procurement_plan/models/procurement_plan_payment.py
+++ procurement_plan/models/procurement_plan_payment.py
@@ -16,22 +16,6 @@ class ProcurementPlanPayment(models.Model):
     )
     number = fields.Integer("Installment Number", default=1)
     number_of_days = fields.Integer("Number of Days")
-    month = fields.Selection(
-        [
-            ("10", "10/67"),
-            ("11", "11/67"),
-            ("12", "12/67"),
-            ("1", "1/68"),
-            ("2", "2/68"),
-            ("3", "3/68"),
-            ("4", "4/68"),
-            ("5", "5/68"),
-            ("6", "6/68"),
-            ("7", "7/68"),
-            ("8", "8/68"),
-            ("9", "9/68"),
-        ],
-        string="Payment Disbursement Date (Month)",
-    )
-    # month = fields.Integer(string="Payment Disbursement Date (Month)")
+    month = fields.Integer(string="Payment Disbursement Date (Month)")
     amount = fields.Float("Payment Amount")
+    state = fields.Selection(related='procurement_plan_id.state')
