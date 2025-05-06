9dc6162c77191152aa2cb9e72fd57043bc916bf8
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-05-06 14:10:38 +0700

    add i18n

--- procurement_plan/__manifest__.py
+++ procurement_plan/__manifest__.py
@@ -8,7 +8,7 @@
     "depends": ["base", "account_fiscal_year", "l10n_th_kmitl_procurement"],
     "data": [
         "security/ir.model.access.csv",
-        "views/procurement_plan_views.xml"
+        "views/procurement_plan_views.xml",
         "views/procurement_plan_menus.xml",
     ],
     "application": False,
