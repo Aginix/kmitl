ddf1537b721e760d4d2bd1f752089113690ac07b
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-05-06 11:16:41 +0700

    [ADD] l10n_th_kmitl_procurement

---
+++ l10n_th_kmitl_procurement/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "L10n_th_kmitl_procurement",
+    "version": "",
+    "summary": """ L10n_th_kmitl_procurement Summary """,
+    "author": "",
+    "website": "",
+    "category": "",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/procurement_method_views.xml",
+        "views/procurement_type_views.xml",
+        "views/purchase_type_views.xml",
+        "data/procurement_method.xml",
+        "data/procurement_type.xml",
+        "data/purchase_type.xml",
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
