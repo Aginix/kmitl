480f0136b39dafc1e8a9d003e3459741ec18e41f
nopparuts <ak3.nopparut@gmail.com>
2025-05-07 00:33:45 +0700

    fix-procurement_plan_payment-month_list

--- procurement_plan/models/procurement_plan_payment.py
+++ procurement_plan/models/procurement_plan_payment.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
 import logging
 
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError, ValidationError
+from odoo import fields, models
 
 _logger = logging.getLogger(__name__)
 
@@ -11,11 +9,26 @@ class ProcurementPlanPayment(models.Model):
     _name = "procurement.plan.payment"
     _description = "Procurement Plan Payment"
 
+    MONTH_SELECTION = [
+        ("01", "January"),
+        ("02", "February"),
+        ("03", "March"),
+        ("04", "April"),
+        ("05", "May"),
+        ("06", "June"),
+        ("07", "July"),
+        ("08", "August"),
+        ("09", "September"),
+        ("10", "October"),
+        ("11", "November"),
+        ("12", "December"),
+    ]
+
     procurement_plan_id = fields.Many2one(
         comodel_name="procurement.plan", readonly=True
     )
     number = fields.Integer("Installment Number", default=1)
     number_of_days = fields.Integer("Number of Days")
-    month = fields.Integer(string="Payment Disbursement Date (Month)")
+    month = fields.Selection(MONTH_SELECTION, "Payment Disbursement Date (Month)")
     amount = fields.Float("Payment Amount")
-    state = fields.Selection(related='procurement_plan_id.state')
+    state = fields.Selection(related="procurement_plan_id.state")
