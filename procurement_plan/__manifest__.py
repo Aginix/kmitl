6c921e895bfc07aa11b3d77a94cd68fce0881e7b
Nopparut Saelim <ak3.nopparut@gmail.com>
2025-05-07 14:22:16 +0700

    [16.0][FIX] procurement_plan-month_selection_i18n (#31)

--- procurement_plan/__manifest__.py
+++ procurement_plan/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Procurement Plan",
-    "version": "16.0.1.0.2",
+    "version": "16.0.1.0.3",
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
     "category": "KMITL",
