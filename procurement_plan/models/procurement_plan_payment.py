93d7405eaaee9e584214829b9afdc2d2daf8dc3a
nopparuts <ak3.nopparut@gmail.com>
2025-05-07 12:10:27 +0700

    16.0-fix-procurement_plan-month_selection

--- procurement_plan/models/procurement_plan_payment.py
+++ procurement_plan/models/procurement_plan_payment.py
@@ -4,25 +4,25 @@ from odoo import fields, models
 
 _logger = logging.getLogger(__name__)
 
+MONTH_SELECTION = [
+    ("1", "January"),
+    ("2", "February"),
+    ("3", "March"),
+    ("4", "April"),
+    ("5", "May"),
+    ("6", "June"),
+    ("7", "July"),
+    ("8", "August"),
+    ("9", "September"),
+    ("10", "October"),
+    ("11", "November"),
+    ("12", "December"),
+]
+
 
 class ProcurementPlanPayment(models.Model):
     _name = "procurement.plan.payment"
     _description = "Procurement Plan Payment"
-
-    MONTH_SELECTION = [
-        ("01", "January"),
-        ("02", "February"),
-        ("03", "March"),
-        ("04", "April"),
-        ("05", "May"),
-        ("06", "June"),
-        ("07", "July"),
-        ("08", "August"),
-        ("09", "September"),
-        ("10", "October"),
-        ("11", "November"),
-        ("12", "December"),
-    ]
 
     procurement_plan_id = fields.Many2one(
         comodel_name="procurement.plan", readonly=True
