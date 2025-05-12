0045b5af6eef644edc29b41a4cb4d7ba3de9cd27
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-05-13 02:30:21 +0700

    [IMP] budget: separate file

--- budget/models/__init__.py
+++ budget/models/__init__.py
@@ -4,3 +4,4 @@ from . import budget_appropriation
 from . import budget_commitment
 from . import budget_commitment_line
 from . import budget_template_structure
+from . import budget_appropriation_line
