f6b2545f60c2338d6e6479c246e4277b7789c197
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-05-14 11:56:18 +0700

    ทดสอบ runbot

---
+++ budget/static/src/components/note_line/note_line.js
@@ -0,0 +1,25 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+import { ListRenderer } from "@web/views/list/list_renderer";
+import { X2ManyField } from "@web/views/fields/x2many/x2many_field";
+
+export class NoteLineRenderer extends ListRenderer {
+    setup() {
+      super.setup();
+      console.log("test setup");
+    }
+  }
+  NoteLineRenderer.template = "budget.NoteLineRenderer";
+
+export class NoteLine extends X2ManyField {
+    setup() {
+        super.setup();
+    }
+}
+NoteLine.components = {
+    ...X2ManyField.components,
+    ListRenderer: NoteLineRenderer,
+}
+
+registry.category("fields").add("note_line", NoteLine);
