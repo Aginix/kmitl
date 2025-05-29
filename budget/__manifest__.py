8cde881376b5de6ef9c41aafa346972dd0a56f83
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-05-29 08:31:10 +0700

    fix data ordering

--- budget/__manifest__.py
+++ budget/__manifest__.py
@@ -14,9 +14,9 @@
         "views/budget_account_views.xml",
         "views/budget_appropriation_views.xml",
         "views/budget_commitment_views.xml",
-        "views/budget_expenditure_report_views.xml",
         "views/budget_journal_views.xml",
         "views/budget_menus.xml",
+        "views/budget_expenditure_report_views.xml",
         "views/budget_move_views.xml",
         "views/budget_procurement_views.xml",
         "views/budget_template_views.xml"
