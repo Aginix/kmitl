92aae00e1a2275de0172b84188d4874310e4702e
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-06-24 20:37:36 +0700

    [ADD]web theme

---
+++ web_theme_classic_special/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Web_theme_classic_special",
+    "version": "16.0.1.0.0",
+    "summary": """ Web_theme_classic_special Summary """,
+    "author": "",
+    "website": "",
+    "category": "",
+    "depends": ["web"],
+    "data": [],
+    "assets": {
+        "web.assets_backend": [
+            "/web_theme_classic_special/static/src/scss/web_theme_classic.scss",
+        ],
+    },
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
