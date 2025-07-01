4aa202d0c20f25c31a768164839d89696acc602e
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-07-02 03:14:10 +0700

    [16.0][ADD] budget_account_mass_edit

---
+++ budget_account_mass_edit/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Budget Account Mass Edit",
+    "version": "16.0.1.0.0",
+    "summary": """ Budget Account Mass Edit""",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "category": "KMITL/Budgeting",
+    "depends": ["budget", "server_action_mass_edit"],
+    "data": ["data/server_action_mass_edit.xml"],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
