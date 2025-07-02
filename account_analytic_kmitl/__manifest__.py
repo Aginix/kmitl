6fa53901aec155e938f963c5234cf7db3f4b5501
nopparuts <ak3.nopparut@gmail.com>
2025-07-02 11:17:56 +0700

    fix-account_analytic_kmitl-master_data

--- account_analytic_kmitl/__manifest__.py
+++ account_analytic_kmitl/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "KMITL Account Analytic",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "summary": """ KMITL Account Analytic""",
     "category": "KMITL/Accounting",
     "author": "Aginix Technologies",
