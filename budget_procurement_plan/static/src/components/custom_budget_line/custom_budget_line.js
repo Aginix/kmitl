fa5379f3a5900102c446c4eb016001b480f8b43f
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-07-13 13:59:46 +0700

    [FIX] nan value

--- budget_procurement_plan/static/src/components/custom_budget_line/custom_budget_line.js
+++ budget_procurement_plan/static/src/components/custom_budget_line/custom_budget_line.js
@@ -4,26 +4,48 @@ import {registry} from "@web/core/registry";
 import {X2ManyField} from "@web/views/fields/x2many/x2many_field";
 import {NoteLineRenderer} from "@budget/components/note_line/note_line.esm";
 import {useService} from "@web/core/utils/hooks";
-import {useState, onWillStart} from "@odoo/owl";
+import {useState, onWillStart, onWillUpdateProps} from "@odoo/owl";
 
 export class CustomeNoteLineRenderer extends NoteLineRenderer {
     setup() {
         super.setup();
         this.orm = useService("orm");
-        this.procurementData = useState({});
+        this.procurementData = useState({
+            draftPlans: [],
+            draftMap: new Map(),
+        });
         onWillStart(() => this._loadProcurementPlans());
+        onWillUpdateProps((nextProps) => this.updateProp(nextProps));
     }
+    updateProp(props) {
+        const records = props.list.records[1].data.procurement_plan_ids.records; // ยังทำให้ทำทีละ record ไม่ได้
+        const draftPlans = records.filter((r) => r.data.state === "draft");
+        const draftMap = new Map();
+        for (const rec of draftPlans) {
+            const planId = rec.data.id;
+            if (this.procurementData.hasOwnProperty(planId)) {
+                this.procurementData[rec.data.id] = rec.data;
+                console.log("เข้าเงื่อนไข draftMap", rec.data.id);
+            } else {
+                console.log("ไม่เข้าเงื่อนไข draftMap", rec.data.id);
+                draftMap.set(rec.data.id, rec);
+            }
+        }
+        this.procurementData.draftPlans = draftPlans;
+        this.procurementData.draftMap = draftMap;
+        console.log("testttt =====>", props)
+        if(draftPlans.length === 0) {
+            this._loadProcurementPlans();
+        }
+    }
+
     formattedAmount(value) {
         return new Intl.NumberFormat("en-US").format(value);
     }
     async _loadProcurementPlans() {
-        try {
-            const plans = await this.orm.searchRead("procurement.plan", [], []);
-            for (const plan of plans) {
-                this.procurementData[plan.id] = plan;
-            }
-        } catch (error) {
-            console.error("ไม่สามารถโหลดแผนจัดซื้อจัดจ้างได้", error);
+        const plans = await this.orm.searchRead("procurement.plan", [], []);
+        for (const plan of plans) {
+            this.procurementData[plan.id] = plan;
         }
     }
 }
