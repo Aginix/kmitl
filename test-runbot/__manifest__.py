527dbaad38cdfd184d7168f36e51f228e0568797
nopparuts <ak3.nopparut@gmail.com>
2025-07-25 17:55:53 +0700

    add-module-test

---
+++ test-runbot/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Test Runbot",
+    "version": "15.0.1.0.0.",
+    "category": "Human Resources",
+    "website": "https://github.com/aginix/kmitl",
+    "author": "nopparuts, Aginix Technologies",
+    "maintainers": ["nopparuts"],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Test module for Runbot",
+    "depends": [],
+}
