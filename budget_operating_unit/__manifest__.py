b9b4bbfad6a3431e9cc92a748c561ac5fc6675c0
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-08-05 17:59:56 +0700

    [FIX] description

--- budget_operating_unit/__manifest__.py
+++ budget_operating_unit/__manifest__.py
@@ -1,23 +1,17 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Budget_operating_unit',
-    'version': '16.0.1.0.0',
-    'summary': """ Budget_operating_unit Summary """,
-    'author': '',
-    'website': '',
-    'category': '',
+    'name': 'Budget Operating Unit',
+    'version': '16.0.0.0.0',
+    'summary': """ Budget Operating Unit """,
+    'author': 'Aginix Technologies',
+    'website': 'https://github.com/Aginix/kmitl',
+    'category': 'KMITL/Budgeting',
     'depends': ['operating_unit' ,'budget'],
     "data": [
         "security/budget_security.xml",
         "views/budget_move_views.xml",
         "views/budget_transfer_views.xml"
     ],
-    'assets': {
-              'web.assets_backend': [
-                  'budget_operating_unit/static/src/**/*'
-              ],
-          },
-    'application': True,
     'installable': True,
     'auto_install': False,
     'license': 'LGPL-3',
--- budget_operating_unit_access_all/__manifest__.py
+++ budget_operating_unit_access_all/__manifest__.py
@@ -1,20 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Budget_operating_unit_access_all',
-    'version': '16.0.1.0.0',
-    'summary': """ Budget_operating_unit_access_all Summary """,
-    'author': '',
-    'website': '',
-    'category': '',
+    'name': 'Budget Operating Unit Access All',
+    'version': '16.0.0.0.0',
+    'summary': """ Budget Operating Unit Access All""",
+    'author': 'Aginix Technologies',
+    'website': 'https://github.com/Aginix/kmitl',
+    'category': 'KMITL/Budgeting',
     'depends': ['budget_operating_unit'],
     'data': [
         "security/budget_security.xml",
     ],
-    'assets': {
-              'web.assets_backend': [
-                  'budget_operating_unit_access_all/static/src/**/*'
-              ],
-          },
     'installable': True,
     'license': 'LGPL-3',
 }
