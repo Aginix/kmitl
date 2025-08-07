e3a06560d2b8a414fca43e0f4a991275189d1aab
Tanathip Singhanon <88467996+madara1150@users.noreply.github.com>
2025-08-07 09:01:30 +0700

    [16.0][ADD] budget-ou (#64)
    Co-authored-by: Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>

---
+++ budget_operating_unit_access_all/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Budget Operating Unit Access All',
+    'version': '16.0.0.0.0',
+    'summary': """ Budget Operating Unit Access All""",
+    'author': 'Aginix Technologies',
+    'website': 'https://github.com/Aginix/kmitl',
+    'category': 'KMITL/Budgeting',
+    'depends': ['budget_operating_unit'],
+    'data': [
+        "security/budget_security.xml",
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
