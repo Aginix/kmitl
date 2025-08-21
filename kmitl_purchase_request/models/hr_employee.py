38e5cdab3988b5084af29cfaa3555c937605d5d9
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-08-21 17:45:36 +0700

    [DEL]pr1 ,pr1_tier, kmitl_substate

--- kmitl_purchase_request/models/hr_employee.py
+++
@@ -1,7 +0,0 @@
-from odoo import _, api, fields, models
-
-
-class HrEmployee(models.Model):
-    _inherit = 'hr.employee'
-
-    user_id = fields.Many2one('res.users', string='Related User', ondelete='set null')
