b1532b83a57da4ba176e09d7b78f10884d5643cb
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-08-22 10:22:57 +0700

    change depends

--- kmitl_purchase_request_2/__manifest__.py
+++ kmitl_purchase_request_2/__manifest__.py
@@ -6,7 +6,7 @@
     'author': 'Aginix Techonologies',
     'website': 'https://github.com/aginix/kmitl',
     'category': 'KMITL',
-    'depends': ['kmitl_purchase_request', 'l10n_th_base_sequence'],
+    'depends': ['purchase_request_kmitl', 'l10n_th_base_sequence'],
     "data": [
         "data/purchase_request_two_sequence.xml",
         "data/res_partner.xml",
