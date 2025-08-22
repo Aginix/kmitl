b6020d88e66b2c0cb7a7d2e31b167401efa533ed
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-08-22 11:46:34 +0700

    [IMP] filter invoice

--- kmitl_purchase_agreement/models/agreement.py
+++ kmitl_purchase_agreement/models/agreement.py
@@ -76,16 +76,10 @@ class Agreement(models.Model):
     work_start_date = fields.Date(related='purchase_order_id.work_start_date', string="Work start date")
     work_end_date = fields.Date(related='purchase_order_id.work_end_date', string="Work end date")
 
-    has_invoice_plan = fields.Boolean(
-        string="Has Invoice Plan",
-        compute="_compute_has_invoice_plan",
-        store=False
+    is_invoice = fields.Boolean(
+        related='purchase_order_id.use_invoice_plan',
+        store=True
     )
-    
-    @api.depends('invoice_plan_ids')
-    def _compute_has_invoice_plan(self):
-        for record in self:
-            record.has_invoice_plan = bool(record.invoice_plan_ids)
 
     def action_open_new_version_wizard(self):
         self.ensure_one()
