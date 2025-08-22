c61bd16977b68466f43ef38ee33c6ec8cf79a400
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-08-22 15:33:55 +0700

    [ADD]budget

---
+++ purchase_request_budget_kmitl/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Request Budget Kmitl',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Request Budget Kmitl Summary """,
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    'depends': ['purchase_request_kmitl', 'budget'],
+    'data': [
+        "security/purchase_request.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
