6da40893cbaed7be5691cf728ae748a3aad2da6e
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-08-24 14:29:46 +0700

    [DEL][ver.OLD] pr2

--- kmitl_purchase_request_2/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import purchase_request
-from . import purchase_request_two 
-from . import purchase_request_two_line
-from . import purchase_request_two_submitted
-from . import purchase_request_two_submitted_line
