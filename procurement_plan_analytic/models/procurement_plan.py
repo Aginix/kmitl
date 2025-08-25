a77d7a454e5ae825a513b5a9024b500c5e08b248
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-08-26 01:37:30 +0700

    fix

--- procurement_plan_analytic/models/procurement_plan.py
+++ procurement_plan_analytic/models/procurement_plan.py
@@ -3,7 +3,6 @@ import logging
 
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
-from procurement_plan import ProcurementPlan
 
 _logger = logging.getLogger(__name__)
 
@@ -19,7 +18,6 @@ class ProcurementPlan(models.Model):
         ondelete="set null",
         domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
         check_company=True,
-        states=ProcurementPlan.READONLY_STATES,
         help="Analytic account to which this procurement plan. \n"
         "Track the costs and revenues of your procurement plan by setting this analytic account on your related documents (e.g. budgetings, purchase requests, purchase orders etc.).",
     )
