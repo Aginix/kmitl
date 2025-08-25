708a6968454ae85332ebd350b0d72ec01da88ae1
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-08-25 23:49:42 +0700

    [FIX] Agreement depend

--- kmitl_purchase_agreement/__manifest__.py
+++ kmitl_purchase_agreement/__manifest__.py
@@ -6,7 +6,7 @@
     'author': 'Aginix Techonologies',
     'website': 'https://github.com/aginix/kmitl',
     'category': 'KMITL',
-    'depends': ['agreement_legal', 'kmitl_purchase_order'],
+    'depends': ['agreement_legal', 'purchase_order_kmitl'],
     "data": [
         "security/ir.model.access.csv",
         "views/agreement_views.xml",
