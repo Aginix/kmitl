5ad4165b357bb198534d9748eabcebc3fe6d478c
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-08-26 02:31:33 +0700

    fix

--- procurement_plan_budget/models/procurement_plan.py
+++ procurement_plan_budget/models/procurement_plan.py
@@ -3,7 +3,6 @@ import logging
 
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
-from procurement_plan import ProcurementPlan
 
 _logger = logging.getLogger(__name__)
 
@@ -12,8 +11,16 @@ class ProcurementPlan(models.Model):
     _name = 'procurement.plan'
     _inherit = ['procurement.plan']
 
+    READONLY_STATES = {
+        "validate": [("readonly", True)],
+        "pending": [("readonly", True)],
+        "procurement": [("readonly", True)],
+        "done": [("readonly", True)],
+        "cancel": [("readonly", True)],
+    }
+
     budget_account_id = fields.Many2one('budget.account')
-    activity_analytic_id = fields.Many2one(required=True, states=ProcurementPlan.READONLY_STATES)
-    department_analytic_id = fields.Many2one(required=True, states=ProcurementPlan.READONLY_STATES)
-    fund_analytic_id = fields.Many2one(required=True, states=ProcurementPlan.READONLY_STATES)
-    source_analytic_id = fields.Many2one(required=True, states=ProcurementPlan.READONLY_STATES)
+    activity_analytic_id = fields.Many2one(states=READONLY_STATES)
+    department_analytic_id = fields.Many2one(states=READONLY_STATES)
+    fund_analytic_id = fields.Many2one(states=READONLY_STATES)
+    source_analytic_id = fields.Many2one(states=READONLY_STATES)
