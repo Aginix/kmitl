ded86e7602514ea87bcc45301ede972ffa8e05c2
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-08-25 18:05:58 +0700

    [ADD] tier_validation

---
+++ purchase_request_approval_tier_validation/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Request Approval Tier Validation',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Request Approval Tier Validation """,
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    'depends': ['base_tier_validation', 'purchase_request_approval'],
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ purchase_request_approval_tier_validation/models/purchase_request_approval.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError, ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+class PurchaseRequestApproval(models.Model):
+    _name = "purchase.request.approval"
+    _inherit = ["purchase.request.approval", "tier.validation"]
+    _state_from = ["submitted"]
+    _state_to = ["approved"]
+
+    _tier_validation_manual_config = False
+
+    @api.model
+    def _get_under_validation_exceptions(self):
+        res = super(PurchaseRequestApproval, self)._get_under_validation_exceptions()
+        res.append("route_id")
+        return res
