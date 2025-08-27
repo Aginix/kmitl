eeedafd0596967f38b01da6fc5aff65892a80a70
Tanathip Singhanon <88467996+madara1150@users.noreply.github.com>
2025-08-27 16:21:19 +0700

    [16.0][FIX] pr1,pr2 one-to-one (#109)

--- purchase_request_approval/models/purchase_request.py
+++ purchase_request_approval/models/purchase_request.py
@@ -1,37 +1,21 @@
 from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class PurchaseRequest(models.Model):
     _inherit = 'purchase.request'
 
-    approval_ids = fields.One2many(
-        'purchase.request.approval',
-        'request_id',
-        string='Approval'
+    approval_id = fields.Many2one(
+        "purchase.request.approval",
+        string="PR2",
     )
 
     def action_create_approval(self):
         self.ensure_one()
-
-        approval = self.env['purchase.request.approval'].create({
-            'request_id': self.id,
-            'purchase_request_number': self.name,
-        })
-
-        line_vals = []
-        for line in self.line_ids:
-            line_vals.append((0, 0, {
-                'product_id': line.product_id.id,
-                'description': line.name,
-                'quantity': line.product_qty,
-                'unit_price' : line.estimated_cost/line.product_qty,
-            }))
-        approval.write({'line_ids': line_vals})
-
         return {
             'type': 'ir.actions.act_window',
-            'res_model': 'purchase.request.approval',
+            'res_model': 'purchase.request.approval.wizard',
             'view_mode': 'form',
-            'res_id': approval.id,
-            'target': 'current',
+            'target': 'new',
+            'context': {'active_id': self.id}
         }
