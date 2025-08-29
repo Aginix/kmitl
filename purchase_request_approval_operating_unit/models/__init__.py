f8d8fd7a53d06b1bb44d3823e67dc7b0c2eb27bc
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-08-29 12:36:49 +0700

    refactor clean

--- purchase_request_approval_operating_unit/models/__init__.py
+++ purchase_request_approval_operating_unit/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
+from . import purchase_request
 from . import purchase_request_approval
 from . import purchase_request_approval_line
--- purchase_request_approval_operating_unit/models/purchase_request.py
+++ purchase_request_approval_operating_unit/models/purchase_request.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
-from odoo import Command, _, api, fields, models
+import logging
+
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
+
+_logger = logging.getLogger(__name__)
 
 
 class PurchaseRequest(models.Model):
-    _name = "purchase.request"
-    _inherit = ["purchase.request", "tier.validation"]
+    _inherit = 'purchase.request'
 
     def _prepare_approval_vals(self):
+        # สร้าง PR2 จาก PR1 ให้คัดลอก OU ไปด้วย
         vals = super()._prepare_approval_vals()
         vals['operating_unit_id'] = self.operating_unit_id.id
         return vals
