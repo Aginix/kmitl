efe8a227be6134a7ef87d792e366aafe43467081
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-08-29 14:58:42 +0700

    แก้ error

--- purchase_work_acceptance_tier_validation/models/work_acceptance.py
+++ purchase_work_acceptance_tier_validation/models/work_acceptance.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import api, models
 
 
 class WorkAcceptance(models.Model):
