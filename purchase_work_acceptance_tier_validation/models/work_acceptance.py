bfd9a441153d4510da99052fe98e30c06de141fd
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-08-29 14:40:55 +0700

    [16.0][MIG] purchase_work_acceptance_tier_validation

---
+++ purchase_work_acceptance_tier_validation/models/work_acceptance.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class WorkAcceptance(models.Model):
+    _name = "work.acceptance"
+    _inherit = ["work.acceptance", "tier.validation"]
+    _state_from = ["draft"]
+    _state_to = ["accept"]
+
+    _tier_validation_manual_config = False
+
+    @api.model
+    def _get_under_validation_exceptions(self):
+        res = super(WorkAcceptance, self)._get_under_validation_exceptions()
+        res.append("route_id")
+        return res
