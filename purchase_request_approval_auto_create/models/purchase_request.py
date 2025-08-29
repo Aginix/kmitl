1273aafc22f61cde3ec2f94500eed683834d4f3c
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-08-29 12:23:47 +0700

    fix

--- purchase_request_approval_auto_create/models/purchase_request.py
+++ purchase_request_approval_auto_create/models/purchase_request.py
@@ -11,12 +11,14 @@ class PurchaseRequest(models.Model):
         return {
             'request_id': self.id,
             'purchase_request_number': self.name,
+            'line_ids': [Command.create(line._prepare_approval_line_vals()) for line in self.line_ids],
         }
 
     def _validate_tier(self, tiers=False):
         super()._validate_tier(tiers)
-        approval = self.env['purchase.request.approval'].create(self._prepare_approval_vals())
+        self._create_approval()
 
-        line_vals = [Command.create(line._prepare_approval_line_vals()) for line in self.line_ids]
+    def _create_approval(self):
+        approval = self.env['purchase.request.approval'].create(self._prepare_approval_vals())
         self.write({'approval_id': approval.id})
-        approval.write({'line_ids': line_vals})
+
