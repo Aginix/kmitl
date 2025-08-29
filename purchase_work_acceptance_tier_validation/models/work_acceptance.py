1e075941e9cdeb2eddddd0212456a13eead710e9
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-08-29 15:16:32 +0700

    นำตัวของ pr มาใช้แทน

--- purchase_work_acceptance_tier_validation/models/work_acceptance.py
+++ purchase_work_acceptance_tier_validation/models/work_acceptance.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class WorkAcceptance(models.Model):
@@ -11,9 +11,3 @@ class WorkAcceptance(models.Model):
     _state_to = ["accept"]
 
     _tier_validation_manual_config = False
-
-    @api.model
-    def _get_under_validation_exceptions(self):
-        res = super(WorkAcceptance, self)._get_under_validation_exceptions()
-        res.append("route_id")
-        return res
