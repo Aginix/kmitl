7d0ebadab4ddecd787c9295305cf4448625e6b84
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-08-29 09:15:45 +0700

    แก้ไข error

--- purchase_request_approval_operating_unit/models/purchase_request_approval.py
+++ purchase_request_approval_operating_unit/models/purchase_request_approval.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from odoo import models, fields, api, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
 
 _logger = logging.getLogger(__name__)
@@ -13,7 +13,7 @@ class PurchaseRequestApproval(models.Model):
     operating_unit_id = fields.Many2one(
         comodel_name="operating.unit",
         string="Operating Unit",
-        default=lambda self: self.env["res.users"]._get_default_operating_unit(
+        default=lambda self: self.env["res.users"].operating_unit_default_get(
             self._uid
         ),
     )
