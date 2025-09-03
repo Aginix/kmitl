60ab5d1865bc12ac46030cd5f592da43841ac8a0
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-03 17:26:20 +0700

    fix error

--- purchase_request_approval/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import (
-    purchase_request,
-    purchase_request_line,
-    purchase_request_approval,
-    purchase_request_approval_line,
-)
--- purchase_request_tier_validation_kmitl/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-from . import purchase_request
-from . import tier_definition
-from . import purchase_request
