6ca1e087a7cfde5b823806b915ddd6bea72cef68
Tanathip Singhanon <88467996+madara1150@users.noreply.github.com>
2025-09-03 19:46:54 +0700

    [16.0][ADD] purchase_request_security (#130)

---
+++ purchase_request_security/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Request Security',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Request Security """,
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "category": "KMITL",
+    'depends': ['purchase_request'],
+    'data': [
+        "security/purchase_request_security.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
