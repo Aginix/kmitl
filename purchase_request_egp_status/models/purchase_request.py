1df77269124bfe7050d23a51a8f1a4ea5a2a92b6
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-04 11:14:58 +0700

    แก้เงื่อนไข egp_status

--- purchase_request_egp_status/models/purchase_request.py
+++ purchase_request_egp_status/models/purchase_request.py
@@ -8,7 +8,4 @@ class PurchaseRequest(models.Model):
     @api.depends("estimated_cost", "state")
     def _compute_is_egp(self):
         for record in self:
-            if record.state == "approved":
-                record.is_egp = record.estimated_cost > 100000
-            else:
-                record.is_egp = False
+            record.is_egp = record.estimated_cost > 100000
