ee28fb85ed72eaa62fe038cc78625b16968287c2
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-04 10:46:05 +0700

    [16.0][FIX] pr_egp ให้เป็น base

---
+++ purchase_request_egp_status/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Request e-GP Status',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Request e-GP Status """,
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    'depends': ['purchase_request_egp', 'purchase_request_security'],
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
