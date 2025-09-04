405c3cd415c4c79af8479ea1430b095ffabe95e7
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-04 10:05:47 +0700

    [16.0][ADD] purchase_order_no_create

---
+++ purchase_order_no_create/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Order No Create',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Order No Create """,
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    'depends': ['purchase'],
+    "data": [
+        "views/purchase_order_views.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
