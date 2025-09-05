070fc2dd9c85da55c3145208281c50dab06e810d
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-09-05 20:18:46 +0700

    add attachment dlc

---
+++ purchase_request_attachment/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase_request_attachment',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase_request_attachment Summary """,
+    'author': '',
+    'website': '',
+    'category': '',
+    'depends': ['purchase_request_kmitl'],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/purchase_request_attachment_views.xml"
+    ],
+    'assets': {
+              'web.assets_backend': [
+                  'purchase_request_attachment/static/src/**/*'
+              ],
+          },
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ purchase_request_attachment/models/purchase_request_attachment.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+class PurchaseRequestAttachment(models.Model):
+    _name = 'purchase.request.attachment'
+    _description = 'PurchaseRequestAttachment'
+
+    request_id = fields.Many2one("purchase.request", string="Purchase Request")
+    attachment_type = fields.Selection(
+        [
+            ("tor", "Specification (TOR)"),
+            ("rfq", "Quotation"),
+            ("etc", "Etc"),
+        ],
+        default="tor",
+    )
+    upload_file = fields.Binary(string="Upload File", attachment=False)
+    file_name = fields.Char(string="Filename")
+    attachment_id = fields.Many2one("ir.attachment", string="Stored Attachment", readonly=True)
+    description = fields.Char(string="Description")
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        for record, vals in zip(records, vals_list):
+            if vals.get("upload_file") and vals.get("file_name"):
+                attachment = self.env["ir.attachment"].create({
+                    "name": vals.get("file_name"),
+                    "datas": vals.get("upload_file"),
+                    "res_model": "purchase.request",
+                    "res_id": record.request_id.id,
+                    "type": "binary",
+                })
+                record.attachment_id = attachment.id
+        return records
+
+    def write(self, vals):
+        res = super().write(vals)
+        for record in self:
+            if vals.get("upload_file") and vals.get("file_name"):
+                attachment = self.env["ir.attachment"].create({
+                    "name": vals.get("file_name"),
+                    "datas": vals.get("upload_file"),
+                    "res_model": "purchase.request",
+                    "res_id": record.request_id.id,
+                    "type": "binary",
+                })
+                record.attachment_id = attachment.id
+        return res
