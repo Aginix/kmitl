bacdd481da0655a2b8960068757ffd5fac8d18c8
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-05 21:03:10 +0700

    add func

--- purchase_request_kmitl_tier_validation/models/purchase_request.py
+++ purchase_request_kmitl_tier_validation/models/purchase_request.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from lxml import etree
+
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
 
@@ -7,3 +9,18 @@ class PurchaseRequest(models.Model):
     _inherit = 'purchase.request'
     _state_from = ["to_approve"]
 
+    is_purchase_request = fields.Boolean(compute="_compute_is_purchase_request")
+
+    def _compute_is_purchase_request(self):
+        for rec in self:
+            rec.is_purchase_request = rec._name == "purchase.request"
+
+    def _add_tier_validation_buttons(self, node, params):
+        """"close btn"""
+        if self.is_purchase_request:
+            str_element = self.env["ir.qweb"]._render(
+                "base_tier_validation.tier_validation_buttons", params
+            )
+            new_node = etree.fromstring(str_element)
+            return new_node
+        return etree.Element("div")
