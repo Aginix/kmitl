a3eb9bac89d9b568479ab3ce1fdf30bb2825130a
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-05 19:13:22 +0700

    [16.0][ADD] purchase_request_kmitl_tier_validation

---
+++ purchase_request_kmitl_tier_validation/models/purchase_request.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError, ValidationError
+
+
+class PurchaseRequest(models.Model):
+    _inherit = 'purchase.request'
+    _state_from = ["to_approve"]
+
