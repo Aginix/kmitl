d39e5366b8816561058fde010363e13921440535
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-05 12:18:08 +0700

    [16.0][ADD] purchase_request_budget_security

---
+++ purchase_request_budget_security/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Request Budget Security',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Request Budget Security Summary """,
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    'depends': ['purchase_request_budget', 'purchase_request_security'],
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
