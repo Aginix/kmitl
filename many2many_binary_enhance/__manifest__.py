be43be130604238b47a59fc380e7d9a3751881b1
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-09-07 23:51:21 +0700

    [16.0][ADD] many2many_binary_enhance

---
+++ many2many_binary_enhance/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Many2many_binary_enhance',
+    'version': '16.0.1.0.0',
+    'summary': """ Many2many_binary_enhance Summary """,
+    'author': '',
+    'website': '',
+    'category': '',
+    'depends': ['base', 'web'],
+    'data': [
+        
+    ],
+    'assets': {
+              'web.assets_backend': [
+                  'many2many_binary_enhance/static/src/**/*'
+              ],
+          },
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
