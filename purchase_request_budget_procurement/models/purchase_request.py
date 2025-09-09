1d9cbe92f3f77bc69ee90872e08bfa848a772227
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-09-09 09:19:26 +0700

    add missing field

--- purchase_request_budget_procurement/models/purchase_request.py
+++ purchase_request_budget_procurement/models/purchase_request.py
@@ -18,6 +18,7 @@ class PurchaseRequest(models.Model):
         comodel_name="procurement.plan", string="รายการแผนจัดซื้อจัดจ้าง", domain="", tracking=True
     )
 
+    budget_account_id = fields.Many2one('budget.account', store=True, compute="_compute_procurement_plan_analytic_id")
     activity_analytic_id = fields.Many2one("account.analytic.account", store=True, compute="_compute_procurement_plan_analytic_id")
     department_analytic_id = fields.Many2one("account.analytic.account", store=True, compute="_compute_procurement_plan_analytic_id")
     fund_analytic_id = fields.Many2one("account.analytic.account", store=True, compute="_compute_procurement_plan_analytic_id")
@@ -41,57 +42,3 @@ class PurchaseRequest(models.Model):
                 rec.fund_analytic_id = rec.procurement_plan_id.fund_analytic_id.id
                 rec.source_analytic_id = rec.procurement_plan_id.source_analytic_id.id
                 rec.procurement_plan_analytic_id = rec.procurement_plan_id.procurement_plan_analytic_id.id
-
-    def action_view_procurement_plan(self):
-        self.ensure_one()
-        if not self.procurement_plan_id:
-            return {"type": "ir.actions.act_window_close"}
-        
-        return {
-            "type": "ir.actions.act_window",
-            "res_model": "procurement.plan",
-            "view_mode": "form",
-            "res_id": self.procurement_plan_id.id,
-            "target": "current",
-        }
-
-
-class ProcurementPlan(models.Model):
-    _inherit = "procurement.plan"
-
-    purchase_request_count = fields.Integer(
-        string="Purchase Requests Count",
-        compute="_compute_purchase_request_count"
-    )
-
-    @api.depends("purchase_request_ids")
-    def _compute_purchase_request_count(self):
-        for record in self:
-            record.purchase_request_count = len(record.purchase_request_ids)
-
-    purchase_request_ids = fields.One2many(
-        comodel_name="purchase.request",
-        inverse_name="procurement_plan_id",
-        string="Purchase Requests"
-    )
-
-    def action_view_purchase_requests(self):
-        self.ensure_one()
-        action = self.env["ir.actions.actions"]._for_xml_id("purchase_request.purchase_request_form_action")
-        if len(self.purchase_request_ids) > 1:
-            action["domain"] = [("id", "in", self.purchase_request_ids.ids)]
-        elif len(self.purchase_request_ids) == 1:
-            form_view = [(self.env.ref("purchase_request.view_purchase_request_form").id, "form")]
-            if "views" in action:
-                action["views"] = form_view + [(state, view) for state, view in action["views"] if view != "form"]
-            else:
-                action["views"] = form_view
-            action["res_id"] = self.purchase_request_ids.ids[0]
-        else:
-            action = {"type": "ir.actions.act_window_close"}
-        context = {
-            "default_use_procurement_plan": True,
-            "default_procurement_plan_id": self.id,
-        }
-        action["context"] = context
-        return action
