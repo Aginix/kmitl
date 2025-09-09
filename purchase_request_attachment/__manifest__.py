7546a9e63c8a068f288809c00075f43f7573996a
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-09-10 00:06:16 +0700

    change depend

--- purchase_request_attachment/__manifest__.py
+++ purchase_request_attachment/__manifest__.py
@@ -6,7 +6,7 @@
     'author': 'Aginix Technologies',
     'website': 'https://github.com/aginix/kmitl',
     'category': 'KMITL',
-    'depends': ['purchase_request_kmitl'],
+    'depends': ['purchase_request'],
     "data": [
         "views/purchase_request_views.xml"
     ],
