4b537ac4eb06826e23eec111d91b306d94a70ec6
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-09-09 18:08:31 +0700

    [IMP] clean code

--- purchase_request_attachment/models/purchase_request.py
+++ purchase_request_attachment/models/purchase_request.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
-import logging
-
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError, ValidationError
-
-_logger = logging.getLogger(__name__)
 
 
 class PurchaseRequest(models.Model):
