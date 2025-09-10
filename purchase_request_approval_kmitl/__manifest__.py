83f20a1ac5d950af97e9c794bd5061ae204f9e45
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-10 11:52:02 +0700

    [16.0][ADD] purchase_request_approval_kmitl

---
+++ purchase_request_approval_kmitl/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Request Approval KMITL',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Request Approval Kmitl Summary """,
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "category": "KMITL",
+    'depends': ['l10n_th_gov_purchase_request', 'purchase_request_budget', 'purchase_request_security'],
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
