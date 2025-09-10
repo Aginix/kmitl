9b734b11dac4f2ee0565935fe096173dbf12fb37
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-10 16:13:30 +0700

    [FIX] add base budget

--- purchase_request_approval_kmitl/models/purchase_request.py
+++ purchase_request_approval_kmitl/models/purchase_request.py
@@ -7,9 +7,25 @@ from odoo.exceptions import UserError, ValidationError
 
 class PurchaseRequest(models.Model):
     _inherit = 'purchase.request'
-    _state_from = ["to_approve"]
+    _state_from = ["to_verify", "to_approve"]
+
+    _STATES = [
+        ("to_verify", "to be verified"),
+        ("to_approve",)
+    ]
 
     is_purchase_request = fields.Boolean(compute="_compute_is_purchase_request")
+    state = fields.Selection(
+        selection_add=_STATES,
+        string="Status",
+        index=True,
+        tracking=True,
+        required=True,
+        copy=False,
+        ondelete={
+        "to_verify": "set default",
+        }
+    )
 
     def _compute_is_purchase_request(self):
         for rec in self:
@@ -36,6 +52,38 @@ class PurchaseRequest(models.Model):
     @api.model
     def _get_after_validation_exceptions(self):
         res = super()._get_after_validation_exceptions()
+        res.append("state")
+        res.append("substate_id")
+        return res
+
+    def button_to_verify(self):
+        self.ensure_one()
+        if self.detect_exceptions() and not self.ignore_exception:
+            return self._popup_exceptions()
+        self.write({"state": "to_verify"})
+
+    @api.depends("state")
+    def _compute_is_editable(self):
+        res = super()._compute_is_editable()
+        for record in self:
+            if record.state in ("to_verify"):
+                record.is_editable = False
+
+    def request_validation(self):
+        self.ensure_one()
+        res = super().request_validation()
+        self.write({"state": "to_approve"})
+        return res
+
+    def restart_validation(self):
+        self.ensure_one()
+        res = super().restart_validation()
+        self.write({"state": "to_verify"})
+        return res
+
+    @api.model
+    def _get_under_validation_exceptions(self):
+        res = super()._get_under_validation_exceptions()
         res.append("state")
         res.append("substate_id")
         return res
