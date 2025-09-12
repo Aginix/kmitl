c5c6eb8d2ea10cf65f5d53b53e775c0f937abad6
Tanathip Singhanon <88467996+madara1150@users.noreply.github.com>
2025-09-12 20:37:07 +0700

    [16.0][ADD] hr_department_short_name (#159)

--- hr_department_short_name/__manifest__.py
+++ hr_department_short_name/__manifest__.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Account Analytic Short Name KMITL',
+    'name': 'Hr Department Short Name',
     'version': '16.0.1.0.0',
-    'summary': """ Account Analytic Short Name KMITL Summary """,
+    'summary': """ Hr Department Short Name Summary """,
     "category": "KMITL",
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
-    'depends': ['account_analytic_kmitl'],
+    'depends': ['hr'],
     "data": [
-        "data/account.analytic.account.csv",
-        "views/account_analytic_account_views.xml"
+        "views/hr_department.xml"
     ],
     'installable': True,
     'auto_install': False,
