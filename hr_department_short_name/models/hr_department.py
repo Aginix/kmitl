69bce8d55fe7f461c00ca226292e3cd482a6d2f0
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-12 18:03:18 +0700

    [16.0][ADD] hr_department_short_name

--- hr_department_short_name/models/hr_department.py
+++ hr_department_short_name/models/hr_department.py
@@ -3,7 +3,7 @@ from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
 
 
-class AccountAnalyticAccount(models.Model):
-    _inherit = 'account.analytic.account'
+class HrDepartment(models.Model):
+    _inherit = 'hr.department'
 
     short_name = fields.Char(string='Short Name')
