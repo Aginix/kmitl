0e7e1e5caaf0b76082a14725c7ad3423e0fa8d70
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-14 12:52:12 +0700

    [16.0][ADD] purchase_request_operating_unit_department

---
+++ purchase_request_operating_unit_department/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Request Operating Unit Department',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Request Operating Unit Department Summary """,
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    'depends': ['purchase_request_operating_unit', 'operating_unit_base_department'],
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
