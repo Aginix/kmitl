dd29a423e9a7ad5992127f151460a5f3013a44f2
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-15 12:13:45 +0700

    add : technical

--- purchase_request_operating_unit_department/__manifest__.py
+++ purchase_request_operating_unit_department/__manifest__.py
@@ -6,9 +6,9 @@
     "category": "KMITL",
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
-    'depends': ['purchase_request_operating_unit', 'operating_unit_base_department'],
-    'data': [
-
+    'depends': ['purchase_request_operating_unit', 'operating_unit_base_department', 'purchase_request_department'],
+    "data": [
+        "views/purchase_request_views.xml"
     ],
     'installable': True,
     'auto_install': False,
