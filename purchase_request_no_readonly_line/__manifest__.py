b64dbfb8e6f0e7499d46ee879bf4b961a5d2944b
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-17 12:59:16 +0700

    add: purchase_request_no_readonly_line

---
+++ purchase_request_no_readonly_line/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Request No Readonly Line',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Request No Readonly Line Summary """,
+    'author': '',
+    'website': '',
+    'category': '',
+    'depends': ['purchase_request'],
+    "data": [
+        "views/purchase_request_views.xml"
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
