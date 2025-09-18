9695cad451cfaa19a00d78c407f1d6a9c8db83a2
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-09-18 20:03:40 +0700

    [ADD] purchase_work_acceptance_purchase_order

---
+++ purchase_work_acceptance_purchase_order/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Work Acceptance Purchase Order',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Work Acceptance Purchase Order Summary """,
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    'depends': ['l10n_th_gov_work_acceptance'],
+    "data": [
+        "views/work_acceptance_views.xml"
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
