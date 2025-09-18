419305613028172e9830dbbe9dd94b35bcd03697
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-18 18:16:05 +0700

    fix

--- purchase_request_egp_kmitl/models/purchase_request_line_make_purchase_order.py
+++ purchase_request_egp_kmitl/models/purchase_request_line_make_purchase_order.py
@@ -7,31 +7,22 @@ class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
     _inherit = 'purchase.request.line.make.purchase.order'
 
     def make_purchase_order(self):
-        purchase_obj = self.env["purchase.order"]
-        purchase = False
-
-        for item in self.item_ids:
-            if self.purchase_order_id:
-                purchase = self.purchase_order_id
-            if not purchase:
-                po_data = self._prepare_purchase_order(
-                    line.request_id.picking_type_id,
-                    line.request_id.group_id,
-                    line.company_id,
-                    line.origin,
-                    self.is_egp
-                )
-                purchase = purchase_obj.create(po_data)
-
         res = super().make_purchase_order()
-        purchase_requests = self.item_ids.mapped("request_id")
-        purchase_requests.action_del_egp_status()
-        return res
+        res_id = res['domain'][0][2].pop()
+        return {
+            "name": _("Purchase Order"),
+            "type": "ir.actions.act_window",
+            "res_model": "purchase.order",
+            "view_mode": "form",
+            "res_id": res_id,
+            "view_id": False,
+            "context": False,
+        }
 
     @api.model
-    def _prepare_purchase_order(self, picking_type, group_id, company, origin, egp=False):
+    def _prepare_purchase_order(self, picking_type, group_id, company, origin):
         data = super()._prepare_purchase_order(picking_type, group_id, company, origin)
-        if egp:
+        purchase_request = self.item_ids.mapped('request_id')
+        if purchase_request.is_egp:
             data["state"] = "purchase"
-
         return data
