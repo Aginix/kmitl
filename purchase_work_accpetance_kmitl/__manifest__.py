74cf146eb4f6d0b66b36649e57f9448433117eba
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-09-18 16:04:22 +0700

    custom view + add more th po

--- purchase_work_accpetance_kmitl/__manifest__.py
+++ purchase_work_accpetance_kmitl/__manifest__.py
@@ -7,8 +7,8 @@
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
     'depends': ['l10n_th_gov_work_acceptance'],
-    'data': [
-        
+    "data": [
+        "views/work_acceptance_views.xml"
     ],
     'installable': True,
     'auto_install': False,
