91e664ecc8d69f34db47ddb3d85c098e48a19068
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-18 20:30:02 +0700

    fix

--- purchase_request_egp_kmitl/models/purchase_request_line_make_purchase_order.py
+++ purchase_request_egp_kmitl/models/purchase_request_line_make_purchase_order.py
@@ -9,6 +9,8 @@ class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
     def make_purchase_order(self):
         res = super().make_purchase_order()
         res_id = res['domain'][0][2].pop()
+        purchase_requests = self.item_ids.mapped("request_id")
+        purchase_requests.action_del_egp_status()
         return {
             "name": _("Purchase Order"),
             "type": "ir.actions.act_window",
