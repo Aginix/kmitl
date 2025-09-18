8812794b2610ad106b7cdb9df0d9b240a9560af4
Tanathip Singhanon <88467996+madara1150@users.noreply.github.com>
2025-09-18 21:04:15 +0700

    [16.0][FIX] egp create po (#184)

--- purchase_request_egp_kmitl/models/purchase_request_line_make_purchase_order.py
+++ purchase_request_egp_kmitl/models/purchase_request_line_make_purchase_order.py
@@ -8,6 +8,23 @@ class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
 
     def make_purchase_order(self):
         res = super().make_purchase_order()
+        res_id = res['domain'][0][2].pop()
         purchase_requests = self.item_ids.mapped("request_id")
         purchase_requests.action_del_egp_status()
-        return res
+        return {
+            "name": _("Purchase Order"),
+            "type": "ir.actions.act_window",
+            "res_model": "purchase.order",
+            "view_mode": "form",
+            "res_id": res_id,
+            "view_id": False,
+            "context": False,
+        }
+
+    @api.model
+    def _prepare_purchase_order(self, picking_type, group_id, company, origin):
+        data = super()._prepare_purchase_order(picking_type, group_id, company, origin)
+        purchase_request = self.item_ids.mapped('request_id')
+        if purchase_request.is_egp:
+            data["state"] = "purchase"
+        return data
