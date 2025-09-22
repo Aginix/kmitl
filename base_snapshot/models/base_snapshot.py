02c0c71bc41e526d4fa75a2cd0ece10c3f9d465c
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-22 15:30:29 +0700

    add field

--- base_snapshot/models/base_snapshot.py
+++ base_snapshot/models/base_snapshot.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from odoo import models, fields, api, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
 
 _logger = logging.getLogger(__name__)
@@ -9,6 +9,23 @@ _logger = logging.getLogger(__name__)
 
 class BaseSnapshot(models.Model):
     _name = 'base.snapshot'
-    _description = 'BaseSnapshot'
+    _description = 'Base Snapshot'
 
-    name = fields.Char('Name')
+    current_snapshot_id = fields.Many2one(
+        comodel_name="base.snapshot",
+        string="Current snapshot",
+        readonly=True,
+        copy=True,
+    )
+    old_snapshot_ids = fields.One2many(
+        comodel_name="base.snapshot",
+        inverse_name="current_revision_id",
+        string="Old snapshots",
+        readonly=True,
+        domain=["|", ("active", "=", False), ("active", "=", True)],
+        context={"active_test": False},
+    )
+    snapshot_number = fields.Integer(string="Snapshot", copy=False, default=0)
+    unsnapshoted_name = fields.Char(
+        string="Original Reference", copy=True, readonly=True
+    )
