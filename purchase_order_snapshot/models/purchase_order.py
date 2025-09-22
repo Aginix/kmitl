a2d46c87c1f23f86e76904caa445ec27e04ef13b
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-22 17:54:37 +0700

    add PO snapshot

--- purchase_order_snapshot/models/purchase_order.py
+++ purchase_order_snapshot/models/purchase_order.py
@@ -8,6 +8,7 @@ _logger = logging.getLogger(__name__)
 
 
 class PurchaseOrder(models.Model):
+    _name = "purchase.order"
     _inherit = ['purchase.order', 'base.snapshot']
 
     def _get_snapshot_copy_fields(self):
