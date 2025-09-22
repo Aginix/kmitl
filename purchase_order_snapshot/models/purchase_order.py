2fd5742e23ea47f0c395b76986ab78d69a13ef13
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-22 21:14:55 +0700

    fix snapshot

--- purchase_order_snapshot/models/purchase_order.py
+++ purchase_order_snapshot/models/purchase_order.py
@@ -11,24 +11,26 @@ class PurchaseOrder(models.Model):
     _name = "purchase.order"
     _inherit = ['purchase.order', 'base.snapshot']
 
+    current_snapshot_id = fields.Many2one(
+        comodel_name="purchase.order",
+    )
+    old_snapshot_ids = fields.One2many(
+        comodel_name="purchase.order",
+    )
+
+    # ต้องการให้ copy fields ไหนใส่ในนี้
     def _get_snapshot_copy_fields(self):
         res = super()._get_snapshot_copy_fields()
         res.update({
-            "partner_id": self.partner_id.id,
+            "partner_ref": self.partner_ref,
             "currency_id": self.currency_id.id,
         })
         return res
 
-    # def _compute_snapshot_count(self):
-    #     for order in self:
-    #         order.snapshot_count = len(order.snapshot_ids)
-
-    # def action_create_snapshot(self):
-    #     for order in self:
-    #         snapshot = order.copy({
-    #             "snapshot_of_id": order.id,   # เก็บ reference ว่ามาจาก PO ไหน
-    #         })
-    #     return True
+    def action_create_snapshot(self):
+        for order in self:
+            snapshot = order.copy()
+        return True
 
     def action_view_snapshots(self):
         return {
@@ -36,6 +38,5 @@ class PurchaseOrder(models.Model):
             "name": "Snapshots",
             "res_model": "purchase.order",
             "view_mode": "tree,form",
-            # "domain": [("snapshot_of_id", "=", self.id)],
-            "context": {"default_current_revision_id": self.id},
+            "context": {"default_current_snapshot_id": self.id},
         }
