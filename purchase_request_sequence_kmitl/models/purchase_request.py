a92565b311ba08e9affb2062b3ca5e503a9ab9eb
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-24 16:35:02 +0700

    fix: warning

--- purchase_request_sequence_kmitl/models/purchase_request.py
+++ purchase_request_sequence_kmitl/models/purchase_request.py
@@ -6,32 +6,38 @@ from odoo.exceptions import UserError, ValidationError
 class PurchaseRequest(models.Model):
     _inherit = 'purchase.request'
 
-    @api.model
-    def create(self, vals):
-        if vals.get("operating_unit_id"):
-            ou = self.env["operating.unit"].browse(vals["operating_unit_id"])
-            if ou.department_id:
-                vals["department_id"] = ou.department_id.id
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
 
-        fy_id = self.env["account.fiscal.year"].browse(vals.get("date_range_fy_id"))
-        fiscal_year = fy_id.name[-2:] if fy_id else fields.Date.today().strftime("%y")
+            if vals.get("operating_unit_id"):
+                ou = self.env["operating.unit"].browse(vals["operating_unit_id"])
+                if ou.department_id:
+                    vals["department_id"] = ou.department_id.id
 
-        department = self.env["hr.department"].browse(vals.get("department_id"))
-        short_name = department.short_name or "XXX"
+            fy_id = self.env["account.fiscal.year"].browse(vals.get("date_range_fy_id"))
+            fiscal_year = fy_id.name[-2:] if fy_id else fields.Date.today().strftime("%y")
 
-        if not self.env['ir.sequence'].search([('code', 'like', f"purchase.request.{fiscal_year}.{short_name}")]):
-            self.env['ir.sequence'].create({
-                'name': f'Purchase Request {fiscal_year} {short_name}',
-                'code': f'purchase.request.{fiscal_year}.{short_name}',
-                'prefix': f'PR/{fiscal_year}/{short_name}/',
-                'padding': 4,
-                'number_increment': 1,
-            })
+            department = self.env["hr.department"].browse(vals.get("department_id"))
+            short_name = department.short_name or "XXX"
 
-        sequence = self.env['ir.sequence'].next_by_code(f"purchase.request.{fiscal_year}.{short_name}") or _('New')
-        vals['name'] = sequence
-        record = super().create(vals)
+            seq_code = f"purchase.request.{fiscal_year}.{short_name}"
 
-        if record.operating_unit_id and record.operating_unit_id.department_id:
-            record.department_id = record.operating_unit_id.department_id.id
-        return record
+            if not self.env['ir.sequence'].search([('code', '=', seq_code)], limit=1):
+                self.env['ir.sequence'].create({
+                    'name': f'Purchase Request {fiscal_year} {short_name}',
+                    'code': seq_code,
+                    'prefix': f'PR/{fiscal_year}/{short_name}/',
+                    'padding': 4,
+                    'number_increment': 1,
+                })
+
+            vals['name'] = self.env['ir.sequence'].next_by_code(seq_code) or _('New')
+
+        records = super().create(vals_list)
+
+        for rec in records:
+            if rec.operating_unit_id and rec.operating_unit_id.department_id:
+                rec.department_id = rec.operating_unit_id.department_id.id
+
+        return records
