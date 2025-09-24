273819cf76e4058732113361677ef4594a3dbcda
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-09-24 14:40:16 +0700

    [16.0][ADD] asset_management_usability_kmitl

---
+++ asset_management_usability_kmitl/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Asset Management Usability Kmitl',
+    'version': '16.0.1.0.0',
+    'summary': """ Asset Management Usability Kmitl Summary """,
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    'depends': ['account_asset_management'],
+    "data": [
+        "views/menuitem.xml"
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
