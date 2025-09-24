747ce38e72591c4aa0e7b8034991095a3251e97b
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-09-24 15:40:33 +0700

    [16.0][IMP] purchase_work_accpetance_kmitl: auto tier

---
+++ purchase_work_accpetance_kmitl/models/work_acceptance.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+class WorkAcceptance(models.Model):
+    _inherit = 'work.acceptance'
+
+    def _validate_tier(self, tiers=False):
+        super()._validate_tier(tiers)
+        reviews = self.review_ids.filtered(
+            lambda r: r.status == "pending" and (self.env.user in r.reviewer_ids)
+        )
+        if not reviews:
+            return self.write({'state': 'accept'})
