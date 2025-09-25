faec5e15638fac6ba8cddbf7e712c4c11e359389
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-25 18:01:00 +0700

    add : template wa

--- purchase_work_accpetance_portal/controllers/work_acceptance.py
+++ purchase_work_accpetance_portal/controllers/work_acceptance.py
@@ -17,4 +17,17 @@ class WorkAcceptance(http.Controller):
         values = {
             'work_acceptance': work_acceptance,
         }
-        return request.render("purchase_work_accpetance_portal.purchase_portal_template", values)
+        return request.render("purchase_work_accpetance_portal.work_acceptance_portal_template", values)
+
+    # @http.route(['/wa/portal/<int:wa_id>'],
+    #             type='http', auth="public", website=True)
+    # def work_acceptance_portal_all(self, wa_id, **kw):
+    #     work_acceptance = request.env['work.acceptance'].sudo().browse(wa_id)
+
+    #     if not work_acceptance.exists():
+    #         return request.not_found()
+
+    #     values = {
+    #         'work_acceptance': work_acceptance,
+    #     }
+    #     return request.render("purchase_work_accpetance_portal.work_acceptance_portal_template", values)
