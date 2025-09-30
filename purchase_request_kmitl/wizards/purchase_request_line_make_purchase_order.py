5eb3da34d8f46334c48a6f32c039d1cf14afdd19
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-30 22:51:35 +0700

    update

--- purchase_request_kmitl/wizards/purchase_request_line_make_purchase_order.py
+++ purchase_request_kmitl/wizards/purchase_request_line_make_purchase_order.py
@@ -14,8 +14,8 @@ class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
     def _prepare_purchase_order(self, picking_type, group_id, company, origin):
         vals = super()._prepare_purchase_order(picking_type, group_id, company, origin)
         vals.update({
-            "operating_unit_id": self.purchase_order_id.operating_unit_id,
-            "date_range_fy_id": self.purchase_order_id.date_range_fy_id
+            "operating_unit_id": self.item_ids.request_id.operating_unit_id,
+            "date_range_fy_id": self.item_ids.request_id.date_range_fy_id
         })
         return vals
 
