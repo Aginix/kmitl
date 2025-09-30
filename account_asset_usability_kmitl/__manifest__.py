1ca8320378edbc34ca4040dcddf9e35a29736739
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-09-30 15:32:23 +0700

    imp menu action

--- account_asset_usability_kmitl/__manifest__.py
+++ account_asset_usability_kmitl/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Asset Management Usability Kmitl',
+    'name': 'Account Asset Usability Kmitl',
     'version': '16.0.1.0.0',
-    'summary': """ Asset Management Usability Kmitl Summary """,
+    'summary': """ Account Asset Usability Kmitl Summary """,
     "category": "KMITL",
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
-    'depends': ['account_asset_management'],
-    "data": [
+    'depends': ['l10n_th_gov_account_asset_management'],
+    'data': [
         "views/menuitem.xml"
     ],
     'installable': True,
