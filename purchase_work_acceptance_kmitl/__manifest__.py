57d14dd6ef1bac29e4aa9dc207c5ebd4ec517be5
sunny <88508479+PiyaChaiyanit@users.noreply.github.com>
2025-09-30 14:26:32 +0700

    [16.0][FIX] purchase_work_acceptance_kmitl: fix typo (#204)

--- purchase_work_acceptance_kmitl/__manifest__.py
+++ purchase_work_acceptance_kmitl/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Purchase Work Accpetance Kmitl',
+    'name': 'Purchase Work Acceptance Kmitl',
     'version': '16.0.1.0.0',
-    'summary': """ Purchase Work Accpetance Kmitl Summary """,
+    'summary': """ Purchase Work Acceptance Kmitl Summary """,
     "category": "KMITL",
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
