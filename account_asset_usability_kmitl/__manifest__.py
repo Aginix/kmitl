c5974b3183c26bc53fecc139762f16aca7493029
sunny <88508479+PiyaChaiyanit@users.noreply.github.com>
2025-09-30 17:30:53 +0700

    [16.0][ADD] account_asset_usability_kmitl (#205)

---
+++ account_asset_usability_kmitl/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Account Asset Usability Kmitl',
+    'version': '16.0.1.0.0',
+    'summary': """ Account Asset Usability Kmitl Summary """,
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    'depends': ['l10n_th_gov_account_asset_management'],
+    'data': [
+        "views/menuitem.xml"
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
