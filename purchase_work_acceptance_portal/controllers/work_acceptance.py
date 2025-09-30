4010c6d12f184aa29e95bc19e75de13ca5e8278c
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-09-30 12:12:55 +0700

    refactor code

--- purchase_work_acceptance_portal/controllers/work_acceptance.py
+++ purchase_work_acceptance_portal/controllers/work_acceptance.py
@@ -37,86 +37,65 @@ class WorkAcceptance(http.Controller):
 
     @http.route(['/wa/view/<int:committee_id>/accept'], type='http', auth="public", methods=['POST'], website=True, csrf=False)
     def work_acceptance_committee_confirm(self, committee_id=None, access_token=None, **post):
+
         committee = request.env['work.acceptance.committee'].sudo().browse(int(committee_id))
         if not committee.exists() or access_token != committee.access_token:
             return request.not_found()
-        user = committee.employee_id.user_id.id
-        user2 = committee.employee_id.user_id
-        if not user:
+
+        committee_user = committee.employee_id.user_id
+        if not committee_user:
             return request.not_found()
-        env = request.env(user=user)
-        wa_id = committee.wa_id.id
-        wa = env['work.acceptance'].browse(wa_id)
-        comment = post.get("comment") or "อนุมัติ"
-        reviews = wa.review_ids.filtered(
-            lambda r: r.status == "pending" and (user2 in r.reviewer_ids)
+
+        work_acceptance = request.env(user=committee_user.id)['work.acceptance'].browse(committee.wa_id.id)
+
+        comment_text = post.get("comment") or "อนุมัติ"
+
+        pending_reviews = work_acceptance.review_ids.filtered(
+            lambda review: review.status == "pending" and (committee_user in review.reviewer_ids)
         )
-        if reviews:
-            reviews.write({"comment": comment})
-            wa.with_user(user2)._validate_tier(reviews)
-            wa._update_counter({"review_deleted": True})
-        ctx = {
-            "active_id": wa.id,
-            "active_model": "work.acceptance",
-            "validate_as_not_accept": False,
-            "comment": "ทดสอบๆ"
-        }
 
-        # wa.with_context(**ctx).validate_tier()
+        if pending_reviews:
+            pending_reviews.write({"comment": comment_text})
+            work_acceptance.with_user(committee_user)._validate_tier(pending_reviews)
+            work_acceptance._update_counter({"review_deleted": True})
 
-        # wa.with_user(user).with_context(**ctx).validate_tier()
+        return request.render(
+            "purchase_work_acceptance_portal.work_acceptance_portal_template",
+            {
+                'work_acceptance': committee.wa_id,
+                'committee': committee,
+            }
+        )
 
-
-        return request.render("purchase_work_acceptance_portal.work_acceptance_portal_template", {
-            'work_acceptance': committee.wa_id,
-            'committee': committee,
-        })
-
-    @http.route(['/wa/view/reject'], type='http', auth="public", methods=['POST'], website=True, csrf=False)
+    @http.route(['/wa/view/<int:committee_id>/reject'], type='http', auth="public", methods=['POST'], website=True, csrf=False)
     def work_acceptance_committee_reject(self, committee_id=None, access_token=None, **post):
         committee = request.env['work.acceptance.committee'].sudo().browse(int(committee_id))
         if not committee.exists() or access_token != committee.access_token:
             return request.not_found()
 
-        wa = committee.wa_id
-        user = committee.employee_id.user_id
-        if not user:
+        committee_user = committee.employee_id.user_id
+        if not committee_user:
             return request.not_found()
 
+        work_acceptance = request.env(user=committee_user.id)['work.acceptance'].browse(committee.wa_id.id)
 
-        ctx = {
-            "active_id": wa.id,
-            "active_model": "work.acceptance",
-            "validate_as_not_accept": True,
-            "default_comment": post.get('comment', 'ไม่รับเพราะ...'),
-        }
+        comment_text = post.get("comment") or "อนุมัติ"
 
-        wa.with_user(user).with_context(**ctx).validate_tier()
+        pending_reviews = work_acceptance.review_ids.filtered(
+            lambda review: review.status == "pending" and (committee_user in review.reviewer_ids)
+        )
 
-        return request.render("purchase_work_acceptance_portal.work_acceptance_portal_template", {
-            'work_acceptance': wa,
-            'committee': committee,
-        })
+        if pending_reviews:
+            pending_reviews.write({"comment": comment_text})
+            work_acceptance.with_user(committee_user)._validate_tier(pending_reviews)
+            work_acceptance._update_counter({"review_deleted": True})
+            committee.write({"status": "not_accept", "note": comment_text})
 
-        # <button
-        #                     name="validate_tier"
-        #                     string="Not Accept"
-        #                     attrs="{'invisible': [('can_review', '=', False)]}"
-        #                     type="object"
-        #                     class="btn-icon btn-danger"
-        #                     icon="fa-thumbs-down"
-        #                     context="{'validate_as_not_accept': True, 'default_comment': 'ไม่รับเพราะ...'}"
-        #                 />
+        return request.render(
+            "purchase_work_acceptance_portal.work_acceptance_portal_template",
+            {
+                'work_acceptance': committee.wa_id,
+                'committee': committee,
+            }
+        )
 
-    # @http.route(['/wa/portal/<int:wa_id>'],
-    #             type='http', auth="public", website=True)
-    # def work_acceptance_portal_all(self, wa_id, **kw):
-    #     work_acceptance = request.env['work.acceptance'].sudo().browse(wa_id)
-
-    #     if not work_acceptance.exists():
-    #         return request.not_found()
-
-    #     values = {
-    #         'work_acceptance': work_acceptance,
-    #     }
-    #     return request.render("purchase_work_acceptance_portal.work_acceptance_portal_template", values)
