caa2a0cf75f12385690c4b2dbb9aa3a35b6615b8
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-10-01 00:10:49 +0700

    update

--- purchase_work_acceptance_portal/models/work_acceptance.py
+++ purchase_work_acceptance_portal/models/work_acceptance.py
@@ -25,7 +25,7 @@ class WorkAcceptance(models.Model):
             if wa.work_acceptance_committee_ids:
                 for committee in wa.work_acceptance_committee_ids:
                     wa_url = committee.get_portal_link()
-                    message = f"กรุณาตรวจรับพัสดุที่มีชื่อว่า {wa.name} \n เอกสารสัญญา/ใบสั่งซื้อ/จ้าง : <a href='{order_url}'>คลิกที่นี่</a> \n เอกสารตรวจรับ : <a href='{wa_url}'>คลิกที่นี่</a>"
+                    message = f"กรุณาตรวจรับพัสดุที่มีชื่อว่า {wa.name} \n เอกสารสัญญา/ใบสั่งซื้อ/จ้าง : <a href='{order_url+'&committee_id='+str(committee.id)}'>คลิกที่นี่</a> \n เอกสารตรวจรับ : <a href='{wa_url}'>คลิกที่นี่</a>"
                     channel = self.env['mail.channel'].channel_get([committee.employee_id.user_id.partner_id.id])
                     channel_id = self.env['mail.channel'].browse(channel["id"])
                     channel_id.message_post(
