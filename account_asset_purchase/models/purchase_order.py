34bad7220dedf2aff6d0b0c68d2a3d694e6af635
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-10-01 16:06:05 +0700

    wip

--- account_asset_purchase/models/purchase_order.py
+++ account_asset_purchase/models/purchase_order.py
@@ -9,3 +9,12 @@ _logger = logging.getLogger(__name__)
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
+
+    asset_number = fields.Char(
+        string="Asset Number"
+    )
+
+    parent_asset = fields.Boolean(
+        string="Create Parent Asset",
+        help="ถ้าติ๊กถูกเมื่อลงทะเบียนครุภัณฑ์ จะสร้าง parent asset"
+    )
