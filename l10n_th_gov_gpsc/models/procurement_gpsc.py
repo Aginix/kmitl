ea670b7bbb32f4731317eb22ed1e1f4c63768ff3
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-10-02 16:12:14 +0700

    add unique check

--- l10n_th_gov_gpsc/models/procurement_gpsc.py
+++ l10n_th_gov_gpsc/models/procurement_gpsc.py
@@ -17,11 +17,18 @@ class ProcurementGpsc(models.Model):
     )
 
     code = fields.Char(
-        required=True, 
-        unique=True,
+        required=True,
         string="GPSC Code"
     )
 
     group_code = fields.Char(
         string="Group Code"
     )
+
+    @api.constrains('code')
+    def _check_code(self):
+        for rec in self:
+            domain = [('code', '=', rec.code)]
+            count = self.sudo().search_count(domain)
+            if count > 1:
+                raise ValidationError(_("Code should be unique"))
