ce7b044db38d49ae8f989410edf4b67057b737a2
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-10-03 11:18:56 +0700

    order by code & change unique method

--- l10n_th_gov_gpsc/models/procurement_gpsc.py
+++ l10n_th_gov_gpsc/models/procurement_gpsc.py
@@ -10,6 +10,10 @@ _logger = logging.getLogger(__name__)
 class ProcurementGpsc(models.Model):
     _name = 'procurement.gpsc'
     _description = 'ProcurementGpsc'
+    _order = 'code'
+    _sql_constraints = [
+        ('unique_code', 'unique (code)', "Code already exists!"),
+    ]
 
     name = fields.Char(
         required=True,
@@ -24,11 +28,3 @@ class ProcurementGpsc(models.Model):
     group_code = fields.Char(
         string="Group Code"
     )
-
-    @api.constrains('code')
-    def _check_code(self):
-        for rec in self:
-            domain = [('code', '=', rec.code)]
-            count = self.sudo().search_count(domain)
-            if count > 1:
-                raise ValidationError(_("Code should be unique"))
