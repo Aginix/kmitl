bbeb0feb59bc0c742563e12d9566575b790b4078
sunny <88508479+PiyaChaiyanit@users.noreply.github.com>
2025-10-03 15:52:11 +0700

    [16.0][ADD] l10n_th_gov_gpsc (#210)

---
+++ l10n_th_gov_gpsc/models/procurement_gpsc.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+class ProcurementGpsc(models.Model):
+    _name = 'procurement.gpsc'
+    _description = 'ProcurementGpsc'
+    _order = 'code'
+    _sql_constraints = [
+        ('unique_code', 'unique (code)', "Code already exists!"),
+    ]
+    _rec_names_search = ["name", "code"]
+
+    name = fields.Char(
+        required=True,
+        string="GPSC Name"
+    )
+
+    code = fields.Char(
+        required=True,
+        string="GPSC Code"
+    )
+
+    group_code = fields.Char(
+        string="Group Code"
+    )
+
+    def name_get(self):
+        result = []
+        for rec in self:
+            name = f"[{rec.code}] {rec.name}"
+            result.append((rec.id, name))
+        return result
