bfa516607b8369ebbe4fead9bafcde86e35ba257
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-10-05 22:06:51 +0700

    fix bug

--- account_asset_number_kmitl/models/account_asset.py
+++ account_asset_number_kmitl/models/account_asset.py
@@ -26,7 +26,7 @@ class AccountAsset(models.Model):
                 raise ValidationError(_("Missing department or GPSC."))
 
             fiscal_year = str(asset.account_fiscal_year_id.name)[-2:]
-            short_name = asset.department_id.department.short_name or "XXX"
+            short_name = asset.department_id.short_name or "XXX"
             seq_code = f"account.asset.{fiscal_year}.{short_name}.{asset.gpsc_id.code}"
             seq = self.env['ir.sequence'].sudo().search([('code', '=', seq_code)], limit=1)
 
