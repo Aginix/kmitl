2438752a4b3de61b2951febdda9e9b005eac7404
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-10-05 22:07:04 +0700

    asset number readonly

--- account_asset_number_kmitl/__manifest__.py
+++ account_asset_number_kmitl/__manifest__.py
@@ -7,8 +7,8 @@
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
     'depends': ['account_asset_kmitl'],
-    'data': [
-        
+    "data": [
+        "views/account_asset_views.xml"
     ],
     'installable': True,
     'auto_install': False,
