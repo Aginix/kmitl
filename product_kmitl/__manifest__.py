0ffba959acdfff199524dbd72572893107eb7523
PHATTARAPON.K <phattarapon.k@aginix.tech>
2025-10-05 22:59:12 +0700

    wip

--- product_kmitl/__manifest__.py
+++ product_kmitl/__manifest__.py
@@ -1,26 +1,22 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    "name": "KMITL Demo",
+    "name": "KMITL Product Data",
     "version": "16.0.0.0.0",
     "category": "KMITL",
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
     "depends": [
-        "base",
+        "stock",
         "account_kmitl",
-        "hr_department_code",
-        "account_fiscal_year",
-        "account_analytic_kmitl",
+        "product_category_code_unique",
     ],
-    "data": [
-        "data/company.xml",
-        "data/hr.department.csv",
-        "data/account.fiscal.year.csv",
-        "data/ir_config_parameter.xml",
+    "data": [               
+        "data/product_category.xml",
+        "data/product_template_storable.xml",
+        "data/product_template_consumable.xml",
+        "data/product_template_asset.xml",
     ],
-    "post_init_hook": "post_init",
-    "uninstall_hook": "uninstall_hook",
     "application": False,
     "installable": True,
     "auto_install": False,
