cc742e4f89833bb4cea8448640e3c32312c2ac3a
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-10-07 19:23:17 +0700

    [16.0][ADD] server action สร้างเลข asset

--- account_asset_number_kmitl/__manifest__.py
+++ account_asset_number_kmitl/__manifest__.py
@@ -8,7 +8,8 @@
     "website": "https://github.com/aginix/kmitl",
     'depends': ['account_asset_kmitl'],
     "data": [
-        "views/account_asset_views.xml"
+        "data/ir_actions_server_data.xml",
+        "views/account_asset_views.xml",
     ],
     'installable': True,
     'auto_install': False,
