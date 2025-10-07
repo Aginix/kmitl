c4d1d38e778148d935c92948b41d6e6334880338
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-10-07 15:00:04 +0700

    wip

--- account_asset_purchase/__manifest__.py
+++ account_asset_purchase/__manifest__.py
@@ -9,9 +9,7 @@
     'depends': ['account_asset_number_kmitl', 'purchase'],
     "data": [
         "security/ir.model.access.csv",
-        "views/account_asset_bartch_views.xml",
         "views/account_asset_batch_views.xml",
-        "views/purchase_asset_batch_line​_views.xml",
         "views/purchase_asset_batch_line_views.xml",
         "views/purchase_asset_line_views.xml",
         "views/purchase_order_views.xml"
