f5172e7edaf2bf037dbff9cb10e77118c1d38fde
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-10-07 21:29:51 +0700

    new version

--- account_asset_purchase/models/account_asset_batch_line.py
+++ account_asset_purchase/models/account_asset_batch_line.py
@@ -12,19 +12,10 @@ class AccountAssetBatchLine(models.Model):
     _inherit = "analytic.mixin"
     _description = 'AccountAssetBatchLine'
 
-    product_id = fields.Many2one(
-        "product.product",
-        string="Product",
-        required=True,
-        tracking=True,
-        domain=[],
-    )
-
     name = fields.Char(
-        string="Asset Name", 
-        compute="_compute_name", 
+        string="Name", 
         store=True,
-        tracking=True
+        tracking=True,
     )
 
     sequence = fields.Integer(
@@ -47,7 +38,7 @@ class AccountAssetBatchLine(models.Model):
 
     gpsc_id = fields.Many2one(
         "procurement.gpsc",
-        string="GPSC Number",
+        string="GPSC Id",
         required=True,
         tracking=True
     )
@@ -83,34 +74,10 @@ class AccountAssetBatchLine(models.Model):
 
     notes = fields.Text()
 
-    @api.depends("product_id")
-    def _compute_name(self):
-        for line in self:
-            line.name = line.product_id.display_name or ""
-
     @api.depends("amount", "price_per_unit")
     def _compute_amount_total(self):
         for rec in self:
             rec.amount_total = rec.amount * rec.price_per_unit
-
-    def _onchange_batch_id(self):
-        for line in self:
-            if line.batch_id and line.batch_id.purchase_id:
-                po_products = line.batch_id.purchase_id.order_line.mapped("product_id").ids
-                return {"domain": {"product_id": [("id", "in", po_products)]}}
-            return {"domain": {"product_id": []}}
-        
-    @api.onchange("product_id")
-    def _onchange_product_id(self):
-        for line in self:
-            if line.product_id and line.batch_id and line.batch_id.purchase_id:
-                po_line = self.env["purchase.order.line"].search([
-                    ("order_id", "=", line.batch_id.purchase_id.id),
-                    ("product_id", "=", line.product_id.id)
-                ], limit=1)
-                if po_line:
-                    line.price_per_unit = po_line.price_unit
-                    line.amount = po_line.product_qty
 
     def unlink(self):
         for line in self:
