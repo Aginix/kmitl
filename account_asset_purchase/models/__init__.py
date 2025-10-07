e9cb49bde834e77767df780f7f81834b78199425
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-10-07 15:00:04 +0700

    wip

--- account_asset_purchase/models/__init__.py
+++ account_asset_purchase/models/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-from . import purchase_order
-from . import purchase_asset_line
 from . import account_asset
-from . import account_asset_bartch
 from . import account_asset_batch
-from . import purchase_asset_batch_line​
+from . import purchase_asset_batch_line
+from . import purchase_order
