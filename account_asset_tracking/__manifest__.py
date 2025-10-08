2897bc54678552e4b9ec5abe77d2767e7ccd4dec
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-10-08 10:11:00 +0700

    wip

---
+++ account_asset_tracking/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Account Asset Tracking',
+    'version': '16.0.1.0.0',
+    'summary': """ Account Asset Tracking Summary """,
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    'depends': ['account_asset_number_kmitl'],
+    'data': [
+        
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
