bca4ab5dfc5578a8bf81be5b1c8031eb74deb1d0
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-10-08 18:00:52 +0700

    [16.0][ADD] product_budget

---
+++ product_budget/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Product Budget',
+    'version': '16.0.1.0.0',
+    'summary': """ Product Budget Summary """,
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "category": "KMITL",
+    'depends': ['base', 'web'],
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
