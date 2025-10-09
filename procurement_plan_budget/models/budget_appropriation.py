91881def172cae3db4035bc2de9c92f47a2a7980
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-10-09 10:24:47 +0700

    [16.0][FIX] budget_appropriation: remove unused procurement_plan_ids

--- procurement_plan_budget/models/budget_appropriation.py
+++ procurement_plan_budget/models/budget_appropriation.py
@@ -10,26 +10,10 @@ _logger = logging.getLogger(__name__)
 class BudgetAppropriation(models.Model):
     _inherit = "budget.appropriation"
 
-    procurement_plan_ids = fields.Many2many(
-        comodel_name="procurement.plan",
-        compute="_compute_procurement_plan_ids",
-        string="รายการแผนจัดซื้อจัดจ้าง",
-        store=False,
-    )
-
-    def _compute_procurement_plan_ids(self):
-        for record in self:
-            pass
-
     def action_review(self):
         super().action_review()
-        for line in self.line_ids:
-            if line.procurement_plan:
-                for procurement in line.procurement_plan_ids:
-                    procurement.action_validate()
 
     def action_post(self):
-        self._create_procurement_plan()
         super().action_post()
 
     def _create_procurement_plan(self):
@@ -44,7 +28,7 @@ class BudgetAppropriation(models.Model):
 
     def budget_move_line_vals(self):
         lines = super().budget_move_line_vals()
-        for line in self.line_ids:
-            if line.enable_procurement_plan and line.procurement_plan_id:
-                vals['procurement_plan_id'] = line.procurement_plan_id.id
+        # for line in self.line_ids:
+        #     if line.enable_procurement_plan and line.procurement_plan_id:
+        #         vals['procurement_plan_id'] = line.procurement_plan_id.id
         return lines
