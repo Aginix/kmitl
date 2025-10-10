e42eeb8943fef2c2bfb357d5414e96f1214afb39
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-10-10 18:03:42 +0700

    improve code

--- hr_department_operating_unit/models/hr_department.py
+++ hr_department_operating_unit/models/hr_department.py
@@ -14,4 +14,27 @@ class HrDepartment(models.Model):
         comodel_name="operating.unit",
         string="Operating Unit",
         default=lambda self: (self.env["res.users"].operating_unit_default_get()),
+        compute="_compute_operating_unit",
+        readonly=False,
+        store=True
     )
+
+    parent_id = fields.Many2one(
+        compute="_compute_operating_unit",
+        readonly=False,
+        store=True
+    )
+
+    @api.constrains('operating_unit_id', 'parent_id')
+    def _check_operating_unit_match_parent(self):
+        for record in self:
+            if record.parent_id and record.operating_unit_id != record.parent_id.operating_unit_id:
+                raise ValidationError(
+                    "The Operating Unit must match the Operating Unit of the parent department."
+                )
+
+    @api.depends('parent_id.operating_unit_id', 'parent_id')
+    def _compute_operating_unit(self):
+        for record in self:
+            if record.parent_id:
+                record.operating_unit_id = record.parent_id.operating_unit_id
