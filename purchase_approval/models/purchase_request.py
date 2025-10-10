be8e171b264c42490a5b6fb5807b8b051ee3003e
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-10-10 17:18:11 +0700

    checkpoint

--- purchase_approval/models/purchase_request.py
+++ purchase_approval/models/purchase_request.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
-import logging
-
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
-
-_logger = logging.getLogger(__name__)
 
 
 class PurchaseRequest(models.Model):
@@ -13,10 +9,6 @@ class PurchaseRequest(models.Model):
     is_required_approval = fields.Boolean(
         compute="_compute_is_required_approval", store=False, readonly=True
     )
-
-    # purchase_approval_count = fields.Integer(
-    #     string="PR2s count", compute="_compute_purchase_approval_count", readonly=True
-    # )
     report_id = fields.Many2one(
         'purchase.request.report', string='Request Report', ondelete='set null', index=True
     )
@@ -25,41 +17,6 @@ class PurchaseRequest(models.Model):
     def _compute_is_required_approval(self):
         for rec in self:
             rec.is_required_approval = rec.estimated_cost <= 100000
-
-    # def action_view_purchase_approval(self):
-    #     action = self.env["ir.actions.actions"]._for_xml_id("purchase_approval.action_purchase_approval")
-    #     lines = self.mapped("line_ids.purchase_lines.order_id")
-    #     if len(lines) > 1:
-    #         action["domain"] = [("id", "in", lines.ids)]
-    #     elif lines:
-    #         action["views"] = [
-    #             (self.env.ref("purchase_approval.view_purchase_approval_form").id, "form")
-    #         ]
-    #         action["res_id"] = lines.id
-    #     return action
-
-    # @api.depends("line_ids")
-    # def _compute_purchase_approval_count(self):
-    #     for rec in self:
-    #         rec.purchase_approval_count = len(rec.mapped("line_ids.purchase_lines.order_id").filtered("request_id"))
-
-    # def action_open_request_report(self):
-    #     self.ensure_one()
-    #     report = self.env['purchase.request.report'].create({
-    #             'name': f'Report for {self.name}',
-    #             'company_id': self.company_id.id,
-    #             'currency_id': self.currency_id.id if hasattr(self, 'currency_id') else self.env.company.currency_id.id,
-    #             'request_ids': [(4, self.id)],
-    #         })
-    #     self.report_id = report.id
-    #     return {
-    #         'type': 'ir.actions.act_window',
-    #         'name': _('Purchase Request Report'),
-    #         'res_model': 'purchase.request.report',
-    #         'res_id': report.id,
-    #         'view_mode': 'form',
-    #         'target': 'new',
-    #     }
 
     def action_open_request_report(self):
         """Create one Purchase Request Report from selected requests"""
@@ -81,9 +38,10 @@ class PurchaseRequest(models.Model):
         if len(currencies) > 1:
             raise UserError(_("Please select requests with the same currency."))
 
+        seq_name = self.env['ir.sequence'].next_by_code('purchase.order.approval')
         # สร้าง report เดียว
         report = self.env['purchase.request.report'].create({
-            'name': name,
+            'name': seq_name,
             'company_id': companies.id if companies else self.env.company.id,
             'currency_id': currencies.id if currencies else self.env.company.currency_id.id,
             'request_ids': [(6, 0, self.ids)],
