4240d077ee802eac84cb843d26f5ae014a999017
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-10-10 21:02:26 +0700

    [16.0][ADD] po_department

---
+++ purchase_order_department_operating_unit /__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Order Department Operating Unit ',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Order Department Operating Unit  Summary """,
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "category": "KMITL",
+    'depends': ['purchase_order_department', 'purchase_operating_unit'],
+    "data": [
+        "views/purchase_order_views.xml"
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
