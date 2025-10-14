fbf4ad6e1cd68f98637b1aafb3fc6ff2d9f2fa1c
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-10-14 10:11:27 +0700

    change related to compute

--- purchase_order_department_operating_unit/models/purchase_order.py
+++ purchase_order_department_operating_unit/models/purchase_order.py
@@ -11,5 +11,21 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     operating_unit_id = fields.Many2one(
-        related="department_id.operating_unit_id"
+        compute="_compute_operating_unit_id",
+        default=lambda self: self._default_operating_unit_id(),
+        store=True,
     )
+
+    def _default_operating_unit_id(self):
+        department_id = self.env.user.employee_id.department_id
+        if department_id and department_id.operating_unit_id:
+            return department_id.operating_unit_id.id
+        return self.env["res.users"].operating_unit_default_get()
+
+    @api.depends("department_id")
+    def _compute_operating_unit_id(self):
+        for rec in self:
+            if rec.department_id and rec.department_id.operating_unit_id:
+                rec.operating_unit_id = rec.department_id.operating_unit_id.id
+            else:
+                rec.operating_unit_id = False
