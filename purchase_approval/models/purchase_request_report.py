907bbd7210a6a760afa5b9f0a7f418e1a108accc
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-10-14 14:46:48 +0700

    เพิ่มเงื่อนไขในการเลือก request

--- purchase_approval/models/purchase_request_report.py
+++ purchase_approval/models/purchase_request_report.py
@@ -37,7 +37,7 @@ class PurchaseRequestReport(models.Model):
         'purchase_request_report_rel',
         'report_id', 'request_id',
         string='Requests',
-        domain="[('payment_type', '=', payment_type), ('state', '=', 'to_verify')]",
+        domain="[('payment_type', '=', payment_type), ('state', '=', 'to_verify'), ('report_id', '=', False)]",
     )
 
     def _compute_is_editable(self):
@@ -47,5 +47,9 @@ class PurchaseRequestReport(models.Model):
     def action_save_changes(self):
         for rec in self:
             rec.write({})
-
         return {'type': 'ir.actions.act_window_close'}
+
+    @api.onchange('payment_type')
+    def _onchange_payment_type_clear_requests(self):
+        if self.request_ids:
+            self.request_ids = [(5, 0, 0)]
