acfd86341e5ea68f3faf6910a92bb5d7ee590935
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-10-14 10:41:25 +0700

    ปิดปุ่มส่งคำขออนุมัติ

--- purchase_request_approval_kmitl/__manifest__.py
+++ purchase_request_approval_kmitl/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
     "category": "KMITL",
-    'depends': ['l10n_th_gov_purchase_request', 'purchase_request_budget', 'purchase_request_security', 'purchase_request_tier_validation'],
+    'depends': ['l10n_th_gov_purchase_request', 'purchase_request_budget', 'purchase_request_security', 'purchase_request_tier_validation' ,'purchase_request_egp_kmitl'],
     "data": [
         "data/substate.xml",
         "data/tier_definition.xml",
