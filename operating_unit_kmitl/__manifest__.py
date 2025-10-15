5ce511912b56d8f7448e7aa5f02284b82d08a768
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-10-15 17:13:35 +0700

    del department_id

---
+++ operating_unit_kmitl/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Operating Unit KMITL',
+    'version': '16.0.0.0.1',
+    'summary': """Operating Unit KMITL""",
+    'author': 'Aginix Technologies',
+    'website': 'https://github.com/Aginix/kmitl',
+    'category': 'KMITL/Budgeting',
+    'depends': ['operating_unit', 'operating_unit_base_department', 'kmitl_demo'],
+    'data': [
+        "data/operating_unit_data.xml",
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
