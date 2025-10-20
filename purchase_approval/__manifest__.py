ec24147bccddfcc58421275477c463abf217fdfa
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-10-20 13:04:56 +0700

    imp manifest

--- purchase_approval/__manifest__.py
+++ purchase_approval/__manifest__.py
@@ -5,7 +5,7 @@
     "category": "KMITL",
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
-    "depends": ["purchase_request_kmitl", "purchase_no_rfq", 'purchase_request_approval_kmitl'],
+    "depends": ["purchase_request_kmitl", "purchase_no_rfq"],
     "data": [
         "data/sequence.xml",
         "security/ir.model.access.csv",
