556b05512645e5a7e95fd27f309d9f8b69b8cb3d
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-10-21 16:23:41 +0700

    move exception

---
+++ purchase_request_line_mandatory_exception/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Request Line Mandatory Exception',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Request Line Mandatory Exception Summary """,
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    'depends': ['purchase_request_exception'],
+    'data': [
+        "data/purchase_request_line_exception.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
