bdb40c27cebddfda565732b4bdee623ccb8bb287
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-10-21 17:27:55 +0700

    [16.0][ADD] purchase_order_procurement_committee

---
+++ purchase_order_procurement_committee/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Order Procurement Committee',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Order Procurement Committee Summary """,
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "category": "KMITL",
+    'depends': ['l10n_th_gov_purchase_request'],
+    "data": [
+        "views/purchase_order_views.xml"
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
