3cbcbb446167a14d62885812bec2c28609691762
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-10-21 15:37:44 +0700

    Changes auto-committed by Conductor

--- budget_account_root/__manifest__.py
+++ budget_account_root/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "Budget Product",
+    "name": "Budget Account Root",
     "version": "16.0.1.0.0",
     "category": "KMITL/Budgeting",
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
-    "depends": ["budget", "budget_account_root", "product_category_code"],
-    "data": ["data/budget_account.xml", "views/budget_account_views.xml"],
+    "depends": ["budget"],
+    "data": [],
     "installable": True,
     "auto_install": False,
     "license": "LGPL-3",
