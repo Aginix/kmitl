ad386bb6976717a1d4fa279052295ec990330647
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-10-22 20:29:43 +0700

    [16.0][IMP] ข้อมูล hr_department ทำ tracking

---
+++ hr_department_short_name_tracking/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Hr Department Short Name Tracking',
+    'version': '16.0.1.0.0',
+    'summary': """ Hr Department Short Name Tracking Summary """,
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    'depends': ['hr.department'],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
