cefc2f36287dd7f7ac0a2e69e574da6aab37ccb6
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-10-24 16:07:57 +0700

    ทำให้เหมือน main

--- purchase_order_kmitl/models/purchase_order.py
+++ purchase_order_kmitl/models/purchase_order.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from odoo import _, api, fields, models
+from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 
 _logger = logging.getLogger(__name__)
@@ -19,11 +19,6 @@ class PurchaseOrder(models.Model):
     account_fiscal_year_id = fields.Many2one(
         comodel_name="account.fiscal.year",
         string="ปีงบประมาณ",
-        tracking=True,
-        states=READONLY_STATES,
-    )
-    contract_name = fields.Char(
-        string="ชื่อสัญญา/ใบสั่งซื้อ/จ้าง",
         tracking=True,
         states=READONLY_STATES,
     )
