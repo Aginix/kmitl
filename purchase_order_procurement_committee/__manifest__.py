3ea013cccef62b90cb296da2587f0d4d34a35ca2
sunny <88508479+PiyaChaiyanit@users.noreply.github.com>
2025-10-24 08:54:34 +0700

    [16.0][ADD] purchase_order_procurement_committee (#261)

---
+++ purchase_order_procurement_committee/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Order Procurement Committee',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Order Procurement Committee Summary """,
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "category": "KMITL",
+    'depends': ['purchase_request_kmitl', 'purchase'],
+    "data": [
+        "views/purchase_order_views.xml"
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
