a3ae46d91d4fbd14927f6adafe0f8222bbe32e9e
Tanathip Singhanon <88467996+madara1150@users.noreply.github.com>
2025-10-24 17:04:26 +0700

    update

--- purchase_order_contract_name/models/purchase_order.py
+++ purchase_order_contract_name/models/purchase_order.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
-import logging
-
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
-
-_logger = logging.getLogger(__name__)
 
 
 class PurchaseOrder(models.Model):
