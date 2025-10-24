09841afac8ce063936a7f023bb6140c5e2f27b97
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-10-24 17:27:27 +0700

    fix

--- purchase_order_contract_name/models/purchase_order.py
+++ purchase_order_contract_name/models/purchase_order.py
@@ -6,6 +6,12 @@ from odoo.exceptions import UserError, ValidationError
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
+    READONLY_STATES = {
+        'purchase': [('readonly', True)],
+        'done': [('readonly', True)],
+        'cancel': [('readonly', True)],
+    }
+
     contract_name = fields.Char(
         string="ชื่อสัญญา/ใบสั่งซื้อ/จ้าง",
         tracking=True,
