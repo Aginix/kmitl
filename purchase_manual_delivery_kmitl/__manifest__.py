6fa619bd397f6234879b48320c8f8a66ae45f5c9
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-10-27 14:20:24 +0700

    fix manifest

--- purchase_manual_delivery_kmitl/__manifest__.py
+++ purchase_manual_delivery_kmitl/__manifest__.py
@@ -8,7 +8,8 @@
     "category": "KMITL",
     'depends': ['purchase_manual_delivery'],
     'data': [
-        'wizards/create_manual_stock_picking.xml'
+        'wizards/create_manual_stock_picking.xml',
+        'views/stock_picking_views.xml',
     ],
     'installable': True,
     'auto_install': False,
