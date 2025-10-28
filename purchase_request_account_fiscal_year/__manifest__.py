4d57671170e714b20faf219abdb524b373e2a7a1
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-10-28 11:18:45 +0700

    [ADD] purchase_request_account_fiscal_year

---
+++ purchase_request_account_fiscal_year/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Purchase Request Account Fiscal Year",
+    "version": "16.0.1.0.0",
+    "category": "Purchase Management",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "depends": ["purchase_request", "account_fiscal_year"],
+    "data": ["views/purchase_request_views.xml"],
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
