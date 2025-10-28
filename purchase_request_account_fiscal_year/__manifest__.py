7e84a35a6d1c4c11b8d9d60f2f86da4a2dd1c9ce
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-10-28 11:39:24 +0700

    fix

--- purchase_request_account_fiscal_year/__manifest__.py
+++ purchase_request_account_fiscal_year/__manifest__.py
@@ -5,7 +5,7 @@
     "category": "Purchase Management",
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
-    "depends": ["purchase_request", "account_fiscal_year"],
+    "depends": ["l10n_th_gov_purchase_request", "account_fiscal_year"],
     "data": ["views/purchase_request_views.xml"],
     "installable": True,
     "auto_install": False,
