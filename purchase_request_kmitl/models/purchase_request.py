ae0f18d2f8156ad7eacd9b5e83b4af48fd15ff5c
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-10-28 11:22:39 +0700

    clean

--- purchase_request_kmitl/models/purchase_request.py
+++ purchase_request_kmitl/models/purchase_request.py
@@ -17,6 +17,7 @@ class PurchaseRequest(models.Model):
         domain=[("committee_type", "=", "tor_committee")],
         copy=True,
     )
+
     price_determine_committee_ids = fields.One2many(
         comodel_name="procurement.committee",
         inverse_name="request_id",
@@ -24,6 +25,7 @@ class PurchaseRequest(models.Model):
         domain=[("committee_type", "=", "price_determine")],
         copy=True,
     )
+
     evaluation_committee_ids = fields.One2many(
         comodel_name="procurement.committee",
         inverse_name="request_id",
@@ -31,42 +33,7 @@ class PurchaseRequest(models.Model):
         domain=[("committee_type", "=", "evaluation")],
         copy=True,
     )
-    title = fields.Char(
-        string="title",
-        required=True
-    )
-    description = fields.Text(string="reason", required=True)
-    payment_type = fields.Selection([
-        ("direct", "Direct paid"),
-        ("loan", "Loan"),
-        ("prepaid", "Prepaid")
-    ])
-    contract_type = fields.Selection([
-        ('order', 'Purchase/Hire Order'),
-        ('contract_buy', 'Contract buy'),
-        ('contract_construction', 'Contract construction'),
-    ], string="Contract type", required=True)
-    department_id = fields.Many2one(
-        comodel_name="hr.department",
-        string="Department",
-        related="requested_by.employee_ids.department_id",
-        store=True,
-        readonly=True,
-    )
-    account_fiscal_year_id = fields.Many2one(
-        comodel_name="account.fiscal.year",
-        string="ปีงบประมาณ",
-        required=True,
-        tracking=True,
-        readonly=False,
-    )
-    product_id = fields.Many2one(
-        comodel_name="product.product",
-        string="Product",
-        domain="[('purchase_request_ok', '=', True)]",
-        related=False,
-        readonly=False,
-    )
+
     hide_create_po_button = fields.Boolean(compute="_hide_create_po_button")
 
     @api.depends('state')
@@ -75,20 +42,3 @@ class PurchaseRequest(models.Model):
             rec.hide_create_po_button = True
             if rec.state in ('approved', 'in_progress') and rec.purchase_count == 0:
                 rec.hide_create_po_button = False
-
-    @api.onchange("product_id")
-    def _onchange_product_id_create_line(self):
-        product = self.product_id
-        if not product:
-            return
-
-        if self.line_ids:
-            for line in self.line_ids:
-                line.product_id = product
-        else:
-            self.line_ids = [Command.create({
-                'product_id': product,
-                'name': product.display_name,
-                'product_uom_id': product.uom_id.id,
-                'product_qty': 1.0,
-            })]
