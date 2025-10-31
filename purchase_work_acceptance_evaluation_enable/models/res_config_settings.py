8e8221d5c6ce3c61e373c0e45a6eaa9abe48d358
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-10-31 12:44:23 +0700

    fix : error

--- purchase_work_acceptance_evaluation_enable/models/res_config_settings.py
+++ purchase_work_acceptance_evaluation_enable/models/res_config_settings.py
@@ -3,7 +3,7 @@ from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
 
 
-class ResConfigSettings(models.Model):
+class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     group_enable_eval_on_wa = fields.Boolean(
