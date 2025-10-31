e4794fbe3653f5de77524cfd290047022b87e880
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-10-31 16:47:24 +0700

    add: purchase_order_move_request

---
+++ purchase_order_move_request/models/purchase_order.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError, ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    move_request_ids = fields.One2many(
+        comodel_name='account.move.request',
+        inverse_name='purchase_id',
+        string='Move Requests',
+    )
+    move_reaquest_count = fields.Integer(
+        string='Move Request Count',
+        compute='_compute_move_request_count',
+    )
+    @api.depends('move_request_ids')
+    def _compute_move_request_count(self):
+        for order in self:
+            order.move_reaquest_count = len(order.move_request_ids)
+
+    def action_move_request(self):
+        self.ensure_one()
+
+        line_vals = []
+        for line in self.order_line:
+            line_vals.append((0, 0, {
+                'product_id': line.product_id.id,
+                'name': line.name,
+                'quantity': line.product_qty,
+                'price_unit': line.price_unit,
+                'tax_ids': [(6, 0, line.taxes_id.ids)],
+            }))
+
+        move_request = self.env['account.move.request'].create({
+            'purchase_id': self.id,
+            'partner_id': self.partner_id.id,
+            'line_ids': line_vals,
+        })
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'account.move.request',
+            'view_mode': 'form',
+            'res_id': move_request.id,
+            'target': 'current',
+        }
+
+    def action_view_move_request(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Move Requests',
+            'res_model': 'account.move.request',
+            'view_mode': 'tree,form',
+            'domain': [('purchase_id', '=', self.id)],
+            'context': {'default_purchase_id': self.id},
+        }
