13141df16ee2822bbb1949e4411f03183ad9e252
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-11-03 15:50:13 +0700

    fix : แก้ไขและ refactor

--- purchase_work_acceptance_kmitl/models/work_acceptance.py
+++ purchase_work_acceptance_kmitl/models/work_acceptance.py
@@ -6,9 +6,5 @@ class WorkAcceptance(models.Model):
     _inherit = 'work.acceptance'
 
     evaluation_result_ids = fields.One2many(
-        comodel_name="work.acceptance.evaluation.result",
-        inverse_name="wa_id",
-        string="Evaluation Results",
-        default=lambda self: self._default_evaluation_result_ids(),
         groups="purchase_work_acceptance_evaluation.group_enable_eval_on_wa"
     )
