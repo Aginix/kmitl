e6fdff74f9c60a69b64c65b993259cc9359b5532
PHATTARAPON.K <phattarapon.k@aginix.tech>
2025-11-03 16:31:26 +0700

    [16.0][ADD] account_move_request_fiscal_year

---
+++ account_move_request_fiscal_year/models/account_move_request.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+
+class AccountMoveRequest(models.Model):
+    _inherit = "account.move.request"
+
+    account_fiscal_year_id = fields.Many2one(
+        comodel_name="account.fiscal.year",
+        string="Fiscal Year",
+    )
