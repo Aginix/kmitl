d7cd989566a2910276456c1ea91100b2fc1605ad
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-11-04 00:04:02 +0700

    seperate module

--- stock_scrap_hide_location_id/__manifest__.py
+++ stock_scrap_hide_location_id/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Stock Scrap Kmitl',
+    'name': 'Stock Scrap Hide Location Id',
     'version': '16.0.1.0.0',
-    'summary': """ Stock Scrap Kmitl Summary """,
+    'summary': """ Stock Scrap Hide Location Id Summary """,
     "category": "KMITL",
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
--- stock_scrap_responsible_user/models/stock_scrap.py
+++ stock_scrap_responsible_user/models/stock_scrap.py
@@ -10,19 +10,11 @@ _logger = logging.getLogger(__name__)
 class StockScrap(models.Model):
     _inherit = 'stock.scrap'
 
-    reason = fields.Text(
-        string="Reason",
-        states={'done': [('readonly', True)]}
-    )
-
     user_id = fields.Many2one(
         comodel_name='res.users',
         string='Created By',
         readonly=True,
         default=lambda self: self.env.user,
-        states={'done': [('readonly', True)]}
-    )
-
-    origin = fields.Char(
-        states={'done': [('readonly', True)]}
+        states={'done': [('readonly', True)]},
+        tracking=True
     )
