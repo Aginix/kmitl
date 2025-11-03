3a4b6dbdc646ea5cf0af4b4a35b815f2b28902bd
Champ Phattarapon <kkw.phattarapon@gmail.com>
2025-11-03 17:37:08 +0700

    [16.0][ADD] account_move_request_fiscal_year (#298)
    Co-authored-by: PHATTARAPON.K <phattarapon.k@aginix.tech>

---
+++ account_move_request_fiscal_year/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Account Move Request Fiscal Year",
+    "version": "16.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "author": "AGINIX",
+    "website": "https://github.com/aginix/kmitl",
+    "depends": [
+        "account_move_request",
+        "account_fiscal_year",
+    ],
+    "data": [
+        "views/account_move_request_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
