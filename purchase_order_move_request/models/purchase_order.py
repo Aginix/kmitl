dd00e5b8862c149c3abe7531f8611e77d55e0ed6
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-11-03 21:19:55 +0700

    fix : bug

--- purchase_order_move_request/models/purchase_order.py
+++ purchase_order_move_request/models/purchase_order.py
@@ -22,44 +22,41 @@ class PurchaseOrder(models.Model):
             order.move_request_count = len(order.move_request_ids)
 
     def _prepare_move_request_line_vals(self):
-		account = (
-			self.product_id.property_account_expense_id
-			or self.product_id.categ_id.property_account_expense_categ_id
-		)
-		return {
-			"product_id": self.product_id.id,
+        account = (
+            self.product_id.property_account_expense_id
+            or self.product_id.categ_id.property_account_expense_categ_id
+        )
+        return {
+            "product_id": self.product_id.id,
             "name": self.name,
             "quantity": self.product_qty,
             "price_unit": self.price_unit,
-            "account_id": account.id,
+            "account_id": account.id if account else False,
             "tax_ids": [Command.set(self.taxes_id.ids)],
             "analytic_distribution": self.analytic_distribution,
-		}
+        }
 
     def _prepare_move_request_vals(self):
         return {
-                'purchase_id': self.id,
-                'partner_id': self.partner_id.id,
-                'line_ids': [Command.create(line._prepare_move_request_line_vals()) for line in self.order_line],
-            }
+            "purchase_id": self.id,
+            "partner_id": self.partner_id.id,
+            "line_ids": [
+                Command.create(line._prepare_move_request_line_vals())
+                for line in self.order_line
+            ],
+        }
 
     def action_move_request(self):
         self.ensure_one()
-
-        line_vals = []
-        for line in self.order_line:
-            line_vals.append(
-                Command.create(line._prepare_move_request_line_vals())
-            )
-
-        move_request = self.env['account.move.request'].create(self._prepare_move_request_vals())
-
+        move_request = self.env["account.move.request"].create(
+            self._prepare_move_request_vals()
+        )
         return {
-            'type': 'ir.actions.act_window',
-            'res_model': 'account.move.request',
-            'view_mode': 'form',
-            'res_id': move_request.id,
-            'target': 'current',
+            "type": "ir.actions.act_window",
+            "res_model": "account.move.request",
+            "view_mode": "form",
+            "res_id": move_request.id,
+            "target": "current",
         }
 
     def action_view_move_request(self):
