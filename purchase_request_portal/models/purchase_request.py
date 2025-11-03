8f61cee654749ffbd63e03d8970e680e61bf0bf8
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-11-04 00:54:47 +0700

    fix

--- purchase_request_portal/models/purchase_request.py
+++ purchase_request_portal/models/purchase_request.py
@@ -11,21 +11,3 @@ class PurchaseRequest(models.Model):
         super()._compute_access_url()
         for request in self:
             request.access_url = f"/my/purchase_request/{request.id}"
-
-    def get_portal_url(self, suffix=None, report_type=None, **kwargs):
-        """
-        Get the portal URL for this purchase request.
-
-        Args:
-            report_type: 'html' or 'pdf' to generate report URL
-            suffix: Additional URL suffix
-        """
-        self.ensure_one()
-        url = self.access_url
-        if report_type:
-            url = f"{url}/{report_type}"
-        if suffix:
-            url = f"{url}{suffix}"
-        if kwargs.get("query_string"):
-            url = f"{url}?{kwargs['query_string']}"
-        return url
