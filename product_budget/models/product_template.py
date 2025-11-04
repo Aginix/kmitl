7aadf6cc33167dbc17eed4cb487a218ebe9a7e6c
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-11-04 18:00:23 +0700

    clean unused

--- product_budget/models/product_template.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import _, api, fields, models
-from odoo.exceptions import UserError, ValidationError
-
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    budget_account_id = fields.Many2one('budget.account')
