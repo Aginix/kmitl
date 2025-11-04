8c8a1933842139d57a77e9b6736a985d4700640c
Tanathip Singhanon <88467996+madara1150@users.noreply.github.com>
2025-11-04 14:22:58 +0700

    [16.0][IMP] ปรับกระบวนการสร้างใบขอเบิกใน PO แทนการสร้าง Bill (#290)

---
+++ purchase_order_move_request/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Order Move Request',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Order Move Request Summary """,
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "category": "KMITL",
+    'depends': ['purchase', 'account_move_request'],
+    "data": [
+        "views/purchase_order_views.xml"
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ purchase_order_move_request/models/purchase_order_line.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from odoo import Command, _, api, fields, models
+from odoo.exceptions import UserError, ValidationError
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    def _prepare_move_request_line_vals(self):
+        account = (
+            self.product_id.property_account_expense_id
+            or self.product_id.categ_id.property_account_expense_categ_id
+        )
+        return {
+            "product_id": self.product_id.id,
+            "name": self.name,
+            "quantity": self.product_qty,
+            "price_unit": self.price_unit,
+            "account_id": account.id if account else False,
+            "tax_ids": [Command.set(self.taxes_id.ids)],
+            "analytic_distribution": self.analytic_distribution,
+        }
