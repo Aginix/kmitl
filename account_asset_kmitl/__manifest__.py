7c4d5bacdb7acb4724bd1138f783a2b64093cb78
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-11-05 17:29:50 +0700

    fix depends

--- account_asset_kmitl/__manifest__.py
+++ account_asset_kmitl/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "KMITL",
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
-    'depends': ['l10n_th_gov_account_asset_management', 'account_asset_operating_unit', 'account_fiscal_year', 'operating_unit_base_department', 'l10n_th_gov_gpsc', 'account_kmitl'],
+    'depends': ['l10n_th_gov_account_asset_management', 'account_asset_operating_unit', 'account_fiscal_year', 'l10n_th_gov_gpsc', 'account_kmitl'],
     "data": [
         "data/account_asset_profile.xml",
         "views/account_asset_views.xml",
