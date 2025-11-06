7b99d4861f1075370cfa1d821a1b9c546671c8a1
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-11-06 22:25:23 +0700

    filter product type

--- account_asset_purchase/models/purchase_order.py
+++ account_asset_purchase/models/purchase_order.py
@@ -15,11 +15,25 @@ class PurchaseOrder(models.Model):
         compute="_compute_asset_count",
     )
 
+    has_storable_product = fields.Boolean(
+        string="Has Storable Product",
+        compute="_compute_has_storable_product",
+        store=False,
+    )
+
     def _compute_asset_count(self):
         for po in self:
             po.asset_count = self.env['account.asset.batch'].search_count([
                 ('purchase_id', '=', po.id)
             ])
+
+    @api.depends('order_line.product_id.type')
+    def _compute_has_storable_product(self):
+        for po in self:
+            po.has_storable_product = any(
+                line.product_id.type == 'product' 
+                for line in po.order_line
+            )
 
     def action_open_asset_batch(self):    
         self.ensure_one()
