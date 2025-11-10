db61501ebcf198a19c24d0e63387dcbdf60892fd
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-11-10 11:44:41 +0700

    new module

--- purchase_manual_delivery_security/__manifest__.py
+++ purchase_manual_delivery_security/__manifest__.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Purchase Manual Delivery Kmitl',
+    'name': 'Purchase Manual Delivery Security',
     'version': '16.0.1.0.0',
-    'summary': """ Purchase Manual Delivery Kmitl Summary """,
+    'summary': """ Purchase Manual Delivery Security Summary """,
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
     "category": "KMITL",
     'depends': ['purchase_manual_delivery'],
     "data": [
-        "data/settings.xml",
+        "security/security.xml",
         "views/purchase_order_views.xml",
-        "views/stock_picking_views.xml",
-        "wizards/create_manual_stock_picking.xml"
     ],
     'installable': True,
     'auto_install': False,
