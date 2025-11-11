902572d82f01b040dd202d27957a498f483b3dee
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-11-11 12:19:25 +0700

    fix tier

--- stock_request_tier_validation_custom/models/tier_definition.py
+++ stock_request_tier_validation_custom/models/tier_definition.py
@@ -12,6 +12,6 @@ class TierDefinition(models.Model):
 
     @api.model
     def _get_tier_validation_model_names(self):
-        res = super()._get_tier_validation_model_names()
-        res.extend(("stock.request"))
+        res = super(TierDefinition, self)._get_tier_validation_model_names()
+        res.append("stock.request")
         return res
