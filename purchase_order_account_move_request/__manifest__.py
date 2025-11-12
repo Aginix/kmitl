4d20406a9d56265ae3abd53e755bfbe3c4082db9
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-11-12 17:37:45 +0700

    test

--- purchase_order_account_move_request/__manifest__.py
+++ purchase_order_account_move_request/__manifest__.py
@@ -8,6 +8,7 @@
     "category": "KMITL",
     'depends': ['purchase', 'account_move_request'],
     "data": [
+        "views/account_move_request_views.xml",
         "views/purchase_order_views.xml"
     ],
     'installable': True,
