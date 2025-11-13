d895bf41915ceac2a4516df51cf24fe52d41a67a
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-11-13 16:18:07 +0700

    fix

--- purchase_order_payment_type/models/purchase_order.py
+++ purchase_order_payment_type/models/purchase_order.py
@@ -10,8 +10,15 @@ _logger = logging.getLogger(__name__)
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
+    READONLY_STATES = {
+        'purchase': [('readonly', True)],
+        'done': [('readonly', True)],
+        'cancel': [('readonly', True)],
+    }
+
     payment_type = fields.Selection(
         [("direct", "Direct paid"), ("loan", "Loan"), ("prepaid", "Prepaid")],
         tracking=True,
         string="Payment Type",
+        states=READONLY_STATES,
     )
