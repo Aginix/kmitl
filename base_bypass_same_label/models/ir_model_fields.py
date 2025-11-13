0320033aba28505fe055ac153ca1904dd4ce1b89
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-11-13 16:51:09 +0700

    add : base bypass

--- base_bypass_same_label/models/ir_model_fields.py
+++ base_bypass_same_label/models/ir_model_fields.py
@@ -6,7 +6,7 @@ from odoo.tools import unique
 from odoo.addons.base.models import ir_model
 
 select_en = ir_model.select_en
-
+upsert_en = ir_model.upsert_en
 
 class IrModelFields(models.Model):
     _inherit = 'ir.model.fields'
