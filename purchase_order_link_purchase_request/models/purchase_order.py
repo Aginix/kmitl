f9db528cfe8b650f327b4b06033dfe81078885ef
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2025-11-13 15:53:11 +0700

    fix

--- purchase_order_link_purchase_request/models/purchase_order.py
+++ purchase_order_link_purchase_request/models/purchase_order.py
@@ -19,6 +19,9 @@ class PurchaseOrder(models.Model):
     @api.depends("order_line.purchase_request_lines")
     def _compute_request_id(self):
         for rec in self:
-            for request_line in rec.order_line.mapped('purchase_request_lines'):
-                rec.request_id = request_line.request_id.id
-                break
+            for line in rec.order_line:
+                for request_line in line.purchase_request_lines:
+                    rec.request_id = request_line.request_id.id
+                    break
+                if rec.request_id:
+                    break
