e611693f8c0559eb6ecfc43536e339fc0b67391c
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-11-14 15:10:15 +0700

    เพิ่ม field

--- account_asset_portal/controllers/account_asset.py
+++ account_asset_portal/controllers/account_asset.py
@@ -7,14 +7,13 @@ from odoo.addons.portal.controllers import portal
 class AccountAsset(portal.CustomerPortal):
     @http.route(['/account_assets/<string:access_uid>'], type='http', auth="public", website=True)
     def work_acceptance_view(self, access_uid, **kw):
-        Asset = request.env['account.asset'].sudo()
+        asset = request.env['account.asset'].sudo()
 
-        asset = Asset.search([('access_uid', '=', access_uid)], limit=1)
-        print('access_uid =====================>', asset)
-        # if not asset:
-        #     return request.not_found()
+        access = asset.search([('access_uid', '=', access_uid)], limit=1)
+        if not access:
+            return request.not_found()
 
         values = {
-            "asset": asset,
+            "asset": access,
         }
         return request.render("account_asset_portal.portal_account_asset_page", values)
