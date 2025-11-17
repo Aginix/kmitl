c5574e97818c4d0df48bfcb264aa5ea91bea850e
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-11-17 11:44:40 +0700

    add : module

---
+++ account_asset_label_kmitl/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Account Asset Label KMITL',
+    'version': '16.0.1.0.0',
+    'summary': """ Account Asset Label KMITL Summary """,
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "category": "KMITL",
+    'depends': ['account_asset_number'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
