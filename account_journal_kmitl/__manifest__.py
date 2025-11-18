14839721917c651d10b2de552e2db4d3f9bb780b
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-11-18 14:35:17 +0700

    [16.0][ADD] account_journal_kmitl

---
+++ account_journal_kmitl/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Account Journal KMITL',
+    'version': '16.0.1.0.0',
+    'summary': """ Account Journal KMITL Summary """,
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    'depends': ['account_usability'],
+    'data': ['views/account_menu.xml'],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
