3da7909122c6375bac5373803935af0ad84fb0e8
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-11-23 21:59:12 +0700

    fix bug

--- stock_picking_kmitl/models/stock_move.py
+++ stock_picking_kmitl/models/stock_move.py
@@ -20,8 +20,9 @@ class StockMove(models.Model):
     def _compute_qty_available(self):
         for move in self:
             if move.product_id and move.location_id:
-                move.qty_available = move.product_id.with_context(
+                available = move.product_id.with_context(
                     location=move.location_id.id
                 ).qty_available
+                move.qty_available = abs(available)
             else:
                 move.qty_available = 0.0
