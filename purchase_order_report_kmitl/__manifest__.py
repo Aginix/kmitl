254b239978857005b6b1b343bce5cbb2dd1bc672
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-11-24 12:05:32 +0700

    [16.0][ADD] purchase_order_report_kmitl

---
+++ purchase_order_report_kmitl/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Order Report KMITL',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Order Report KMITL Summary """,
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "category": "KMITL",
+    'depends': ['purchase_request' , 'thai_date_utils', 'purchase_contract_kmitl'],
+    'data': [
+        "reports/report_purchase_order.xml",
+        "reports/paperformat_purchase_order.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
