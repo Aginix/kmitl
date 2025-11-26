63faac23178a539146137f8b49174c7dd2333243
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-11-26 16:25:30 +0700

    [16.0][IMP] purchase_order_department

--- purchase_order_department/models/purchase_order.py
+++ purchase_order_department/models/purchase_order.py
@@ -10,7 +10,14 @@ _logger = logging.getLogger(__name__)
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
+    READONLY_STATES = {
+        'purchase': [('readonly', True)],
+        'done': [('readonly', True)],
+        'cancel': [('readonly', True)],
+    }
+
     department_id = fields.Many2one(
         comodel_name="hr.department",
         string="Department",
+        states=READONLY_STATES,
     )
