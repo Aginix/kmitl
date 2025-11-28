9cc23e0e86a47a688739b5cf7de522994504f8e6
Tanathip Singhanon <88467996+madara1150@users.noreply.github.com>
2025-11-28 10:34:38 +0700

    [16.0][IMP] clear warning แบบ bypass (#342)

--- product_kmitl/models/product_template.py
+++ product_kmitl/models/product_template.py
@@ -5,7 +5,6 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     purchase_request_ok = fields.Boolean(
-        string="Purchase Request",
         default=False,
         help="If checked, this product can be used in purchase requests",
     )
