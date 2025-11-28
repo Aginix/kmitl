eace54a19a516f222787922e79d862c398a3c7e4
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-11-28 18:11:50 +0700

    testing

---
+++ purchase_request_approval_operating_unit/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Request Approval Operating Unit',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Request Approval Operating Unit Summary """,
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "category": "KMITL",
+    'depends': ['purchase_request_approval', 'purchase_request_operating_unit', 'purchase_request_department', 'hr_department_operating_unit'],
+    'data': [
+        
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
