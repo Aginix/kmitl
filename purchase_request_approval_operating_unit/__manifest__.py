8ee82595ac900171666373de19fac8c0f760b04b
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-12-01 11:32:44 +0700

    add views

--- purchase_request_approval_operating_unit/__manifest__.py
+++ purchase_request_approval_operating_unit/__manifest__.py
@@ -7,8 +7,9 @@
     "website": "https://github.com/aginix/kmitl",
     "category": "KMITL",
     'depends': ['purchase_request_approval', 'purchase_request_operating_unit', 'purchase_request_department', 'hr_department_operating_unit'],
-    'data': [
-        
+    "data": [
+        "security/purchase_request_approval_operating_unit.xml",
+        "views/purchase_request_approval_views.xml"
     ],
     'installable': True,
     'auto_install': False,
