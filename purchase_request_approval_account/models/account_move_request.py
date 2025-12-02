b41321332d43a2213eb12cb652275590fa7a6268
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-12-02 17:01:37 +0700

    add full

--- purchase_request_approval_account/models/account_move_request.py
+++ purchase_request_approval_account/models/account_move_request.py
@@ -8,11 +8,32 @@ class AccountMoveRequest(models.Model):
 
     purchase_request_approval_id = fields.Many2one(
         comodel_name="purchase.request.approval",
-        string="Purchase Order",
+        string="Purchase Request Approval",
         ondelete="set null",
         index=True,
         tracking=True,
     )
 
     def action_view_purchase_approval(self):
-        print("test")
+        self.ensure_one()
+        if not self.purchase_request_approval_id:
+            raise UserError(_('No Purchase Request Approval linked to this request.'))
+
+        return {
+            'type': 'ir.actions.act_window',
+            'name': _('Purchase Request Approval'),
+            'res_model': 'purchase.request.approval',
+            'res_id': self.purchase_request_approval_id.id,
+            'view_mode': 'form',
+            'target': 'current',
+        }
+
+    def _message_link_back_to_request(self):
+        name = self.purchase_request_approval_id.name
+        link = self.purchase_request_approval_id._get_record_url()
+
+        return _(
+            'This record has been created from: <a href="%(link)s" target="_blank">%(name)s</a>',
+            link=link,
+            name=name,
+        )
--- purchase_request_approval_account/models/purchase_request_approval.py
+++ purchase_request_approval_account/models/purchase_request_approval.py
@@ -19,7 +19,12 @@ class PurchaseRequestApproval(models.Model):
     )
 
     billing_status = fields.Selection(
-        selection=[('nothing', 'Nothing'), ('', '')],
+        selection=[
+            ("draft", "Draft"),
+            ("submitted", "Submitted"),
+            ("validated", "Validated"),
+            ("cancel", "Cancelled"),
+        ],
         string='Billing Status',
         compute="_compute_billing_status",
         store=True,
@@ -44,7 +49,13 @@ class PurchaseRequestApproval(models.Model):
         store=False,
     )
 
-    purchase_count = fields.Integer(related="request_id.purchase_count", store=True,)
+    purchase_count = fields.Integer(
+        related="request_id.purchase_count",
+        store=True,
+    )
+
+    def _get_record_url(self):
+        return "/web#id={}&model={}&view_type=form".format(self.id, self._name)
 
     def approval_make_purchase_order(self):
         return self.request_id.approval_make_purchase_order()
@@ -61,7 +72,58 @@ class PurchaseRequestApproval(models.Model):
 
     @api.depends("account_move_request_ids", "account_move_request_ids.state")
     def _compute_billing_status(self):
-        print("test")
+        for approval in self:
+            approval.billing_status = approval.account_move_request_ids.state
+
+    def _prepare_move_request_vals(self):
+        return {
+            "purchase_request_approval_id": self.id,
+            "partner_id": self.request_id.partner_id.id,
+            "line_ids": [
+                Command.create(line._prepare_move_request_line_vals())
+                for line in self.request_id.line_ids
+            ],
+            "ref": self.request_id.name,
+            "payment_type": self.request_id.payment_type,
+        }
+
+    def action_view_move_request(self):
+        self.ensure_one()
+        return {
+            "type": "ir.actions.act_window",
+            "res_model": "account.move.request",
+            "view_mode": "form,tree",
+            "res_id": self.account_move_request_ids.id,
+            "target": "current",
+        }
+
+    def action_move_request(self):
+        self.ensure_one()
+        move_request = self._create_account_move_request()
+        return {
+            "type": "ir.actions.act_window",
+            "res_model": "account.move.request",
+            "view_mode": "form",
+            "res_id": move_request.id,
+            "target": "current",
+        }
 
     def _create_account_move_request(self):
-        print("create")
+        move_request = self.env["account.move.request"].create(
+            self._prepare_move_request_vals()
+        )
+        link_back_message = move_request._message_link_back_to_request()
+        move_request.message_post(body=link_back_message, message_type="comment")
+        message = self._purchase_request_approval_create_bill_message_content(move_request)
+        self.message_post(body=message, message_type="comment")
+        return move_request
+
+    def _purchase_request_approval_create_bill_message_content(self, move_request):
+        message = _(
+            "Billing %(mr_name)s for %(pa_name)s created successfully, waiting for operation."
+        ) % {
+            "mr_name": move_request.name,
+            "pa_name": self.name,
+        }
+
+        return message
