6fad73000fb051ef1f267b1bd9aae94fe30042ec
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2025-12-03 10:32:42 +0700

    add tier comment to depends

--- purchase_work_acceptance_kmitl/__manifest__.py
+++ purchase_work_acceptance_kmitl/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "KMITL",
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
-    'depends': ['l10n_th_gov_work_acceptance'],
+    'depends': ['l10n_th_gov_work_acceptance', 'base_tier_validation_comment'],
     "data": [
         "data/server_action.xml",
         "data/tier.definition.csv",
