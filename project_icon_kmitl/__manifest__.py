1a9876a00ae3f7c9925a422142c1f950cb30fa93
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2025-12-04 17:35:42 +0700

    [ADD] icon: project, procurement_plan, stock_request

---
+++ project_icon_kmitl/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Project Icon KMITL',
+    'version': '16.0.1.0.0',
+    'summary': """ Project Icon KMITL Summary """,
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    'depends': ['base', 'project'],
+    'data': [
+        "data/project_icon.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
