5e337919f54d3dda0ba2f80f41a05bb3fad8a6b0
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-01-06 11:58:55 +0700

    refactor code

--- purchase_order_change/models/purchase_order_change.py
+++ purchase_order_change/models/purchase_order_change.py
@@ -11,7 +11,6 @@ class PurchaseOrderChange(models.Model):
     date = fields.Date(string='Date', default=fields.Date.context_today)
     change_type = fields.Selection(selection=[('none', 'None'), ('impact', 'Impact')])
     section_ids = fields.Many2many(comodel_name="purchase.change.section")
-    state = fields.Selection(selection=[("draft", "Draft"), ("done", "Done"), ("cancel", "Cancel")] , default="draft")
     purchase_id = fields.Many2one(comodel_name="purchase.order")
     change_field_ids = fields.One2many(
         comodel_name='purchase.order.change.field',
