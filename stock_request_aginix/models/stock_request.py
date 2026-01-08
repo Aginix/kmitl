04f572aa85c14c64663a1dc8f54be0c8f34e977f
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2026-01-08 10:47:37 +0700

    imp sr

--- stock_request_aginix/models/stock_request.py
+++ stock_request_aginix/models/stock_request.py
@@ -69,7 +69,7 @@ class StockRequest(models.Model):
         copy=False
     )
     requested_by = fields.Many2one(
-        'res.users',
+        'res.partner',
         default=lambda self: self.env.user,
         tracking=True
     )
@@ -116,6 +116,7 @@ class StockRequest(models.Model):
         self.ensure_one()
         return {
             'picking_type_id': self.picking_type_id.id,
+            'partner_id': self.requested_by.id,
             'location_id': self.location_id.id,
             'location_dest_id': self.location_dest_id.id,
             'origin': self.name,
