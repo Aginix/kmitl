5f519ef1a62a5318837dab96a496fa62f8b5e8a5
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2026-01-09 18:02:15 +0700

    remove store true

--- purchase_request_egp/models/purchase_request.py
+++ purchase_request_egp/models/purchase_request.py
@@ -11,7 +11,7 @@ _logger = logging.getLogger(__name__)
 class PurchaseRequest(models.Model):
     _inherit = "purchase.request"
 
-    is_egp = fields.Boolean(string="e-GP", store=True, tracking=True)
+    is_egp = fields.Boolean(string="e-GP", tracking=True)
 
     egp_project_id = fields.Char(string="เลขที่โครงการ e-GP", tracking=True)
     egp_project_url = fields.Char(
