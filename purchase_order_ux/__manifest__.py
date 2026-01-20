b4be35a7307720974000ccce9788a812e7789728
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-01-20 11:15:47 +0700

    [16.0][IMP] ปรับฟอร์มของ PO

---
+++ purchase_order_ux/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Order UX',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Order UX Summary """,
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "category": "KMITL",
+    'depends': ['purchase_order_kmitl', 'purchase_contract_kmitl', 'purchase_invoice_plan' ,'purchase_order_department' ,'purchase_order_payment_type'],
+    "data": [
+        "views/purchase_order_views.xml"
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
