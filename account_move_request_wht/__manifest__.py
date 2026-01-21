39d563e6d6d7b395916c6fbb2bae0203ddec4155
PHATTARAPON.K <phattarapon.k@aginix.tech>
2026-01-21 13:54:50 +0700

    [16.0][ADD] account_move_request_wht: add WHT support to account move request

---
+++ account_move_request_wht/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Account Move Request - Withholding Tax",
+    "version": "16.0.1.0.0",
+    "category": "Accounting",
+    "summary": "Add WHT support to Account Move Request",
+    "depends": ["account_move_request", "l10n_th_account_tax"],
+    "data": [
+        "views/account_move_request_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
---
+++ account_move_request_wht/models/__init__.py
@@ -0,0 +1,2 @@
+from . import account_move_request
+from . import account_move_request_line
