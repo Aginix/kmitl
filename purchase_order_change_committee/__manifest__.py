e19a86670a7bee7ec0e6cc4723e299e2845a2ef8
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-01-22 10:46:52 +0700

    [16.0][ADD] purchase_order_change_committee

---
+++ purchase_order_change_committee/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Order Change Committee',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Order Change Committee Summary """,
+    'author': 'Aginix Technologies',
+    'website': 'https://github.com/aginix/kmitl',
+    'category': 'KMITL',
+    'depends': ['purchase_order_change'],
+    'data': [
+        "data/purchase_change_section_data.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ purchase_order_change_committee/models/purchase_order_change.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo import _, api, fields, models
+
+
+class PurchaseOrderChange(models.Model):
+    _inherit = 'purchase.order.change'
+
+    @api.model
+    def _get_change_type_section_map(self):
+        res = super()._get_change_type_section_map()
+
+        res.setdefault("none", []).append(
+            "purchase_order_change_committee.purchase_change_section_4"
+        )
+
+        return res
