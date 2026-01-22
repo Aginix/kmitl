b9bbfbae1bfab697d2af40254085df0e7c6bdfbc
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-01-22 14:04:43 +0700

    fix : error

--- purchase_order_change_committee/wizards/purchase_order_change_wizard.py
+++ purchase_order_change_committee/wizards/purchase_order_change_wizard.py
@@ -7,7 +7,7 @@ class PurchaseOrderChangeWizard(models.TransientModel):
     _description = _('Purchase Order ChangeWizard')
 
     show_committee = fields.Boolean()
-    work_acceptance_committee_ids = fields.One2many(comodel_name="procurement.committee", inverse_name="purchase_id")
+    work_acceptance_committee_ids = fields.One2many(comodel_name="procurement.committee")
 
     def _prepare_old_values(self, purchase):
         vals = super()._prepare_old_values(purchase)
