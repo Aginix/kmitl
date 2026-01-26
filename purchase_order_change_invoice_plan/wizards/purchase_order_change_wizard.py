18417763747d9be2d6da7e5f69be303788805f68
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-01-26 15:08:30 +0700

    [16.0][ADD] purchase_order_change_invoice_plan

---
+++ purchase_order_change_invoice_plan/wizards/purchase_order_change_wizard.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo import _, api, fields, models
+
+
+class PurchaseOrderChangeWizard(models.TransientModel):
+    _inherit = 'purchase.order.change.wizard'
+    _description = _('Purchase Order ChangeWizard')
+
+    show_invoice = fields.Boolean()
