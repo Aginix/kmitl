d014706934280d84d9fffd2c10aa6349098f23ee
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-01-26 17:30:33 +0700

    final

--- purchase_order_change_invoice_plan/models/purchase_order_change.py
+++ purchase_order_change_invoice_plan/models/purchase_order_change.py
@@ -10,7 +10,27 @@ class PurchaseOrderChange(models.Model):
         res = super()._get_change_type_section_map()
 
         res.setdefault("none", []).append(
-            "purchase_order_change_committee.purchase_change_section_5"
+            "purchase_order_change_invoice_plan.purchase_change_section_5"
         )
 
         return res
+
+    def _prepare_wizard_context(self, extra_context=None):
+        self.ensure_one()
+
+        base_context = {
+            "default_invoice_plan_ids": [
+                (0, 0, {
+                    "installment": plan.installment,
+                    "plan_date": plan.plan_date,
+                    "percent": plan.percent,
+                    "invoice_plan_id": plan.id,
+                })
+                for plan in self.purchase_id.invoice_plan_ids
+            ],
+        }
+
+        if extra_context:
+            base_context.update(extra_context)
+
+        return base_context
--- purchase_order_change_invoice_plan/wizards/purchase_order_change_wizard.py
+++ purchase_order_change_invoice_plan/wizards/purchase_order_change_wizard.py
@@ -7,3 +7,61 @@ class PurchaseOrderChangeWizard(models.TransientModel):
     _description = _('Purchase Order ChangeWizard')
 
     show_invoice = fields.Boolean()
+
+    invoice_plan_ids_old = fields.Many2many(
+        comodel_name="purchase.invoice.plan",
+        relation="purchase_change_wizard_invoice_old_rel",
+        column1="wizard_id",
+        column2="invoice_plan_id",
+        string="Old Invoice Plan",
+        readonly=True,
+    )
+
+    invoice_plan_ids = fields.Many2many(
+        comodel_name="purchase.invoice.plan",
+        relation="purchase_change_wizard_invoice_new_rel",
+        column1="wizard_id",
+        column2="invoice_plan_id",
+        string="New Invoice Plan",
+        readonly=True,
+    )
+
+    def _prepare_old_values(self, purchase):
+        vals = super()._prepare_old_values(purchase)
+
+        invoice_plans = purchase.invoice_plan_ids
+
+        vals["invoice_plan_ids_old"] = [
+            (6, 0, invoice_plans.ids)
+        ]
+
+        return vals
+
+    def _prepare_section_visibility(self, section_xml_ids):
+            vals = super()._prepare_section_visibility(section_xml_ids)
+            vals["show_invoice"] = "purchase_change_section_5" in section_xml_ids
+            return vals
+
+    def _get_track_fields(self):
+        fields = super()._get_track_fields()
+        fields.update({
+            "invoice_plan_ids": "งวดงาน"
+        })
+        return fields
+
+    def _save_invoice_changes(self):
+        po = self.purchase_id.sudo()
+
+        if self.invoice_plan_ids:
+            commands = [(6, 0, self.invoice_plan_ids.ids)]
+        else:
+            commands = [(5, 0, 0)]
+
+        po.write({
+            "invoice_plan_ids": commands
+        })
+
+    def action_save_changes(self):
+        res = super().action_save_changes()
+        self._save_invoice_changes()
+        return res
