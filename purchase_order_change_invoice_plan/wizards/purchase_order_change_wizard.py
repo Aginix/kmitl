1c1e09139e914518c39cb9ad5a39924c82e922c9
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-01-27 12:59:26 +0700

    update : view

--- purchase_order_change_invoice_plan/wizards/purchase_order_change_wizard.py
+++ purchase_order_change_invoice_plan/wizards/purchase_order_change_wizard.py
@@ -41,10 +41,10 @@ class PurchaseOrderChangeWizard(models.TransientModel):
         return vals
 
     def _prepare_section_visibility(self, section_xml_ids):
-            _logger.warning("SECTION XML IDS >>> %s", section_xml_ids)
-            vals = super()._prepare_section_visibility(section_xml_ids)
-            vals["show_invoice"] = "purchase_change_section_5" in section_xml_ids
-            return vals
+        _logger.warning("SECTION XML IDS >>> %s", section_xml_ids)
+        vals = super()._prepare_section_visibility(section_xml_ids)
+        vals["show_invoice"] = "purchase_change_section_5" in section_xml_ids
+        return vals
 
     def _get_track_fields(self):
         fields = super()._get_track_fields()
