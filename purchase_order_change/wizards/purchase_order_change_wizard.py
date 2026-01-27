412d4f2a8a89ba1a98d535243b8d1ce09c2fd552
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-01-27 14:10:36 +0700

    test

--- purchase_order_change/wizards/purchase_order_change_wizard.py
+++ purchase_order_change/wizards/purchase_order_change_wizard.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+import logging
+
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
 
-
+_logger = logging.getLogger(__name__)
 class PurchaseOrderChangeWizard(models.TransientModel):
     _name = 'purchase.order.change.wizard'
     _description = _('PurchaseOrderChangeWizard')
