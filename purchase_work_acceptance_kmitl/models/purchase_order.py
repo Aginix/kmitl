beb891c3f09745a49fbacc61c309e83f34861dab
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2026-01-28 15:56:57 +0700

    fix bug

--- purchase_work_acceptance_kmitl/models/purchase_order.py
+++ purchase_work_acceptance_kmitl/models/purchase_order.py
@@ -10,7 +10,7 @@ _logger = logging.getLogger(__name__)
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    def _get_committee_line(self):
+    def _get_committee_line(self, purchase_requests=None):
         committees = self.mapped("work_acceptance_committee_ids")
         lines = [(0, 0, self._prepare_committee_line(line)) for line in committees]
         return lines
