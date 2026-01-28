0961abec39698b7fd79ee2841c66cf655057d02b
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-01-28 16:24:40 +0700

    fix : แก้ไข งวดงานไม่ได้

--- purchase_order_change_invoice_plan/wizards/purchase_create_invoice_plan.py
+++ purchase_order_change_invoice_plan/wizards/purchase_create_invoice_plan.py
@@ -11,9 +11,19 @@ class PurchaseCreateInvoicePlan(models.TransientModel):
     _inherit = 'purchase.create.invoice.plan'
 
     def purchase_create_invoice_plan(self):
-        res = super().purchase_create_invoice_plan()
-        purchase_id = self.env.context.get("default_purchase_id")
-        if purchase_id:
-            purchase = self.env["purchase.order"].browse(purchase_id)
-            purchase.use_invoice_plan = True
-        return res
+        self.ensure_one()
+
+        purchase_id = self._context.get("default_purchase_id")
+
+        if not purchase_id:
+            purchase_id = self._context.get("active_id")
+
+        purchase = self.env["purchase.order"].browse(purchase_id)
+        purchase.create_invoice_plan(
+            self.num_installment,
+            self.installment_date,
+            self.interval,
+            self.interval_type,
+        )
+        purchase.use_invoice_plan = True
+        return {"type": "ir.actions.act_window_close"}
