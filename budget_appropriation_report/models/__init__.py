a927406684c6d2fdff166da65d00214d259341d6
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2026-01-29 21:46:57 +0700

    feat: add f11w report

--- budget_appropriation_report/models/__init__.py
+++ budget_appropriation_report/models/__init__.py
@@ -7,6 +7,7 @@ from . import budget_appropriation_f5p_expense
 from . import budget_appropriation_f5w_expense
 from . import budget_appropriation_f8w_expense
 from . import budget_appropriation_f9w_expense
+from . import budget_appropriation_f11w_expense
 from . import budget_appropriation_report_revenue_overview
 from . import budget_appropriation_f5_expense
 from . import budget_appropriation_report
