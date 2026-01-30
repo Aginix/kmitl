089ef8421a53ad557524747be5091d943b47ae51
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-01-30 15:20:04 +0700

    [IMP] Icon

--- purchase_kmitl/__manifest__.py
+++ purchase_kmitl/__manifest__.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Stock Icon KMITL',
+    'name': 'Purchase KMITL',
     'version': '16.0.1.0.0',
-    'summary': """ Stock Icon KMITL Summary """,
+    'summary': """ Purchase KMITL Summary """,
     "category": "KMITL",
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
-    'depends': ['stock'],
+    'depends': ['purchase'],
     'data': [
-        "views/stock.xml"
+        "views/purchase.xml"
     ],
     'assets': {
               'web.assets_backend': [
-                  'stock_icon_kmitl/static/src/**/*'
+                  'purchase_kmitl/static/src/**/*'
               ],
           },
     'installable': True,
