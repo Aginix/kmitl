2782c377bd379f96df1e264740dd002b258d13b3
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2026-02-04 18:09:45 +0700

    remove product menu + th po

--- purchase_menu/__manifest__.py
+++ purchase_menu/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Purchase_menu',
+    'name': 'Purchase Menu Custom',
     'version': '16.0.1.0.0',
-    'summary': """ Purchase_menu Summary """,
-    'author': '',
-    'website': '',
-    'category': '',
+    'summary': """ Purchase Menu Summary """,
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "category": "Purchase",
     'depends': ['purchase_guarantee_kmitl', 'purchase_work_acceptance_kmitl', 'l10n_th_gov_account_asset_management'],
     'data': [
         'views/purchase_tracking.xml',
