fbec67ccc308bc14857f9608f00706fc57f68038
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-02-04 11:40:24 +0700

    [ADD] purchase_order_state_kmitl

---
+++ purchase_order_state_kmitl/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Order State KMITL',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase Order State KMITL Summary """,
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "category": "KMITL",
+    'depends': ['purchase'],
+    'data': [
+
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ purchase_order_state_kmitl/models/purchase_order.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError, ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    state = fields.Selection([
+        ('draft', 'draft'),
+        ('open', 'Open'),
+        ('done', 'Locked'),
+        ('cancel', 'Cancel')
+    ],default='draft')
