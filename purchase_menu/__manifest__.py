05816caa18990e1b0432832bf45e953790387c68
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2026-02-04 12:31:44 +0700

    testing

---
+++ purchase_menu/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase_menu',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase_menu Summary """,
+    'author': '',
+    'website': '',
+    'category': '',
+    'depends': ['purchase_guarantee_kmitl', 'purchase_work_acceptance_kmitl', 'l10n_th_gov_account_asset_management'],
+    'data': [
+        'views/menu.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
