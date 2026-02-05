066d4826ad39c3644b2266a3d02cbe3cecbea0f0
PHATTARAPON.K <phattarapon.k@aginix.tech>
2026-02-05 14:55:47 +0700

    fix error depends

--- kmitl_depends_dev/__manifest__.py
+++ kmitl_depends_dev/__manifest__.py
@@ -32,7 +32,7 @@
                 'purchase_guarantee_kmitl', 'purchase_guarantee_lock', 'purchase_guarantee_no_invoice_bill', 'purchase_guarantee_purchase_order_kmitl',
                 'purchase_guarantee_tracking', 'purchase_manual_delivery_security', 'purchase_manual_delivery_work_acceptance',
                 'purchase_operating_unit_access_all', 'purchase_order_disbursement', 'purchase_order_disbursement_attach_existing_attachments',
-                'purchase_order_account_move_request_budget'],
+                'purchase_order_disbursement_budget'],
     'data': [
 
     ],
