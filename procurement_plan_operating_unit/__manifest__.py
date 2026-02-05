b038e9240b0695019182c415f8b6d3c823ee5c80
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2026-02-05 23:09:14 +0700

    [16.0][ADD] procurement_plan: add operating unit support
    Add operating unit integration to procurement plan through two modules:
    - procurement_plan_operating_unit: Core operating unit functionality
    - procurement_plan_operating_unit_access_all: Bypass group for unrestricted access

---
+++ procurement_plan_operating_unit/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Procurement Plan with Operating Units",
+    "version": "16.0.1.0.0",
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "depends": [
+        "operating_unit",
+        "procurement_plan",
+    ],
+    "data": [
+        "security/procurement_plan_security.xml",
+        "views/procurement_plan_views.xml",
+    ],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
