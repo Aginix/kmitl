b7f827020ccb44984d9552a03f7281b9b195aa96
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-02-06 11:57:58 +0700

    fix

--- purchase_request_ux_kmitl/models/purchase_request.py
+++ purchase_request_ux_kmitl/models/purchase_request.py
@@ -17,7 +17,10 @@ class PurchaseRequest(models.Model):
 
     @api.onchange("department_id")
     def _onchange_department_id(self):
-        self.requested_by = False
+        if self.requested_by and \
+        self.requested_by.employee_ids.department_id != self.department_id:
+            self.requested_by = False
+
         return {
             "domain": {
                 "requested_by": [
