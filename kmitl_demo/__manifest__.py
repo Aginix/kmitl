a31521cb02737a7107c400b57c25d296480a1d3f
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2026-02-07 23:14:05 +0700

    [IMP] purchase_request_approval_kmitl: restore module
    Restore purchase_request_approval_kmitl module and its dependencies:
    - Restore all module files from commit caf9e68
    - Add back dependency in kmitl_demo
    - Uncomment button_to_verify calls in demo data

--- kmitl_demo/__manifest__.py
+++ kmitl_demo/__manifest__.py
@@ -22,6 +22,7 @@
         "purchase_request_department",
         "purchase_request_budget",
         "purchase_request_price_tax_included",
+        "purchase_request_approval_kmitl",
         "purchase_request_payment_type",
         "operating_unit_kmitl",
         "analytic_operating_unit",
---
+++ purchase_request_approval_kmitl/models/purchase_request.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+from lxml import etree
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError, ValidationError
+
+
+class PurchaseRequest(models.Model):
+    _inherit = "purchase.request"
+    _state_from = ["to_verify", "to_approve"]
+
+    _STATES = [("to_verify", "To be verified"), ("to_approve",)]
+
+    is_purchase_request = fields.Boolean(compute="_compute_is_purchase_request")
+    state = fields.Selection(
+        selection_add=_STATES,
+        string="Status",
+        index=True,
+        tracking=True,
+        required=True,
+        copy=False,
+        ondelete={
+            "to_verify": "set default",
+        },
+    )
+    can_request = fields.Boolean(compute="_compute_can_request")
+    hide_request_validation_button = fields.Boolean(
+        compute="_compute_hide_request_validation_button",
+        string="Hide Request Validation Button",
+        store=False,
+    )
+    hide_restart_validation_button = fields.Boolean(
+        compute="_compute_hide_restart_validation_button",
+        string="Hide Restart Validation Button",
+        store=False,
+    )
+
+    @api.depends("need_validation", "validation_status", "can_request")
+    def _compute_hide_restart_validation_button(self):
+        for record in self:
+            record.hide_restart_validation_button = (
+                record.need_validation != True
+                or record.validation_status != "pending"
+                or record.can_request is False
+            )
+
+    @api.depends('need_validation', 'validation_status', 'rejected', 'state', 'can_request')
+    def _compute_hide_request_validation_button(self):
+        current_user = self.env.user
+        for record in self:
+            record.hide_request_validation_button = (
+                record.need_validation != True
+                or record.validation_status == 'pending'
+                or record.rejected
+                or record.state != 'to_approve'
+                or record.can_request is False
+                or record.requested_by != current_user
+            )
+
+    def _compute_is_purchase_request(self):
+        for rec in self:
+            rec.is_purchase_request = rec._name == "purchase.request"
+
+    def _add_tier_validation_buttons(self, node, params):
+        """ "close btn"""
+        if self.is_purchase_request:
+            str_element = self.env["ir.qweb"]._render(
+                "base_tier_validation.tier_validation_buttons", params
+            )
+            new_node = etree.fromstring(str_element)
+            return new_node
+        return etree.Element("div")
+
+    def _validate_tier(self, tiers=False):
+        super()._validate_tier(tiers)
+        reviews = self.review_ids.filtered(
+            lambda r: r.status == "pending" and (self.env.user in r.reviewer_ids)
+        )
+        if not reviews:
+            return self.button_approved()
+
+    @api.model
+    def _get_after_validation_exceptions(self):
+        res = super()._get_after_validation_exceptions()
+        res.append("state")
+        res.append("substate_id")
+        return res
+
+    def button_to_verify(self):
+        self.ensure_one()
+        if self.detect_exceptions() and not self.ignore_exception:
+            return self._popup_exceptions()
+        self.write({"state": "to_verify"})
+
+    @api.depends("state")
+    def _compute_is_editable(self):
+        res = super()._compute_is_editable()
+        for record in self:
+            if record.state in ("to_verify"):
+                record.is_editable = False
+
+    def request_validation(self):
+        self.ensure_one()
+        res = super().request_validation()
+        return res
+
+    def restart_validation(self):
+        self.ensure_one()
+        res = super().restart_validation()
+        self.write({"state": "to_verify"})
+        return res
+
+    @api.model
+    def _get_under_validation_exceptions(self):
+        res = super()._get_under_validation_exceptions()
+        res.append("state")
+        res.append("substate_id")
+        res.append("approved_by")
+        res.append("date_verified")
+        res.append("date_approved")
+        return res
+
+    @api.depends("requested_by")
+    def _compute_can_request(self):
+        current_user = self.env.user
+        is_manager = current_user.has_group(
+            "purchase_request.group_purchase_request_manager"
+        )
+        is_admin = current_user.has_group("base.group_erp_manager")
+        for rec in self:
+            own_by_me = rec.requested_by.id == current_user.id
+            rec.can_request = own_by_me or is_manager or is_admin
+
+    def _compute_hide_reserve_budget_button(self):
+        super()._compute_hide_reserve_budget_button()
+        for rec in self:
+            if rec.state == "to_verify":
+                rec.hide_reserve_budget_button = False
+
+    def _compute_is_budget_editable(self):
+        super()._compute_is_budget_editable()
+        can_edit = self.env.user.has_group("budget.group_budget_commitment")
+        for rec in self:
+            if rec.substate_sequence == 10 and rec.state == "to_verify" and can_edit:
+                rec.is_budget_editable = True
