8693819ad1b4eb9df6dffc3c84b72d49cc7352bc
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-02-08 12:37:43 +0700

    [bug] พจ.1

--- purchase_order_link_purchase_request/models/purchase_order.py
+++ purchase_order_link_purchase_request/models/purchase_order.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from odoo import models, fields, api, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
 
 _logger = logging.getLogger(__name__)
@@ -14,6 +14,7 @@ class PurchaseOrder(models.Model):
         "purchase.request",
         compute="_compute_request_id",
         string="Purchase Request",
+        store=True,
     )
 
     @api.depends("order_line.purchase_request_lines")
