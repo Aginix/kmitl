2c9da84e764ffdd018523aec759f03087cbc26d9
Nonpawit Teerachetmongkol <n3n@N3N-Macbook.local>
2026-02-11 12:01:46 +0700

    [IMP] purchase_work_acceptance_kmitl: remove unused dependency

--- purchase_work_acceptance_kmitl/__manifest__.py
+++ purchase_work_acceptance_kmitl/__manifest__.py
@@ -9,7 +9,6 @@
     'depends': [
         'l10n_th_gov_work_acceptance',
         'base_tier_validation_comment',
-        'purchase_work_acceptance_late_fines',
     ],
     "data": [
         "data/server_action.xml",
