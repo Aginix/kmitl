ed7477233afe0e491757a7eb00632dff9d911913
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-02-12 12:53:13 +0700

    fix

--- purchase_work_acceptance_kmitl/__manifest__.py
+++ purchase_work_acceptance_kmitl/__manifest__.py
@@ -9,6 +9,8 @@
     'depends': [
         'l10n_th_gov_work_acceptance',
         'base_tier_validation_comment',
+        'thai_date_utils',
+        'purchase_contract_kmitl',
     ],
     "data": [
         "data/server_action.xml",
