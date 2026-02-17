7e9716f3d44119b73414850d2da15fa219467ea8
Champ Phattarapon <kkw.phattarapon@gmail.com>
2026-02-17 14:13:10 +0700

    [16.0][ADD] aginix_approval (#397)
    Co-authored-by: PHATTARAPON.K <phattarapon.k@aginix.tech>

---
+++ agx_approval_disbursement/models/disbursement_request.py
@@ -0,0 +1,30 @@
+from odoo import _, fields, models
+from odoo.exceptions import UserError
+
+
+class DisbursementRequest(models.Model):
+    _inherit = "disbursement.request"
+
+    approval_request_id = fields.Many2one(
+        comodel_name="approval.request",
+        string="Approval Request",
+        ondelete="set null",
+        index=True,
+        copy=False,
+        tracking=True,
+    )
+
+    def action_view_approval_request(self):
+        self.ensure_one()
+        if not self.approval_request_id:
+            raise UserError(
+                _("No Approval Request linked to this request.")
+            )
+        return {
+            "type": "ir.actions.act_window",
+            "name": _("Approval Request"),
+            "res_model": "approval.request",
+            "res_id": self.approval_request_id.id,
+            "view_mode": "form",
+            "target": "current",
+        }
