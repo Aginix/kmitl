a1d0b3dd4ccf65c1e2de6c92451d6331213e3b51
n3n <nonpawit.tee@gmail.com>
2026-02-20 16:27:02 +0700

    [16.0][ADD] account_asset_depreciation_board: add standalone depreciation board
    Adds a new module that provides a standalone list view for account.asset.line with search filters and batch-posting capability. Users can now view all pending depreciation entries, filter by profile/month/asset, and batch-post journal entries without navigating through individual asset forms.

---
+++ account_asset_depreciation_board/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Account Asset Depreciation Board",
+    "version": "16.0.1.0.0",
+    "summary": "Standalone depreciation board for batch-posting depreciation entries",
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "depends": ["account_asset_usability_kmitl"],
+    "data": [
+        "views/account_asset_line_views.xml",
+        "views/menuitem.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
