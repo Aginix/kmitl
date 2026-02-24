7bd64ad5159b3738a7262d76a9a64cad1b91b5f8
n3n <nonpawit.tee@gmail.com>
2026-02-24 18:14:03 +0700

    [16.0][FIX] account_asset_depreciation_report: fix download action in dialog
    Return wizard form view instead of act_url to avoid 'Operation not supported'
    error when returning URL actions from a target=new dialog. Show binary download
    field in the wizard after report generation.

--- account_asset_depreciation_report/wizard/asset_depreciation_report_wizard.py
+++ account_asset_depreciation_report/wizard/asset_depreciation_report_wizard.py
@@ -87,8 +87,9 @@ class AssetDepreciationReportWizard(models.TransientModel):
         filename = "asset_depreciation_report.xlsx"
         self.write({"data": base64.b64encode(xlsx_data), "filename": filename})
         return {
-            "type": "ir.actions.act_url",
-            "url": f"/web/content?model=asset.depreciation.report.wizard"
-                   f"&id={self.id}&field=data&download=true&filename={filename}",
-            "target": "self",
+            "type": "ir.actions.act_window",
+            "res_model": self._name,
+            "view_mode": "form",
+            "res_id": self.id,
+            "target": "new",
         }
