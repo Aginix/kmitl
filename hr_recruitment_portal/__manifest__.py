da482296056de74bba1bfb2689ce6cbf0d2e8b0c
nopparuts <ak3.nopparut@gmail.com>
2026-02-26 02:02:19 +0700

    [ADD] hr_recruitment_portal: add recruitment application portal
    Allow portal users to view their own job application status.
    Includes application list, detail page with stage progression,
    and security rules matching by partner or email.

---
+++ hr_recruitment_portal/__manifest__.py
@@ -0,0 +1,19 @@
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "HR Recruitment Portal",
+    "version": "16.0.1.0.0",
+    "author": "Aginix Technologies, KMITL",
+    "category": "Human Resources",
+    "license": "LGPL-3",
+    "depends": [
+        "hr_recruitment",
+        "portal",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/security.xml",
+        "views/portal_templates.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
