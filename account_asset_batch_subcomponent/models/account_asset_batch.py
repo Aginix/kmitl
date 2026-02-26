cd6c6b29f46cff5b2121427d4685dfab24d7411e
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-02-26 16:39:52 +0700

    แก้ไข function

--- account_asset_batch_subcomponent/models/account_asset_batch.py
+++ account_asset_batch_subcomponent/models/account_asset_batch.py
@@ -11,33 +11,20 @@ class AccountAssetBatch(models.Model):
     _inherit = 'account.asset.batch'
 
     def action_register_assets(self):
+        res = super().action_register_assets()
+
+        asset_model = self.env['account.asset']
+        sub_model = self.env['account.asset.subcomponent']
+
         for batch in self:
-            if not batch.line_ids:
-                raise ValidationError(_("Cannot register assets without any lines."))
-
-            if any(line.amount <= 0 for line in batch.line_ids):
-                raise ValidationError(_("Some lines have zero amount. Please correct them before proceeding."))
-
-            asset_model = self.env['account.asset']
-            sub_model = self.env['account.asset.subcomponent']
+            assets = asset_model.search([
+                ('batch_id', '=', batch.id)
+            ])
 
             for line in batch.line_ids:
-                for i in range(line.amount):
-                    asset = asset_model.create({
-                        "name": line.name,
-                        "analytic_distribution": batch.purchase_id.analytic_distribution,
-                        "date_start": batch.date,
-                        "account_fiscal_year_id": batch.account_fiscal_year_id.id,
-                        "operating_unit_id": batch.operating_unit_id.id,
-                        "department_id": batch.department_id.id,
-                        "purchase_id": batch.purchase_id.id,
-                        "gpsc_id": line.gpsc_id.id,
-                        "profile_id": line.profile_id.id,
-                        "purchase_value": line.price_per_unit,
-                        "batch_line_id": line.id,
-                        "batch_id": batch.id,
-                    })
+                line_assets = assets.filtered(lambda a: a.batch_line_id.id == line.id)
 
+                for asset in line_assets:
                     for sub in line.subcomponent_ids:
                         sub_model.create({
                             'asset_id': asset.id,
@@ -45,4 +32,4 @@ class AccountAssetBatch(models.Model):
                             'sequence': sub.sequence,
                         })
 
-            batch.state = 'done'
+        return res
