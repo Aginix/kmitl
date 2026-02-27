a699813d3941a4dbba5fdce7d82dbf177952b4e3
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2026-02-27 11:10:40 +0700

    remove attachment type

--- agx_approval_attachment/models/ir_attachment.py
+++ agx_approval_attachment/models/ir_attachment.py
@@ -10,12 +10,4 @@ _logger = logging.getLogger(__name__)
 class IrAttachment(models.Model):
     _inherit = 'ir.attachment'
 
-    attachment_type = fields.Selection(
-        [
-            ("tor", "Specification (TOR)"),
-            ("rfq", "Quotation"), 
-            ("etc", "Etc"),
-        ],
-        string="Attachment Type"
-    )
     sequence = fields.Integer(string="Sequence")
