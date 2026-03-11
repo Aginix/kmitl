ce5d6d82e51568bee245c1dc054fd79c43d7e658
PHATTARAPON.K <phattarapon.k@aginix.tech>
2026-03-11 15:02:22 +0700

    [16.0][IMP] purchase_guarantee_account_payment: validate guarantee method before creating payment

--- purchase_guarantee_account_payment/models/purchase_guarantee.py
+++ purchase_guarantee_account_payment/models/purchase_guarantee.py
@@ -1,4 +1,5 @@
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class PurchaseGuarantee(models.Model):
@@ -32,24 +33,22 @@ class PurchaseGuarantee(models.Model):
 
     def action_create_payment(self):
         self.ensure_one()
-        payment_type = False
         if self.guarantee_method_id == self.env.ref("l10n_th_gov_purchase_guarantee.bid_guarantee"):
             payment_type = self.env.ref("account_payment_kmitl.payment_type_bid_guarantee_receive")
         elif self.guarantee_method_id == self.env.ref("l10n_th_gov_purchase_guarantee.advance_payment_guarantee"):
             payment_type = self.env.ref("account_payment_kmitl.payment_type_guarantee_receive")
+        else:
+            raise UserError(_("ไม่สามารถสร้างใบส่งเงินสำหรับประเภทหลักประกัน '%s' ได้", self.guarantee_method_id.name))
         vals = {
             "partner_id": self.partner_id.id,
             "amount": self.amount,
             "currency_id": self.currency_id.id,
             "purchase_guarantee_id": self.id,
+            "kmitl_payment_type_id": payment_type.id,
+            "payment_type": payment_type.direction,
         }
-        if payment_type:
-            vals.update({
-                "kmitl_payment_type_id": payment_type.id,
-                "payment_type": payment_type.direction,
-            })
-            if payment_type.journal_id:
-                vals["journal_id"] = payment_type.journal_id.id
+        if payment_type.journal_id:
+            vals["journal_id"] = payment_type.journal_id.id
         payment = self.env["account.payment"].create(vals)
         return {
             "type": "ir.actions.act_window",
