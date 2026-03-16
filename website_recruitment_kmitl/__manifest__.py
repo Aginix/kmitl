109d46590595f1f63546d322c3e665193cc92a48
n3n <nonpawit.tee@gmail.com>
2026-03-16 23:48:09 +0700

    [16.0][IMP] website_recruitment_kmitl: refactor theme, add controllers and website pages

--- website_recruitment_kmitl/__manifest__.py
+++ website_recruitment_kmitl/__manifest__.py
@@ -1,26 +1,31 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Website Recruitment KMITL',
-    'version': '16.0.1.0.0',
-    'summary': 'Custom theme and pages for KMITL recruitment website',
-    'author': 'KMITL',
-    'category': 'Website',
-    'depends': ['website', 'website_hr_recruitment'],
-    'data': [
-        'views/website_hr_recruitment_templates.xml',
-        'views/snippets/s_benefit_card.xml',
-        'views/snippets/snippets.xml',
+    "name": "Website Recruitment KMITL",
+    "version": "16.0.1.0.0",
+    "summary": "Custom theme and pages for KMITL recruitment website",
+    "author": "KMITL",
+    "category": "Website",
+    "depends": ["website", "website_hr_recruitment"],
+    "data": [
+        "data/website_data.xml",
+        "views/website_hr_recruitment_templates.xml",
+        "views/snippets/s_benefit_card.xml",
+        "views/snippets/snippets.xml",
+        "views/website_templates.xml",
     ],
-    'assets': {
-        'web.assets_frontend': [
-            'website_recruitment_kmitl/static/src/scss/theme.scss',
+    "assets": {
+        "web.assets_frontend": [
+            "website_recruitment_kmitl/static/src/scss/theme.scss",
         ],
-        'web._assets_frontend_helpers': [
-            ('prepend', 'website_recruitment_kmitl/static/src/scss/bootstrap_overridden.scss'),
+        "web._assets_frontend_helpers": [
+            (
+                "prepend",
+                "website_recruitment_kmitl/static/src/scss/bootstrap_overridden.scss",
+            ),
         ],
     },
-    'application': False,
-    'installable': True,
-    'auto_install': False,
-    'license': 'LGPL-3',
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
 }
