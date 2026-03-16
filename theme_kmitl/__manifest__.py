6551c2b80fabfba9af7b3ab4352b5155facb385f
n3n <nonpawit.tee@gmail.com>
2026-03-16 16:18:01 +0700

    [16.0][ADD] theme_kmitl: add website theme module with KMITL brand color palettes

---
+++ theme_kmitl/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'KMITL Theme',
+    'description': 'KMITL Website Theme',
+    'category': 'Theme/Corporate',
+    'summary': 'Corporate, University, Education, KMITL',
+    'sequence': 150,
+    'version': '16.0.1.0.0',
+    'author': 'KMITL',
+    'depends': ['theme_common'],
+    'data': [
+        'data/ir_asset.xml',
+    ],
+    'license': 'LGPL-3',
+}
---
+++ theme_kmitl/models/theme_kmitl.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo import models
+
+
+class ThemeKmitl(models.AbstractModel):
+    _inherit = 'theme.utils'
+
+    def _theme_kmitl_post_copy(self, mod):
+        pass
