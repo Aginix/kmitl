1037f09ec25260dc8fc09996c490efe16a934bd0
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2026-03-18 17:44:43 +0700

    testing

--- purchase_work_acceptance_kmitl/models/work_acceptance.py
+++ purchase_work_acceptance_kmitl/models/work_acceptance.py
@@ -17,6 +17,42 @@ class WorkAcceptance(models.Model):
         states={"draft": [("readonly", False)]},
     )
 
+    late_days = fields.Integer(
+        default=0,
+    )
+
+    fines_rate = fields.Monetary(
+        default=0,
+    )
+
+    price_subtotal = fields.Monetary(
+        compute="_compute_price_subtotal",
+        string="Subtotal",
+        store=True,
+    )
+
+    fines_total = fields.Monetary(
+        string="Total",
+        compute="_compute_fines_total",
+        store=True,
+    )
+
+    @api.depends("price_subtotal", "fines_late")
+    def _compute_fines_total(self):
+        for rec in self:
+            rec.fines_total = rec.price_subtotal + rec.fines_late
+
+    @api.depends("wa_line_ids", "wa_line_ids.price_subtotal")
+    def _compute_price_subtotal(self):
+        for rec in self:
+            rec.price_subtotal = sum(rec.wa_line_ids.mapped("price_subtotal"))
+
+    @api.onchange('purchase_id')
+    def _onchange_purchase_id_fines(self):
+        if self.purchase_id:
+            self.late_days = self.purchase_id.late_days
+            self.fines_rate = self.purchase_id.fines_rate
+
     def action_view_purchase_order(self):
         self.ensure_one()
         if not self.purchase_id:
