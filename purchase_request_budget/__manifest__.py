473a315f42aa892123729f914cf266ba7fd258f9
Tanathip Singhanon <88467996+madara1150@users.noreply.github.com>
2026-03-19 14:03:22 +0700

    [IMP] purchase_request_kmitl: ยุบรวม (#544)

--- purchase_request_budget/__manifest__.py
+++ purchase_request_budget/__manifest__.py
@@ -10,7 +10,7 @@
         "l10n_th_gov_purchase_request",
         "budget",
         "budget_product",
-        "purchase_request_account_fiscal_year",
+        "purchase_request_kmitl",
     ],
     "data": [
         "data/purchase_request_exception.xml",
--- purchase_request_kmitl/models/__init__.py
+++ purchase_request_kmitl/models/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-from . import procurement_committee, purchase_request
+from . import ir_attachment, procurement_committee, purchase_request, purchase_request_line
