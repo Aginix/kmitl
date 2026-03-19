8b3fa919712195768569d7c07ea17c2ff18973bf
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2026-03-19 17:45:14 +0700

    fix bug

--- purchase_work_acceptance_kmitl/models/work_acceptance.py
+++ purchase_work_acceptance_kmitl/models/work_acceptance.py
@@ -34,10 +34,12 @@ class WorkAcceptance(models.Model):
         store=True
     )
 
-    @api.onchange("late_days")
+    @api.onchange("late_days", 'date_receive', 'date_due')
     def _onchange_late_days(self):
+        res = super()._onchange_late_days()
         if self.late_days < 0:
             self.late_days = 0
+        return res
 
     @api.onchange("fines_rate")
     def _onchange_fines_rate(self):
