a4cf3ccf508056d2507502c68bf0deca86f7f436
Tanathip Singhanon <88467996+madara1150@users.noreply.github.com>
2026-03-23 13:55:42 +0700

    [IMP] feedback พี่กาย (#559)

--- purchase_invoice_plan_kmitl/models/purchase_invoice_plan.py
+++ purchase_invoice_plan_kmitl/models/purchase_invoice_plan.py
@@ -2,7 +2,7 @@
 import logging
 from datetime import timedelta
 
-from odoo import models, fields, api, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
 
 _logger = logging.getLogger(__name__)
