c1f314aa938899869c5d44295e2f5f1be84b75f6
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-03-24 14:17:56 +0700

    [FIX] purchase_request_kmitl: remove circular dependency with purchase_request_payment_type

--- purchase_request_kmitl/__manifest__.py
+++ purchase_request_kmitl/__manifest__.py
@@ -15,7 +15,6 @@
         "account_fiscal_year",
         "purchase_order_kmitl",
         "purchase_operating_unit",
-        "purchase_request_payment_type",
     ],
     "data": [
         "security/ir.model.access.csv",
