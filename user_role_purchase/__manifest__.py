8ede8a7d62e87b4267044aed621a0013ff3a452e
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-03-31 12:56:36 +0700

    [FIX] user_role_purchase: update ref after l10n_th_gov_purchase_request merge
    l10n_th_gov_purchase_request was merged into purchase_request_kmitl.
    Update depends and group references accordingly.

--- user_role_purchase/__manifest__.py
+++ user_role_purchase/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "KMITL",
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
-    'depends': ['base_user_role', 'purchase_request_kmitl', 'l10n_th_gov_purchase_request', 'budget', 'disbursement', 'procurement_plan', 'agx_sarabun', 'operating_unit_access_all'],
+    'depends': ['base_user_role', 'purchase_request_kmitl', 'budget', 'disbursement', 'procurement_plan', 'agx_sarabun', 'operating_unit_access_all'],
     'data': [
         'data/res.users.role.xml',
     ],
