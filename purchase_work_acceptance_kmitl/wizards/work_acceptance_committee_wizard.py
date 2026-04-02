f9452aa810fb7f766a945bcbfefbda7fa7296fdb
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2026-04-02 16:24:05 +0700

    testing

--- purchase_work_acceptance_kmitl/wizards/work_acceptance_committee_wizard.py
+++ purchase_work_acceptance_kmitl/wizards/work_acceptance_committee_wizard.py
@@ -43,24 +43,6 @@ class WorkAcceptanceCommitteeWizard(models.TransientModel):
 
     def button_confirm(self):
         self.ensure_one()
-        # incomplete = self.line_ids.filtered(
-        #     lambda l: not l.is_done and not l.status
-        # )
-        # if incomplete:
-        #     raise UserError(_("Please fill in all the inspection results."))
-
-        # if incomplete:
-        #     return {
-        #         'type': 'ir.actions.client',
-        #         'tag': 'display_notification',
-        #         'params': {
-        #             'title': _('Please fill in all the inspection results.'),
-        #             'message': _('Please fill in all the inspection results.'),
-        #             'type': 'warning',
-        #             'sticky': False,
-        #             'next': {'type': 'ir.actions.act_window_close'},
-        #         },
-        #     }
 
         for line in self.line_ids:
             if line.is_done:
