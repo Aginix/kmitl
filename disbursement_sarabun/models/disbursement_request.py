e571246d2ce2c9461152d591b92270cae96e7949
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2026-04-03 16:35:20 +0700

    change from state validate to signed

--- disbursement_sarabun/models/disbursement_request.py
+++ disbursement_sarabun/models/disbursement_request.py
@@ -64,7 +64,7 @@ class DisbursementRequest(models.Model):
         )
         for record in self:
             if record.state == "submitted":
-                record.state = "signed"
+                record.action_send()
                 record.message_post(
                     body=_("Approved by head via Sarabun: %s") % document.name,
                     subtype_xmlid="mail.mt_note",
