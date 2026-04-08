51d8f6908f8e202b1e626231c41055c4344d665f
nopparuts <ak3.nopparut@gmail.com>
2026-04-08 17:22:54 +0700

    [IMP] hr_recruitment_kmitl: add navigation menu via post_init_hook
    Add Home, Jobs, My Applications, and Profile menu items to the HR
    Career website. Menus are created via a post_init_hook to ensure
    proper theme loading and parent menu resolution. Also updates the
    website record with theme, homepage_url, and new logo.

--- hr_recruitment_kmitl/__init__.py
+++ hr_recruitment_kmitl/__init__.py
@@ -1,4 +1,47 @@
-# -*- coding: utf-8 -*-
 from . import controllers
 from . import models
+from odoo import api, SUPERUSER_ID
 
+
+def create_job_website_and_menu(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    website = env.ref("hr_recruitment_kmitl.job_website")
+    theme = env.ref("base.module_theme_kmitl")
+    # theme.with_context(website_id=website.id).button_upgrade()
+    theme._theme_load(website)
+    website.menu_id.child_id.unlink()
+
+    default_menu = website.menu_id.filtered(lambda m: m.url == "/default-main-menu")
+
+    env["website.menu"].create(
+        [
+            {
+                "name": "Home",
+                "url": "/jobs",
+                "website_id": website.id,
+                "parent_id": default_menu.id,
+                "sequence": 10,
+            },
+            {
+                "name": "Jobs",
+                "url": "/jobs",
+                "website_id": website.id,
+                "parent_id": default_menu.id,
+                "sequence": 20,
+            },
+            {
+                "name": "My Applications",
+                "url": "/my/applications",
+                "website_id": website.id,
+                "parent_id": default_menu.id,
+                "sequence": 30,
+            },
+            {
+                "name": "Profile",
+                "url": "/my/profile",
+                "website_id": website.id,
+                "parent_id": default_menu.id,
+                "sequence": 40,
+            },
+        ]
+    )
--- hr_recruitment_kmitl/__manifest__.py
+++ hr_recruitment_kmitl/__manifest__.py
@@ -53,5 +53,6 @@
     "application": False,
     "installable": True,
     "auto_install": False,
+    "post_init_hook": "create_job_website_and_menu",
     "license": "LGPL-3",
 }
