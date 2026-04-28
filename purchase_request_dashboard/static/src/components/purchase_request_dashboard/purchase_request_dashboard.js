d4f9651d041fa13b461569821377c61365403eac
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2026-04-28 16:49:52 +0700

    fix data

--- purchase_request_dashboard/static/src/components/purchase_request_dashboard/purchase_request_dashboard.js
+++ purchase_request_dashboard/static/src/components/purchase_request_dashboard/purchase_request_dashboard.js
@@ -163,7 +163,7 @@ export class PurchaseRequestDashboard extends Component {
             this.state.chart4Data = response.chart4_approved_trend;
             this.state.chart5Data = response.chart5_purchase_type_by_dept;
             this.state.chart6Data = response.chart6_expense_type_by_dept;
-            this.state.leadtimeHeatmap = response.leadtime_heatmap;
+            this.state.leadtimeHeatmap = response.chart7_leadtime_heatmap;
         } catch (error) {
             console.error("Error loading dashboard data:", error);
         } finally {
