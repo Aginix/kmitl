6900470a5694332d0d10e6e6c145f32a6feaf7f6
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-04-30 12:00:35 +0700

    เพิ่ม tier validation

---
+++ advance_payment_tier_validation/models/advance_payment.py
@@ -0,0 +1,17 @@
+from odoo import models
+
+
+class AdvancePayment(models.Model):
+    _name = "advance.payment"
+    _inherit = ["advance.payment", "tier.validation"]
+    _state_from = ["submitted"]
+    _state_to = ["approved"]
+    _tier_validation_manual_config = False
+
+    def _validate_tier(self, tiers=False):
+        super()._validate_tier(tiers)
+        reviews = self.review_ids.filtered(
+            lambda r: r.status == "pending" and (self.env.user in r.reviewer_ids)
+        )
+        if not reviews:
+            return self.action_approve()
