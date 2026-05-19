8a8cd483bd90f6b4026d6e6ec4d4a004ed4ea30c
nopparuts <ak3.nopparut@gmail.com>
2026-05-19 15:13:36 +0700

    [ADD] hr_employee_decoration

---
+++ hr_employee_decoration/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "HR Employee Decoration",
+    "version": "16.0.1.2.1",
+    "category": "Human Resources",
+    "website": "https://github.com/aginix/kmitl-odoo",
+    "author": "nopparuts, Aginix Technologies",
+    "maintainers": ["nopparuts"],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Allows storing information about employee decoration in KMITL",
+    "depends": ["hr", "office_order", "hr_employee_security_role"],
+    "assets": {
+        "web.assets_backend": [
+            "hr_employee_decoration/static/src/css/*.css",
+        ],
+    },
+    "data": [
+        "data/master_data_decoration_relation.xml",
+        "security/ir.model.access.csv",
+        "views/hr_employee.xml",
+        "views/hr_employee_decoration_views.xml",
+        "views/hr_employee_decoration_relation.xml",
+        "views/hr_menus.xml",
+    ],
+}
