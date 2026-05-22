2706455d9e17a9c3d88e09220aa7d34eddff0d5c
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2026-05-22 10:24:53 +0700

    fix actual amount

--- kris_project/models/kris_project_allocation.py
+++ kris_project/models/kris_project_allocation.py
@@ -73,21 +73,10 @@ class KrisProjectAllocationLine(models.Model):
             else:
                 line.allocation_pct = 0.0
 
-    @api.depends(
-        "project_id.receipt_ids.net_amount",
-        "estimated_amount",
-        "project_id.maintenance_deduction_amount",
-    )
+    @api.depends("receipt_allocation_ids.amount")
     def _compute_actual_amount(self):
         for line in self:
-            base = line.project_id.maintenance_deduction_amount
-            if base and line.estimated_amount:
-                ratio = line.estimated_amount / base
-                line.actual_amount = (
-                    sum(line.project_id.receipt_ids.mapped("net_amount")) * ratio
-                )
-            else:
-                line.actual_amount = 0.0
+            line.actual_amount = sum(line.receipt_allocation_ids.mapped("amount"))
 
     @api.constrains("estimated_amount")
     def _check_estimated_amount_sum(self):
