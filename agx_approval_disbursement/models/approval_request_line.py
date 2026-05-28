89f5d6e64a80f37d9ff25d85c204cdbadcc4c95e
Champ Phattarapon <kkw.phattarapon@gmail.com>
2026-05-28 23:26:13 +0700

    [16.0][IMP] agx_approval: multi-payee PDF + masked bank account (#797)
    Co-authored-by: PHATTARAPON.K <phattarapon.k@aginix.tech>

--- agx_approval_disbursement/models/approval_request_line.py
+++ agx_approval_disbursement/models/approval_request_line.py
@@ -16,4 +16,5 @@ class ApprovalRequestLine(models.Model):
             "price_unit": self.actual_amount,
             "account_id": account.id if account else False,
             "analytic_distribution": self.request_id.analytic_distribution,
+            "partner_bank_id": self._get_payee_bank().id or False,
         }
