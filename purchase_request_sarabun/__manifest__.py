a9f012b9a411c32d0ad39f4d86e97bbf5a609a77
PiyaChaiyanit <piya.chaiyanit@gmail.com>
2026-05-28 15:21:57 +0700

    imp th.po

--- purchase_request_sarabun/__manifest__.py
+++ purchase_request_sarabun/__manifest__.py
@@ -22,7 +22,6 @@
         "data/sarabun_route_template_data.xml",
         "views/portal_templates.xml",
         "views/purchase_request_views.xml",
-        "views/sarabun_document_views.xml",
         "reports/paperformat_purchase_request.xml",
         "reports/report_purchase_request.xml"
     ],
