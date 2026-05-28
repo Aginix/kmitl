e1073e258faee2d04f799828a2db9ae20f5783cd
PHATTARAPON.K <phattarapon.k@aginix.tech>
2026-05-28 10:39:10 +0700

    [16.0][IMP] accounting_kmitl: hide operating_unit_id from account.move views
    - Add account_operating_unit to depends so OCA views can be inherited.
    - Inherit OCA views and replace operating_unit_id field, search field,
      and Group By filter on both account.move and account.move.line
      (form, tree, search).
    - Hide analytic_distribution from the Budget group on account.move form
      by switching it to invisible.

--- accounting_kmitl/__manifest__.py
+++ accounting_kmitl/__manifest__.py
@@ -7,6 +7,7 @@
     "website": "https://www.kmitl.ac.th",
     "license": "AGPL-3",
     "depends": [
+        "account_operating_unit",
         "base_tier_validation",
         "budget",
         "l10n_th_account_tax",
