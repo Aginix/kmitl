c13067b3be82d23c7460128f8f569220dfdfde76
Nopparut Saelim <ak3.nopparut@gmail.com>
2026-05-29 18:33:55 +0700

    [16.0][ADD] hr_employee_kmitl_kid: add unique KID identifier to employees (#801)

---
+++ hr_employee_kmitl_kid/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "HR Employee KMITL KID",
+    "version": "16.0.1.0.0",
+    "summary": """ HR Employee KMITL KID Summary """,
+    "author": "nopparuts, Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl-odoo",
+    "category": "Human Resources",
+    "depends": [
+        "hr",
+    ],
+    "data": ["data/ir_sequence.xml", "views/hr_view_employee_form_views.xml"],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
