1358d51477597832f645fbd4fb9b3f9f1b6caa24
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2026-06-05 17:42:51 +0700

    [16.0][FIX] hr_employee_academic_standing_thailand: mirror fields to public model (#837)

--- hr_employee_academic_standing_thailand/models/hr_employee_public.py
+++ hr_employee_academic_standing_thailand/models/hr_employee_public.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import fields, models
 
 
 class HrEmployeePublic(models.Model):
@@ -6,5 +6,34 @@ class HrEmployeePublic(models.Model):
 
     academic_standing_id = fields.Many2one(
         related="employee_id.academic_standing_id",
+        readonly=True,
+    )
+    rank_id = fields.Many2one(
+        related="employee_id.rank_id",
+        readonly=True,
+    )
+    rank_nobility_id = fields.Many2one(
+        related="employee_id.rank_nobility_id",
+        readonly=True,
+    )
+    profession_id = fields.Many2one(
+        related="employee_id.profession_id",
+        readonly=True,
+    )
+
+    academic_standing_title = fields.Char(
+        related="employee_id.academic_standing_title",
+        readonly=True,
+    )
+    academic_standing_title_abbreviation = fields.Char(
+        related="employee_id.academic_standing_title_abbreviation",
+        readonly=True,
+    )
+    academic_standing_title_en = fields.Char(
+        related="employee_id.academic_standing_title_en",
+        readonly=True,
+    )
+    academic_standing_title_abbreviation_en = fields.Char(
+        related="employee_id.academic_standing_title_abbreviation_en",
         readonly=True,
     )
