62ad75a8c3a2a7141f0397ee896ad3d7ab5d505b
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2026-06-09 08:54:38 +0700

    [16.0][IMP] stock_kmitl: merge stock customization modules into core (#851)

--- stock_kmitl/models/stock_inventory.py
+++ stock_kmitl/models/stock_inventory.py
@@ -1,20 +1,15 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
-from odoo import _, api, fields, models
-from odoo.exceptions import UserError, ValidationError
+from odoo import fields, models
 
 
 class StockInventory(models.Model):
-    _inherit = 'stock.inventory'
-
-    READONLY_STATES = {
-        "draft": [("readonly", False)],
-    }
+    _inherit = "stock.inventory"
 
     def _default_inventory_name(self):
-        today_str = datetime.today().strftime('%d-%m-%Y')
-        return f'Inventory Adjustment {today_str}'
+        today_str = datetime.today().strftime("%d-%m-%Y")
+        return f"Inventory Adjustment {today_str}"
 
     name = fields.Char(
         default=_default_inventory_name,
@@ -23,4 +18,9 @@ class StockInventory(models.Model):
     responsible_id = fields.Many2one(
         comodel_name="res.users",
         default=lambda self: self.env.user,
+    )
+
+    department_id = fields.Many2one(
+        comodel_name="hr.department",
+        string="Department",
     )
