1f04e8a837dde9e453c58eca2ac2d30531e791c4
n3n <nonpawit.tee@gmail.com>
2026-06-18 15:07:31 +0700

    [ADD] mail_notification_inbox_email: add email+inbox notification type
    Adds a new notification type 'email_inbox' to res.users that delivers
    messages both to the Odoo inbox and by email simultaneously.

---
+++ mail_notification_inbox_email/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "User Notifications in Odoo and by Email",
+    "version": "16.0.1.0.0",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "category": "Discuss",
+    "summary": "User Notifications in Odoo and by Email",
+    "depends": ["mail"],
+    "description": "Handle user notifications in Odoo and by email",
+    "author": "Aginix Technologies",
+    "license": "LGPL-3",
+}
