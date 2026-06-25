eaefbd290e254704c5672228802ddf2378db3a79
Champ Phattarapon <kkw.phattarapon@gmail.com>
2026-06-25 23:20:41 +0700

    [16.0][IMP] disbursement_accounting_kmitl: DR smart button on account.move (#914)
    Co-authored-by: PHATTARAPON.K <phattarapon.k@aginix.tech>

--- disbursement_accounting_kmitl/__manifest__.py
+++ disbursement_accounting_kmitl/__manifest__.py
@@ -17,6 +17,7 @@
     "data": [
         "security/ir.model.access.csv",
         "views/disbursement_request_views.xml",
+        "views/account_move_views.xml",
         "views/account_move_line_views.xml",
     ],
     "installable": True,
