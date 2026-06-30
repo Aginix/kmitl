7d89189657c337a0e935bb65f996d569ef21e2aa
Tanathip Singhanon <88467996+madara1150@users.noreply.github.com>
2026-06-30 17:51:34 +0700

    [16.0][ADD] kris_project_receipt_report: เมนูแจกแจงรายรับข้ามโครงการ + Pivot Report (#928)

---
+++ kris_project_receipt_report/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "KRIS Project Receipt Report",
+    "version": "16.0.1.0.0",
+    "category": "KMITL",
+    "summary": "Cross-project listing and pivot report for KRIS receipts",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "depends": [
+        "kris_project",
+    ],
+    "data": [
+        "views/kris_project_receipt_views.xml",
+        "views/kris_project_receipt_menus.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
