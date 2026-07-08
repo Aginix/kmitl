f630efde33648625ad2f7b0f5281a36da6cf9199
Sunny <sunny@MacBook-Pro-khxng-Sunny.local>
2026-07-08 18:08:00 +0700

    imp kris.recepit attachment

--- kris_project/wizard/kris_project_receipt_wizard.py
+++ kris_project/wizard/kris_project_receipt_wizard.py
@@ -45,9 +45,8 @@ class KrisProjectReceiptWizard(models.TransientModel):
         inverse_name="wizard_id",
         string="Allocation",
     )
-    wizard_attachment_ids = fields.One2many(
-        comodel_name="kris.project.receipt.wizard.attachment",
-        inverse_name="wizard_id",
+    attachment_ids = fields.Many2many(
+        comodel_name="ir.attachment",
         string="Attachments",
     )
     note = fields.Text(
@@ -129,17 +128,16 @@ class KrisProjectReceiptWizard(models.TransientModel):
                     "remaining_amount": line.remaining_amount,
                 }
             )
-        for att in self.wizard_attachment_ids:
-            if att.file:
-                self.env["ir.attachment"].create(
-                    {
-                        "name": att.filename or "untitled",
-                        "datas": att.file,
-                        "res_model": "kris.project.receipt",
-                        "res_id": receipt.id,
-                        "document_type_id": att.document_type_id.id or False,
-                    }
-                )
+        # Attachments were uploaded to ir.attachment by the widget while the
+        # wizard was open; re-point them from the transient wizard to the newly
+        # created receipt so they show up on the receipt form.
+        if self.attachment_ids:
+            self.attachment_ids.write(
+                {
+                    "res_model": "kris.project.receipt",
+                    "res_id": receipt.id,
+                }
+            )
         return {"type": "ir.actions.act_window_close"}
 
 
@@ -186,24 +184,3 @@ class KrisProjectReceiptWizardLine(models.TransientModel):
             line.remaining_amount = (
                 alloc.estimated_amount - alloc.actual_amount if alloc else 0.0
             )
-
-
-class KrisProjectReceiptWizardAttachment(models.TransientModel):
-    _name = "kris.project.receipt.wizard.attachment"
-    _description = "KRIS Project Receipt Wizard Attachment"
-
-    wizard_id = fields.Many2one(
-        comodel_name="kris.project.receipt.wizard",
-        required=True,
-        ondelete="cascade",
-    )
-    file = fields.Binary(
-        string="File",
-        required=True,
-        attachment=False,
-    )
-    filename = fields.Char(string="Filename")
-    document_type_id = fields.Many2one(
-        comodel_name="kris.project.document.type",
-        string="Document Type",
-    )
