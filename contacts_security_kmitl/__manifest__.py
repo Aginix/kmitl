bd1facd55804b81f28335eeb48bae06e9242924a
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-08-03 15:31:50 +0700

    add contact security

---
+++ contacts_security_kmitl/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Contacts Security KMITL",
+    "version": "16.0.1.0.0",
+    "summary": "Restrict Contacts app visibility to purchase and billing users",
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "depends": [
+        "contacts_kmitl",
+        "purchase",
+        "account",
+    ],
+    "data": [
+        "views/menu.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
