dc6d67ebd9c9ebb2617adece60cba21ff77a3a2c
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-08-04 16:07:43 +0700

    add purchase group

---
+++ purchase_user_role/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase User Role',
+    'version': '16.0.1.0.0',
+    'summary': """ Purchase User Role Summary """,
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    'depends': ['budget', 'base_user_role', 'agx_sarabun', 'operating_unit_access_all', 'purchase_request_kmitl'],
+    'data': [
+        "data/res_users_role.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
