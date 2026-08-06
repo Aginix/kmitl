fee98ba27e1e02237dc2e6fd24bd74c421ef8516
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-08-06 11:59:16 +0700

    fix

---
+++ budget_user_role/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Budget User Role",
+    "version": "16.0.1.0.0",
+    "summary": "Budget user roles for KMITL",
+    "category": "KMITL",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/aginix/kmitl",
+    "depends": [
+        "budget",
+        "base_user_role",
+        "disbursement",
+        "procurement_plan",
+        "agx_sarabun",
+        "account",
+    ],
+    "data": [
+        "data/res_users_role.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
