7e048b27748cb8ce180558df930d422be2e8e77f
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-08-11 14:53:21 +0700

    fix bug

--- mail_activity_todo_role_unit/__init__.py
+++ mail_activity_todo_role_unit/__init__.py
@@ -1,9 +1,12 @@
+from odoo import SUPERUSER_ID, api
+
 from . import models
 
 
-def post_init_hook(env):
+def post_init_hook(cr, registry):
     """Populate todo_subscribed_operating_unit_ids = assigned_operating_unit_ids
     for all existing users who have an empty subscription list.  Idempotent."""
+    env = api.Environment(cr, SUPERUSER_ID, {})
     users = env["res.users"].with_context(active_test=False).search([])
     for user in users:
         if not user.todo_subscribed_operating_unit_ids:
