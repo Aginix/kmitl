be9d3b53eb1fa44df9fefb06d9fa55e0d10d08c5
Champ Phattarapon <kkw.phattarapon@gmail.com>
2026-08-14 11:30:24 +0700

    [16.0][ADD] finance_kmitl_assignment: มอบหมายผู้รับผิดชอบใบสำคัญจ่ายด้วย rule (#1092)
    Co-authored-by: PHATTARAPON.K <phattarapon.k@aginix.tech>

---
+++ finance_kmitl_assignment/__manifest__.py
@@ -0,0 +1,24 @@
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Finance Assignment KMITL",
+    "version": "16.0.1.0.0",
+    "category": "KMITL/Finance",
+    "summary": "Auto-assign a responsible finance officer to payment vouchers by rules",
+    "author": "KMITL",
+    "website": "https://www.kmitl.ac.th",
+    "license": "LGPL-3",
+    "depends": [
+        "finance_kmitl",
+        "partner_type_aginix",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizards/assign_officer_wizard_views.xml",
+        "views/account_payment_views.xml",
+        "views/assignment_rule_views.xml",
+        "views/finance_assignment_menus.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
