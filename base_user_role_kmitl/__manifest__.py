808974778d7cccf8117b6e9c36f15499efc231a1
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-08-20 16:50:00 +0700

    [16.0][ADD]base_user_role_kmitl

---
+++ base_user_role_kmitl/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Base User Role KMITL",
+    "version": "16.0.1.0.0",
+    "summary": "Predefined 'ผู้ใช้งานทั่วไป' role bundling KMITL general user groups",
+    "author": "Aginix Technologies",
+    "website": "https://github.com/Aginix/kmitl",
+    "category": "KMITL",
+    "depends": [
+        "base_user_role",
+        "purchase_request",
+        "agx_sarabun",
+    ],
+    "data": [
+        "security/res_users_role.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+    "license": "LGPL-3",
+}
