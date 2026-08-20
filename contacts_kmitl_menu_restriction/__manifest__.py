e0358b79ed21e8929c5ddaaaa383985be94448fe
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-08-20 15:23:35 +0700

    add : contacts_kmitl_menu_restriction

--- contacts_kmitl_menu_restriction/__manifest__.py
+++ contacts_kmitl_menu_restriction/__manifest__.py
@@ -1,17 +1,15 @@
 {
-    "name": "Contacts Security KMITL",
+    "name": "Contacts KMITL Menu Restriction",
     "version": "16.0.1.0.0",
-    "summary": "Restrict Contacts app visibility to purchase and billing users",
+    "summary": "Restrict Contacts app visibility to can_access_contacts_app group",
     "category": "KMITL",
     "author": "Aginix Technologies",
     "website": "https://github.com/aginix/kmitl",
     "depends": [
         "contacts_kmitl",
-        "purchase",
-        "account",
     ],
     "data": [
-        "security/ir.model.access.csv",
+        "security/contacts_security.xml",
         "views/menu.xml",
     ],
     "installable": True,
