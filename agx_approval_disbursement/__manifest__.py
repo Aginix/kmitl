b8f75a7af010ea84e792cbc1ac1a27b0176e9617
n3n <nonpawit.tee@gmail.com>
2026-08-21 21:03:59 +0700

    [IMP] agx_approval: bump versions for own-requests role
    agx_approval 16.0.1.2.0 -> 16.0.1.2.1
    agx_approval_disbursement 16.0.1.3.0 -> 16.0.1.3.1

--- agx_approval_disbursement/__manifest__.py
+++ agx_approval_disbursement/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Aginix Approval Disbursement",
-    "version": "16.0.1.3.0",
+    "version": "16.0.1.3.1",
     "category": "Accounting",
     "author": "KMITL",
     "depends": ["agx_approval", "disbursement"],
