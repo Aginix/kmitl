9205372028a42945ddd80cef678c5432728d4391
n3n <nonpawit.tee@gmail.com>
2026-08-26 21:33:49 +0700

    [IMP] partner_type_kmitl: add smart button linking to partners of same type

--- partner_type_kmitl/models/res_partner_type.py
+++ partner_type_kmitl/models/res_partner_type.py
@@ -17,6 +17,29 @@ class ResPartnerType(models.Model):
         comodel_name='res.partner', inverse_name='partner_type_id',
         string='Partners', readonly=True,
     )
+    partner_count = fields.Integer(
+        string='Partners', compute='_compute_partner_count',
+    )
+
+    def _compute_partner_count(self):
+        counts = self.env['res.partner'].read_group(
+            [('partner_type_id', 'in', self.ids)],
+            ['partner_type_id'],
+            ['partner_type_id'],
+        )
+        data = {row['partner_type_id'][0]: row['partner_type_id_count'] for row in counts}
+        for rec in self:
+            rec.partner_count = data.get(rec.id, 0)
+
+    def action_view_partners(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': self.name,
+            'res_model': 'res.partner',
+            'view_mode': 'tree,form',
+            'domain': [('partner_type_id', '=', self.id)],
+        }
 
     def unlink(self):
         for record in self:
