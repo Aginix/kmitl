3969495eb4cc79d6a3313e10d14a7e3a0c994edf
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2026-09-18 17:18:21 +0700

    fix

--- purchase_guarantee_operating_unit/models/__init__.py
+++ purchase_guarantee_operating_unit/models/__init__.py
@@ -1,4 +1,2 @@
 # -*- coding: utf-8 -*-
 from . import purchase_guarantee
-from . import purchase_order
-from . import purchase_request
--- purchase_guarantee_operating_unit/models/purchase_guarantee.py
+++ purchase_guarantee_operating_unit/models/purchase_guarantee.py
@@ -8,10 +8,7 @@ class PurchaseGuarantee(models.Model):
     operating_unit_id = fields.Many2one(
         comodel_name="operating.unit",
         string="Operating Unit",
-        default=lambda self: self.env["res.users"].operating_unit_default_get(),
+        default=lambda self: self.env["res.users"].operating_unit_default_get(
+            self.env.uid
+        )
     )
-
-    def _prepare_account_payment_vals(self):
-        vals = super()._prepare_account_payment_vals()
-        vals["operating_unit_id"] = self.operating_unit_id.id
-        return vals
